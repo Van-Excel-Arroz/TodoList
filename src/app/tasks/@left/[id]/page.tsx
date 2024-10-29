cbded051de08d5486357c63e1204f8098ba59d78
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-10-30 07:08:57 +0800

    commit

---
+++ src/app/tasks/@left/[id]/page.tsx
@@ -0,0 +1,5 @@
+import Sidebar from '@/components/Sidebar/Sidebar';
+
+export default async function LeftPage({ params }: { params: any }) {
+	return <Sidebar />;
+}
--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -1,17 +1,17 @@
 import { SidebarProvider } from '@/components/Context/SidebarContext';
-import Sidebar from '@/components/Sidebar/Sidebar';
 
 interface layoutProps {
 	main: React.ReactNode;
+	left: React.ReactNode;
 }
 
-export default function layout({ main }: layoutProps) {
+export default function layout({ main, left }: layoutProps) {
 	return (
 		<>
 			<SidebarProvider>
 				<div className="flex relative">
-					<Sidebar />
-					{main}
+					<div className="w-1/4">{left}</div>
+					<div className="flex-1">{main}</div>
 				</div>
 			</SidebarProvider>
 		</>
