946ed901134fd9d5cacdfefa154dc71b9b3fed75
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-10-31 07:45:22 +0800

    commit

--- src/actions/todolist-action.ts
+++ src/actions/todolist-action.ts
@@ -2,10 +2,17 @@
 
 import { storeTodo } from '@/lib/todo';
 import { storeTodolist } from '@/lib/todolist';
+import { revalidatePath } from 'next/cache';
 
 export async function createTodolist(title: string) {
 	if (title.trim().length <= 100) {
 		const todolistId = await storeTodolist(title, 1);
+
+		revalidatePath(`/tasks/home`);
+
+		if (todolistId) {
+			revalidatePath(`/tasks/${todolistId}`);
+		}
 
 		return todolistId;
 	} else {
@@ -16,5 +23,6 @@ export async function createTodolist(title: string) {
 
 export async function createTodo(text: string, category: string, dueDatetime: string | null, todolistId: number) {
 	const result = await storeTodo(text, category, dueDatetime, todolistId);
+	revalidatePath(`/tasks/${todolistId}`);
 	return result;
 }
