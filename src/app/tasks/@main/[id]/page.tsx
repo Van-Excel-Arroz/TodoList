17e302217c2a8a8a5935ee6881ad69aead251b89
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-10-31 14:06:58 +0800

    commit

--- src/app/tasks/@main/[id]/page.tsx
+++ src/app/tasks/@main/[id]/page.tsx
@@ -1,26 +1,5 @@
-import TodoList from '@/components/TodoList/TodoList';
-import { getTodos } from '@/lib/todo';
-import { getTodolist } from '@/lib/todolist';
+import Todolist from '@/components/TodoList/Todolist';
 
-import { notFound, redirect } from 'next/navigation';
-import { Suspense } from 'react';
-
-export default async function TodolistPage({ params }: { params: any }) {
-	const todolistId = Number(params.id);
-	const todolist = (await getTodolist(todolistId, 1)) ?? {};
-	const todos = await getTodos(todolistId);
-
-	if (todolistId !== todolist.id) {
-		return notFound();
-	}
-
-	if (!todolist || !todolist.id) {
-		redirect('/tasks/home');
-	}
-
-	return (
-		<Suspense fallback={<h1>Loading.....</h1>}>
-			<TodoList todolistId={todolistId} title={todolist.title} initialTodos={todos} />
-		</Suspense>
-	);
+export default function TodolistPage({ params }: { params: any }) {
+	return <Todolist params={params} />;
 }
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,68 +1,26 @@
-'use client';
+import TodoListContent from '@/components/TodoList/TodoListContent';
+import { getTodos } from '@/lib/todo';
+import { getTodolist } from '@/lib/todolist';
 
-import { useState, useTransition, useCallback, memo } from 'react';
-import SidebarToggle from '../Sidebar/SidebarToggle';
-import TodoForm from './TodoForm';
-import TodoItem from './TodoItem';
-import React from 'react';
-import useSidebarStore from '../Context/SidebarContext';
+import { notFound, redirect } from 'next/navigation';
+import { Suspense } from 'react';
 
-interface Todo {
-	id: number;
-	task_text: string;
-	category: string | null;
-	due_datetime: string | null;
-	creation_date: string;
-	todo_list_id: number;
-}
+export default async function Todolist({ params }: { params: any }) {
+	const todolistId = Number(params.id);
+	const todolist = (await getTodolist(todolistId, 1)) ?? {};
+	const todos = await getTodos(todolistId);
 
-interface TodolistContentProps {
-	todolistId: number;
-	title: string;
-	initialTodos: Todo[];
-}
+	if (todolistId !== todolist.id) {
+		return notFound();
+	}
 
-const MemoizedTodoForm = memo(TodoForm);
-const MemoizedTodoItem = memo(TodoItem);
-const MemoizedSidebarToggle = memo(SidebarToggle, (prevProps, nextProps) => {
-	return true;
-});
-
-function TodoList({ todolistId, title, initialTodos }: TodolistContentProps) {
-	const [todos, setTodos] = useState(initialTodos);
-	const [isPending, startTransition] = useTransition();
-	const { isSidebarOpen } = useSidebarStore();
-
-	const handleAddTodo = useCallback((todo: Todo) => {
-		startTransition(() => {
-			setTodos(prevTodos => [...prevTodos, todo]);
-		});
-	}, []);
+	if (!todolist || !todolist.id) {
+		redirect('/tasks/home');
+	}
 
 	return (
-		<>
-			<div className="flex flex-col px-6 p-9 ">
-				<div className="flex gap-4">
-					{!isSidebarOpen && <MemoizedSidebarToggle />}
-					<p className="text-lg font-bold">
-						{title} #{todolistId}
-					</p>
-				</div>
-				<MemoizedTodoForm todolistId={todolistId} onAdd={handleAddTodo} />
-
-				<div className="grid grid-cols-6 my-3 px-4 font-semibold">
-					<p className="col-span-4">Todos</p>
-					<p className="text-center">Due Date</p>
-					<p className="text-center">Created In</p>
-				</div>
-				<div>
-					{todos.map(todo => (
-						<MemoizedTodoItem key={todo.id} todo={todo} />
-					))}
-				</div>
-			</div>
-		</>
+		<Suspense fallback={<h1>Loading.....</h1>}>
+			<TodoListContent todolistId={todolistId} title={todolist.title} initialTodos={todos} />
+		</Suspense>
 	);
 }
-
-export default React.memo(TodoList);
