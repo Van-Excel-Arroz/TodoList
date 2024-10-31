5fbe0dcc44abe765c39aef7445463aa01edb559d
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-10-31 17:34:55 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,15 @@ import Link from 'next/link';
 export default function page() {
 	return (
 		<>
-			<h1>Welcome</h1>
-			<Link href={'/tasks/home'}>Create Your Tasks!</Link>
+			<div className="container mx-auto h-screen flex flex-col justify-center items-center">
+				<p className="text-9xl mb-5 align-top">Welcome</p>
+				<Link
+					href={'/tasks/home'}
+					className="block bg-black text-white text-center text-3xl px-14 py-7 mt-5 rounded-2xl"
+				>
+					Create Your Tasks!
+				</Link>
+			</div>
 		</>
 	);
 }
