c254c78f437f4e09a2016233654d742c35277557
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-04 20:45:38 +0800

    commit

--- src/components/Sidebar/TodoListItem.tsx
+++ src/components/Sidebar/TodoListItem.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { memo, useState } from 'react';
+import { memo } from 'react';
 import { RiPencilFill } from 'react-icons/ri';
 import DeleteTodolistButton from './DeleteTodolistButton';
 
@@ -16,30 +16,19 @@ interface TodolistItemProps {
 function TodoListItem({ todolist }: TodolistItemProps) {
 	const pathname = usePathname();
 	const isSelectedPath = pathname === `/tasks/${todolist.id}`;
-	const [isHovered, setHovered] = useState(false);
-
-	const handleMouseEnter = () => {
-		setHovered(true);
-	};
-
-	const handleMouseLeave = () => {
-		setHovered(false);
-	};
 
 	return (
 		<div
-			className={`flex items-center pr-5 ${
+			className={`flex items-center pr-5 group ${
 				isSelectedPath ? 'border-l-4 border-slate-500 bg-slate-200' : 'pl-1 hover:bg-slate-100 '
 			}`}
-			onMouseEnter={handleMouseEnter}
-			onMouseLeave={handleMouseLeave}
 		>
 			<Link href={`/tasks/${todolist.id}`} className="font-thin text-md w-full block py-3 px-5">
 				{todolist.title}
 			</Link>
-			<div className="flex items-center gap-3">
-				{isHovered && <RiPencilFill size={16} className="cursor-pointer" />}
-				{isHovered && <DeleteTodolistButton todolistId={todolist.id} />}
+			<div className="flex items-center gap-3 opacity-0 group-hover:opacity-100">
+				<RiPencilFill size={16} className="cursor-pointer" />
+				<DeleteTodolistButton todolistId={todolist.id} />
 			</div>
 		</div>
 	);
