4b730f4fdd05f02bb2231954ef468199b58bd3d9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-05 15:18:36 +0800

    commit

--- src/components/Sidebar/TodoListItem.tsx
+++ src/components/Sidebar/TodoListItem.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { memo, useEffect, useRef, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { RiPencilFill } from 'react-icons/ri';
 import DeleteTodolistButton from './DeleteTodolistButton';
 import { useForm } from 'react-hook-form';
@@ -16,35 +16,31 @@ interface TodolistItemProps {
 	todolist: Todolist;
 }
 
+interface FormInputs {
+	title: string;
+}
+
 function TodoListItem({ todolist }: TodolistItemProps) {
 	const pathname = usePathname();
 	const isSelectedPath = pathname === `/tasks/${todolist.id}`;
 	const [isEditing, setIsEditing] = useState(false);
-	const { register, handleSubmit, reset, watch } = useForm({
+	const { register, handleSubmit, reset } = useForm<FormInputs>({
 		defaultValues: {
 			title: todolist.title,
 		},
 	});
-	const inputRef = useRef<HTMLInputElement>(null);
-
-	console.log('Form values:', watch());
 
 	const handleEditClick = (val: boolean) => {
 		setIsEditing(val);
 	};
 
-	const onSubmit = (data: any) => {
+	const onSubmit = (data: FormInputs) => {
 		console.log(data.title);
-		console.log('On Submit function');
+		console.log('Form submitted with data:', data);
+		console.log('Title value:', data.title);
 		handleEditClick(false);
 		reset();
 	};
-
-	useEffect(() => {
-		if (isEditing && inputRef.current) {
-			inputRef.current.focus();
-		}
-	}, [isEditing]);
 
 	const handleInputBlur = () => {
 		setIsEditing(false);
@@ -56,31 +52,33 @@ function TodoListItem({ todolist }: TodolistItemProps) {
 				isSelectedPath ? 'border-l-4 border-slate-500 bg-slate-200' : 'pl-1 hover:bg-slate-100 '
 			}`}
 		>
-			<Link
-				href={`/tasks/${todolist.id}`}
-				className={`text-md w-full block py-3 px-5 ${isSelectedPath ? 'font-normal' : 'font-light'}`}
-			>
+			<div className="text-md w-full block">
 				{isEditing ? (
 					<form onSubmit={handleSubmit(onSubmit)}>
 						<input
-							{...register('title', { maxLength: { value: 50, message: 'Exceeded maximum length of 50' } })}
+							{...register('title')}
 							type="text"
 							placeholder={todolist.title}
-							className="bg-transparent focus:outline-none border-b border-slate-950"
-							ref={inputRef}
+							className="bg-transparent focus:outline-none border-b border-slate-950 my-3 mx-5"
+							autoFocus
 							onBlur={handleInputBlur}
 							defaultValue={todolist.title}
 						/>
 					</form>
 				) : (
-					todolist.title
+					<Link
+						href={`/tasks/${todolist.id}`}
+						className={`w-full block py-3 px-5 ${isSelectedPath ? 'font-normal' : 'font-light'}`}
+					>
+						{todolist.title}
+					</Link>
 				)}
-			</Link>
+			</div>
+
 			<div className="flex items-center gap-3 opacity-0 group-hover:opacity-100">
 				<button onClick={() => handleEditClick(true)}>
 					<RiPencilFill size={16} className="cursor-pointer" />
 				</button>
-
 				<DeleteTodolistButton todolistId={todolist.id} />
 			</div>
 		</div>
