9ca82bdd9b586df45bcf938707412e14574f8116
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-05 17:07:07 +0800

    commit

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -7,7 +7,7 @@ export default function layout({ main, left }: layoutProps) {
 	return (
 		<>
 			<div className="flex relative h-screen bg-slate-100">
-				<div className="w-1/4 sticky top-0 left-0 h-screen">{left}</div>
+				<div className="sticky top-0 left-0 h-screen">{left}</div>
 				<div className="w-full overflow-y-auto">{main}</div>
 				<div className="w-1/4 bg-white border-l-2">Hello World</div>
 			</div>
--- src/components/Sidebar/SidebarList.tsx
+++ src/components/Sidebar/SidebarList.tsx
@@ -26,9 +26,7 @@ const SidebarList = memo(function ({ initialTodolist }: SidebarListProps) {
 
 	return (
 		<>
-			<div
-				className={`font-body border-r-2 rounded-r-3xl bg-white py-9 h-screen ${isSidebarOpen ? 'w-full' : 'hidden'}`}
-			>
+			<div className={`font-body border-r-2 rounded-r-3xl bg-white py-9 h-screen ${isSidebarOpen ? 'w-72' : 'hidden'}`}>
 				<SidebarContent todolists={todolists} onAddTodolist={handleAddTodolist} />
 			</div>
 		</>
