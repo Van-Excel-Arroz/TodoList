1d25c6342e44e13e0fc2dc2986d09c4397a1ed69
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-05 17:40:01 +0800

    commit

--- src/actions/todolist-action.ts
+++ src/actions/todolist-action.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { storeTodo } from '@/lib/todo';
-import { deleteTodolist, storeTodolist } from '@/lib/todolist';
+import { deleteTodolist, storeTodolist, updateTodolist } from '@/lib/todolist';
 import { revalidatePath } from 'next/cache';
 
 export async function createTodolist(title: string) {
@@ -27,5 +27,14 @@ export async function deleteTodolistAction(todolistId: number, user_id: number) 
 		revalidatePath(`/tasks/home`);
 	} else {
 		console.error('Failed to delete the todolist');
+	}
+}
+
+export async function updateTodolistAction(todolistId: number, title: string) {
+	const result = await updateTodolist(todolistId, title);
+	if (result) {
+		revalidatePath(`/tasks/${todolistId}`);
+	} else {
+		console.error('Failed to update the todolist');
 	}
 }
