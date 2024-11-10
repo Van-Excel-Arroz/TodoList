b4eadda392c91a46815ed84eb105cacb5f53616c
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-10 19:16:41 +0800

    commit

--- src/components/SidebarToggle.tsx
+++ src/components/SidebarToggle.tsx
@@ -5,5 +5,9 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 
 export default function SidebarToggle() {
 	const { toggleSidebar } = useSidebarStore();
-	return <button onClick={toggleSidebar}>{/* <RxHamburgerMenu size={20} /> */}</button>;
+	return (
+		<button onClick={toggleSidebar}>
+			<RxHamburgerMenu size={20} />
+		</button>
+	);
 }
