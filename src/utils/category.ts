52d9ad26201a1318c7a90bfb027e73fc264aea22
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-15 09:29:28 +0800

    commit

--- src/utils/category.ts
+++ src/utils/category.ts
@@ -1,13 +1,16 @@
 export function extractCategory(title: string): string[] {
-	const regex = /#(\S+)/g;
+	const regex = /#(\S+)/g; // patterns like "#work #urgent #school"
 	let categories: string[] = [];
 	let match: RegExpExecArray | null;
 
 	while ((match = regex.exec(title)) !== null) {
+		// return null if it doesnt find # and reaches the end of string
 		categories.push(match[1]);
+		// match[0] -> #(\S+); -> "#work"
+		// match[1] -> (\S+) "work"
 	}
 
-	return categories;
+	return categories; // ['work', 'urgent', 'school']
 }
 
 export function extractTitle(title: string): string {
