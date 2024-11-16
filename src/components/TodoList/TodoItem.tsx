49f0d17fdcf6fc6e3b36c4b27ec49f8dda7a2ba9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-16 11:28:57 +0800

    commit

--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -16,7 +16,7 @@ function TodoItem({ todo }: TodoItemProps) {
 				<div className="flex">
 					{todo.categories &&
 						todo.categories.map(category => (
-							<span className={`text-xs border shadow-md rounded mt-2 py-1 px-2 text-[#${category.hex_color}]`}>
+							<span className={`text-xs border shadow-md rounded mr-2 mt-2 py-1 px-2 text-[#${category.hex_color}]`}>
 								{category.category_title}
 							</span>
 						))}
