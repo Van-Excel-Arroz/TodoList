186dadda6e903f0c37f15123f2a4dcfc17ae5cd0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-16 11:38:19 +0800

    commit

--- src/lib/todo.ts
+++ src/lib/todo.ts
@@ -66,7 +66,7 @@ export async function getTodoWithCategories(todoId: number) {
 		`,
 		[todoId]
 	);
-	return result.rows;
+	return result.rows[0] || null;
 }
 
 export async function getTodosWithCategories(todolistId: number) {
