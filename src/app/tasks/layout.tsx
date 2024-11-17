345fbea8a085fc4f4965f603c04aabf575efb20d
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-17 10:55:12 +0800

    commit

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -9,7 +9,6 @@ export default function layout({ main, left }: layoutProps) {
 			<div className="flex relative h-screen bg-slate-100">
 				<div className="sticky top-0 left-0 h-screen">{left}</div>
 				<div className="w-full overflow-y-auto">{main}</div>
-				<div className="w-1/4 bg-white border-l-2">Hello World</div>
 			</div>
 		</>
 	);
