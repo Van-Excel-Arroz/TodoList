64e911d714295d3cbe96b45ebb4a3122d29caa7b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-18 16:21:08 +0800

    commit

--- src/components/Sidebar/SidebarWrapper.tsx
+++ src/components/Sidebar/SidebarWrapper.tsx
@@ -8,8 +8,8 @@ function SidebarWrapper({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<div
-				className={`font-body border-r-2 rounded-r-3xl drop-shadow-xl bg-white py-9 h-screen ${
-					isSidebarOpen ? 'w-72' : 'hidden'
+				className={`font-body border-r-2 rounded-r-3xl drop-shadow-xl bg-white py-9 h-screen transition-all duration-200 ease-in-out  ${
+					isSidebarOpen ? 'w-72' : 'w-0 overflow-hidden'
 				}`}
 			>
 				{children}
