6b605d9eaf74f8fc249cba7126622f9a6adcc04f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-18 18:09:08 +0800

    commit

--- src/components/SidebarToggle.tsx
+++ src/components/SidebarToggle.tsx
@@ -2,13 +2,14 @@
 
 import useSidebarStore from '@/components/Context/SidebarContext';
 import { memo } from 'react';
-import { RxHamburgerMenu } from 'react-icons/rx';
 
 function SidebarToggle() {
 	const { toggleSidebar } = useSidebarStore();
 	return (
 		<button onClick={toggleSidebar} aria-label="Sidebar Menu">
-			<RxHamburgerMenu size={20} />
+			<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
+				<path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
+			</svg>
 		</button>
 	);
 }
