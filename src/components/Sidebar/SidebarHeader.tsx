9abd64e0405c4375528e34c0eb41d4ef9dda6b4c
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-19 16:39:36 +0800

    commit

--- src/components/Sidebar/SidebarHeader.tsx
+++ src/components/Sidebar/SidebarHeader.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { memo } from 'react';
 import SidebarToggle from '../SidebarToggle';
+import { House } from 'lucide-react';
 
 function SidebarHeader() {
 	const pathname = usePathname();
@@ -12,9 +13,7 @@ function SidebarHeader() {
 		<div className="flex gap-4 items-center">
 			{!isHome && <SidebarToggle />}
 			<Link href="/" className="text-lg font-bold" aria-label="home">
-				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 24 24">
-					<path d="M 12 2 A 1 1 0 0 0 11.289062 2.296875 L 1.203125 11.097656 A 0.5 0.5 0 0 0 1 11.5 A 0.5 0.5 0 0 0 1.5 12 L 4 12 L 4 20 C 4 20.552 4.448 21 5 21 L 9 21 C 9.552 21 10 20.552 10 20 L 10 14 L 14 14 L 14 20 C 14 20.552 14.448 21 15 21 L 19 21 C 19.552 21 20 20.552 20 20 L 20 12 L 22.5 12 A 0.5 0.5 0 0 0 23 11.5 A 0.5 0.5 0 0 0 22.796875 11.097656 L 12.716797 2.3027344 A 1 1 0 0 0 12.710938 2.296875 A 1 1 0 0 0 12 2 z"></path>
-				</svg>
+				<House />
 			</Link>
 		</div>
 	);
--- src/components/SidebarToggle.tsx
+++ src/components/SidebarToggle.tsx
@@ -1,15 +1,14 @@
 'use client';
 
 import useSidebarStore from '@/components/Context/SidebarContext';
+import { Menu } from 'lucide-react';
 import { memo } from 'react';
 
 function SidebarToggle() {
 	const { toggleSidebar } = useSidebarStore();
 	return (
 		<button onClick={toggleSidebar} aria-label="Sidebar Menu">
-			<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
-				<path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
-			</svg>
+			<Menu />
 		</button>
 	);
 }
