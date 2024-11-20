7be8a282e80c019bb840b08646ba1f996779afc4
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-20 10:20:27 +0800

    commit

--- src/components/TodoList/TodosRender.tsx
+++ src/components/TodoList/TodosRender.tsx
@@ -17,7 +17,7 @@ const itemVariants = {
 function TodosRender({ todos }: TodosRenderProps) {
 	return (
 		<>
-			<div className="bg-white px-8 py-4 border">
+			<div className="bg-white px-8 py-4 border rounded-2xl">
 				<div className="grid grid-cols-6 font-semibold">
 					<p className="col-span-4">Todos</p>
 					<p className="text-center">Due Date</p>
