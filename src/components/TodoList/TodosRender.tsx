d4e9d6e88fd1bce4887d4576955f5da1e64e4f8a
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-23 10:03:47 +0800

    commit

--- src/components/TodoList/TodosRender.tsx
+++ src/components/TodoList/TodosRender.tsx
@@ -6,6 +6,9 @@ interface TodosRenderProps {
 }
 
 export default function TodosRender({ todos }: TodosRenderProps) {
+	const notCompletedTodos = todos.filter(todo => todo.is_completed === false);
+	const completedTodos = todos.filter(todo => todo.is_completed === true);
+
 	return (
 		<>
 			<div className="bg-white px-8 py-4 border rounded-2xl">
@@ -15,7 +18,22 @@ export default function TodosRender({ todos }: TodosRenderProps) {
 					<p className="text-center">Created In</p>
 				</div>
 				<ul>
-					{todos.map(todo => (
+					{notCompletedTodos.map(todo => (
+						<li>
+							<TodoItem todo={todo} />
+						</li>
+					))}
+				</ul>
+			</div>
+
+			<div className="bg-white px-8 py-4 border rounded-2xl mt-10">
+				<div className="grid grid-cols-6 font-semibold mb-4">
+					<p className="col-span-4 ml-12">Completed Todos</p>
+					<p className="text-center">Due Date</p>
+					<p className="text-center">Created In</p>
+				</div>
+				<ul>
+					{completedTodos.map(todo => (
 						<li>
 							<TodoItem todo={todo} />
 						</li>
