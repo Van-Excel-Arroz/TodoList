2e961531aeb4cf531bf342b7e93050eb4a23c207
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-23 09:32:59 +0800

    commit

--- src/components/TodoList/TodoListContent.tsx
+++ src/components/TodoList/TodoListContent.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import TodoForm from './TodoForm';
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo, useState } from 'react';
 import TodolistHeader from './TodolistHeader';
 import TodosRender from './TodosRender';
 import { Todo, TodoList } from '@/types';
-import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation';
 
 interface TodolistContentProps {
 	todolist: TodoList;
@@ -16,8 +14,6 @@ interface TodolistContentProps {
 
 function TodoListContent({ todolist, todolistId, initialTodos }: TodolistContentProps) {
 	const [todos, setTodos] = useState<Todo[]>(initialTodos);
-	const pathname = usePathname();
-	const router = useRouter();
 
 	const addTodo = (newTodo: Todo) => {
 		setTodos(prevTodos => [...prevTodos, newTodo]);
@@ -25,17 +21,7 @@ function TodoListContent({ todolist, todolistId, initialTodos }: TodolistContent
 
 	const updateCompletion = (todoId: number, isCompleted: boolean) => {
 		setTodos(prevTodos => prevTodos.map(todo => (todo.id === todoId ? { ...todo, is_completed: isCompleted } : todo)));
-		sessionStorage.setItem('needsRevalidation', pathname);
 	};
-
-	useEffect(() => {
-		const revalidatePathNeeded = sessionStorage.getItem('needsRevalidation');
-
-		if (revalidatePathNeeded === pathname) {
-			router.refresh();
-			sessionStorage.removeItem('needsRevalidation');
-		}
-	}, [pathname]);
 
 	return (
 		<>
