2624ef6c8405ecec4140224d44db6257fc145722
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-25 19:09:59 +0800

    commit

--- src/components/EmptyTodolistMessage.tsx
+++ src/components/EmptyTodolistMessage.tsx
@@ -6,7 +6,7 @@ export default function EmptyTodolistMessage() {
 			<ClipboardList size={48} className="text-gray-400" />
 			<div className="text-center">
 				<p className="text-2xl font-semibold">No todolist selected</p>
-				<p className="mt-2 text-gray-500">
+				<p className="mt-2 text-gray-600">
 					Create a new todolist or select an exisiting one from the sidebar to get started.
 				</p>
 			</div>
