b342d44bab9c6530b4098042b35a31fce0ab67da
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-27 16:47:09 +0800

    commit

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,17 +1,12 @@
 import TodoListContent from '@/components/TodoList/TodoListContent';
 import { getTodosWithCategories } from '@/lib/todo';
 import { getTodolist } from '@/lib/todolist';
-import { notFound } from 'next/navigation';
 import { Suspense } from 'react';
 
 export default async function Todolist({ params }: { params: any }) {
 	const todolistId = Number(params.id);
 	const todolist = await getTodolist(todolistId, 1);
 	const todos = await getTodosWithCategories(todolistId);
-
-	if (todolistId !== todolist.id) {
-		return notFound();
-	}
 
 	return (
 		<Suspense fallback={<h1>Loading.....</h1>}>
