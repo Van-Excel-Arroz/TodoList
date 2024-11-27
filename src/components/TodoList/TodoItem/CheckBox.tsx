a564e80b98f1a5840f533b242ebfdf7c0e656d20
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-27 18:33:14 +0800

    commit

--- src/components/TodoList/TodoItem/CheckBox.tsx
+++ src/components/TodoList/TodoItem/CheckBox.tsx
@@ -9,7 +9,7 @@ interface CheckBoxProps {
 function CheckBox({ isChecked, handleOnClick }: CheckBoxProps) {
 	return (
 		<div className="col-end-1 flex items-center">
-			<button className="flex items-center" onClick={handleOnClick}>
+			<button className="flex items-center" onClick={handleOnClick} aria-label={isChecked ? 'checked' : 'unchecked'}>
 				{isChecked ? (
 					<div className="bg-black p-1 w-5 h-5 flex justify-center items-center rounded-md hover:bg-slate-800 active:bg-slate-700">
 						<Check color="white" size={15} />
