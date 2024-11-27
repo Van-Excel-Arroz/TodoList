db6d904d01836c18c21174cc5081e5b88b5e9929
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-27 17:55:16 +0800

    commit

--- src/components/TodoList/CategoriesRender.tsx
+++ src/components/TodoList/CategoriesRender.tsx
@@ -3,21 +3,19 @@ import { Category } from '@/types';
 export default function CategoriesRender({ selectedCategories }: { selectedCategories: Category[] }) {
 	return (
 		<>
-			<div className="flex flex-wrap">
-				{selectedCategories.length > 0 ? (
-					<div>
-						{selectedCategories.map(selectedCategory => (
-							<span
-								key={selectedCategory.id}
-								className={`text-xs border shadow-md ml-2 rounded py-1 px-2 hover:bg-slate-100 hover:shadow-none active:bg-slate-200`}
-								style={{ color: selectedCategory.hex_color }}
-							>
-								{selectedCategory.category_title}
-							</span>
-						))}
-					</div>
-				) : null}
-			</div>
+			{selectedCategories.length > 0 ? (
+				<div>
+					{selectedCategories.map(selectedCategory => (
+						<div
+							key={selectedCategory.id}
+							className="text-xs border shadow-md ml-2 rounded py-1 px-2 cursor-pointer inline-block  hover:bg-slate-100 hover:shadow-none active:bg-slate-200"
+							style={{ color: selectedCategory.hex_color }}
+						>
+							<p>{selectedCategory.category_title}</p>
+						</div>
+					))}
+				</div>
+			) : null}
 		</>
 	);
 }
