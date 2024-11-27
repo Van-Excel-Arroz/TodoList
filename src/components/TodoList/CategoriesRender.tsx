67fd8fe39c23b2ee8df852c13ecce685e03fbdd2
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-27 18:01:06 +0800

    commit

--- src/components/TodoList/CategoriesRender.tsx
+++ src/components/TodoList/CategoriesRender.tsx
@@ -1,17 +1,18 @@
 import { Category } from '@/types';
+import { X } from 'lucide-react';
 
 export default function CategoriesRender({ selectedCategories }: { selectedCategories: Category[] }) {
 	return (
 		<>
 			{selectedCategories.length > 0 ? (
-				<div>
+				<div className="flex flex-row">
 					{selectedCategories.map(selectedCategory => (
 						<div
 							key={selectedCategory.id}
-							className="text-xs border shadow-md ml-2 rounded py-1 px-2 cursor-pointer inline-block  hover:bg-slate-100 hover:shadow-none active:bg-slate-200"
-							style={{ color: selectedCategory.hex_color }}
+							className="text-xs border shadow-md ml-2 rounded py-1 px-2 w-fit flex flex-row items-center justify-between gap-2"
 						>
-							<p>{selectedCategory.category_title}</p>
+							<p style={{ color: selectedCategory.hex_color }}>{selectedCategory.category_title}</p>
+							<X size={12} className="hover:text-white hover:bg-gray-700 rounded-full cursor-pointer" />
 						</div>
 					))}
 				</div>
