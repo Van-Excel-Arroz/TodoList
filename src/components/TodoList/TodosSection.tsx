194e03174ff5dcfb558249abff92977ffac073e2
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-28 11:14:17 +0800

    commit

--- src/components/TodoList/TodosSection.tsx
+++ src/components/TodoList/TodosSection.tsx
@@ -9,7 +9,7 @@ interface TodosSectionProps {
 export default function TodosSection({ title, todos }: TodosSectionProps) {
 	return (
 		<div className="bg-white px-8 py-4 border rounded-2xl mt-5">
-			<div className="grid grid-cols-6 font-semibold mb-4">
+			<div className="grid grid-cols-6 font-semibold">
 				<p className="col-span-4 ml-12">{title}</p>
 				<p className="text-center">Due Date</p>
 				<p className="text-center">Created In</p>
