935afc53bfed0411dc147a3fb8cad20c48df87bf
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-29 09:35:21 +0800

    commit

--- src/components/TodoList/TodoForm.tsx
+++ src/components/TodoList/TodoForm.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import { createTodoAction } from '@/actions/todolist-action';
 import { extractCategory, extractTitle } from '@/utils/category';
 import { memo } from 'react';
+import { register } from 'module';
 
 interface TodoFormData {
 	todo?: string;
@@ -42,36 +43,44 @@ function TodoForm({ todolistId }: TodoFormProps) {
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)} className="mt-7 flex flex-col items-start">
-			<input
-				{...register('todo', {
-					required: true,
-					maxLength: { value: 100, message: 'Exceeded maximum length of 100' },
-				})}
-				type="text"
-				placeholder="Add a task... #Category"
-				autoComplete="off"
-				className="border rounded-lg rounded-b-none py-2 px-4 w-full drop-shadow-md focus:outline-none focus:border-slate-400 hover:border-slate-400"
-			/>
-			<div className="flex gap-10 px-4 py-2 bg-white rounded-lg rounded-t-none w-full border border-t-0 drop-shadow-md text-sm">
-				<input
-					{...register('date')}
-					type="date"
-					className="bg-transparent focus:outline-slate-400"
-					id="date"
-					aria-label="Due Date"
-				/>
-
-				<input
-					{...register('time')}
-					type="time"
-					step="1"
-					className="bg-transparent focus:outline-slate-400"
-					id="time"
-					aria-label="Due Time"
-				/>
-			</div>
+			<TodoInput register={register} />
+			<TodoDateTimeInputs register={register} />
 		</form>
 	);
 }
+
+const TodoInput = ({ register }: { register: any }) => (
+	<input
+		{...register('todo', {
+			required: true,
+			maxLength: { value: 100, message: 'Exceeded maximum length of 100' },
+		})}
+		type="text"
+		placeholder="Add a task... #Category"
+		autoComplete="off"
+		className="border rounded-lg rounded-b-none py-2 px-4 w-full drop-shadow-md focus:outline-none focus:border-slate-400 hover:border-slate-400"
+	/>
+);
+
+const TodoDateTimeInputs = ({ register }: { register: any }) => (
+	<div className="flex gap-10 px-4 py-2 bg-white rounded-lg rounded-t-none w-full border border-t-0 drop-shadow-md text-sm">
+		<input
+			{...register('date')}
+			type="date"
+			className="bg-transparent focus:outline-slate-400"
+			id="date"
+			aria-label="Due Date"
+		/>
+
+		<input
+			{...register('time')}
+			type="time"
+			step="1"
+			className="bg-transparent focus:outline-slate-400"
+			id="time"
+			aria-label="Due Time"
+		/>
+	</div>
+);
 
 export default memo(TodoForm);
