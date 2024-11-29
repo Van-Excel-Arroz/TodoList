3c90d1008bd37bb2d666292aa2d4fccaaeed2855
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-29 09:23:09 +0800

    commit

--- src/components/TodoList/TodoForm.tsx
+++ src/components/TodoList/TodoForm.tsx
@@ -4,7 +4,6 @@ import { useForm } from 'react-hook-form';
 import { createTodoAction } from '@/actions/todolist-action';
 import { extractCategory, extractTitle } from '@/utils/category';
 import { memo } from 'react';
-import { Calendar, Timer } from 'lucide-react';
 
 interface TodoFormProps {
 	todolistId: number;
@@ -58,20 +57,23 @@ function TodoForm({ todolistId }: TodoFormProps) {
 				autoComplete="off"
 				className="border rounded-lg rounded-b-none py-2 px-4 w-full drop-shadow-md focus:outline-none focus:border-slate-400 hover:border-slate-400"
 			/>
-			<div className="flex gap-2 px-4 py-2 bg-white rounded-lg rounded-t-none w-full border border-t-0 text-gray-400 drop-shadow-md ">
-				<Calendar size={25} className="cursor-pointer hover:bg-slate-200 rounded-sm p-1" />
-				<Timer size={25} className="cursor-pointer hover:bg-slate-200 rounded-sm p-1" />
-			</div>
+			<div className="flex gap-10 px-4 py-2 bg-white rounded-lg rounded-t-none w-full border border-t-0 drop-shadow-md text-sm">
+				<input
+					{...register('date')}
+					type="date"
+					className="bg-transparent focus:outline-slate-400"
+					id="date"
+					aria-label="Due Date"
+				/>
 
-			<div className="flex gap-10">
-				<label htmlFor="date" className="sr-only">
-					Due Date
-				</label>
-				<input {...register('date')} type="date" className="bg-transparent" id="date" />
-				<label htmlFor="time" className="sr-only">
-					Due Time
-				</label>
-				<input {...register('time')} type="time" step="1" className="bg-transparent" id="time" />
+				<input
+					{...register('time')}
+					type="time"
+					step="1"
+					className="bg-transparent focus:outline-slate-400"
+					id="time"
+					aria-label="Due Time"
+				/>
 			</div>
 		</form>
 	);
