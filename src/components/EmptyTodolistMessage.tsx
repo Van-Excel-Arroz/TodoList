ee48f4be2c6698b98d4b38dec8737c84fcbb7265
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-29 10:37:02 +0800

    commit

--- src/components/EmptyTodolistMessage.tsx
+++ src/components/EmptyTodolistMessage.tsx
@@ -2,10 +2,10 @@ import { ClipboardList } from 'lucide-react';
 
 export default function EmptyTodolistMessage() {
 	return (
-		<div className="my-10 flex flex-col items-center justify-center">
+		<div className="flex flex-col items-center justify-center h-full">
 			<ClipboardList size={48} className="text-gray-400" />
 			<div className="text-center">
-				<p className="text-2xl font-semibold">No todolist selected</p>
+				<p className="mt-4 text-2xl font-semibold">No todolist selected</p>
 				<p className="mt-2 text-gray-600">
 					Create a new todolist or select an exisiting one from the sidebar to get started.
 				</p>
