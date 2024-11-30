4328a05fc7d255870570726265b9d5f64b20b3ad
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-11-30 17:26:11 +0800

    Enhance SidebarHeader component: restructure layout to include a timestamp display with current time and date

--- src/components/leftSidebar/SidebarHeader.tsx
+++ src/components/leftSidebar/SidebarHeader.tsx
@@ -3,17 +3,25 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { House } from 'lucide-react';
+import { format } from 'date-fns';
 import SidebarToggle from '../SidebarToggle';
 
 export default function SidebarHeader() {
 	const pathname = usePathname();
 	const isHome = pathname === '/tasks/home';
 	return (
-		<div className="flex gap-4 items-center">
-			{!isHome && <SidebarToggle />}
-			<Link href="/" className="text-lg font-bold" aria-label="home">
-				<House />
-			</Link>
+		<div className="flex justify-between">
+			<div className="flex gap-4 items-center">
+				{!isHome && <SidebarToggle />}
+				<Link href="/" className="text-lg font-bold" aria-label="home">
+					<House />
+				</Link>
+			</div>
+
+			<div className="flex flex-col">
+				<div className="text-xl flex justify-end font-semibold">{format(new Date(), `h:mm a`)} </div>
+				<div className="text-sm flex justify-end">{format(new Date(), `EEEE, MMMM d`)}</div>
+			</div>
 		</div>
 	);
 }
