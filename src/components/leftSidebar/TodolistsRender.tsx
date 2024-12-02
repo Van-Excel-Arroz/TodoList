0a4b64e313d600dfdc78787e208f9b8572a7aa11
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-02 17:19:32 +0800

    Update TodolistsRender component: add margin-bottom to the list for improved spacing

--- src/components/leftSidebar/TodolistsRender.tsx
+++ src/components/leftSidebar/TodolistsRender.tsx
@@ -4,7 +4,7 @@ import TodoListItem from './TodoListItem';
 export default function TodolistsRender({ todolists }: { todolists: TodoList[] }) {
 	return (
 		<div className="h-[calc(100%-90px)] overflow-y-auto overflow-x-hidden">
-			<ul className="container mx-auto flex flex-col gap-2">
+			<ul className="container mx-auto flex flex-col gap-2 mb-4">
 				{todolists.map(todolist => (
 					<li key={todolist.id}>
 						<TodoListItem key={todolist.id} todolist={todolist} />
