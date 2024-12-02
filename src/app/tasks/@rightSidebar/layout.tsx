9a7fbfd22c2086f8ee202661d773bb6734f0ad10
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-02 10:58:16 +0800

    Refactor RightSidebarLayout to improve todo retrieval: update to use getTodo function, add error handling for missing todos, and adjust props to include taskId. This enhances the component's robustness and user experience by providing clear feedback when a todo is not found.

--- src/app/tasks/@rightSidebar/layout.tsx
+++ src/app/tasks/@rightSidebar/layout.tsx
@@ -1,15 +1,26 @@
 import RightSidebarPage from '@/components/rightSidebar/RightSidebarPage';
-import { getTodoWithCategories } from '@/lib/todo';
+import { getTodo } from '@/lib/todo';
 
 interface RightSidebarLayoutProps {
+	params: {
+		taskId: string;
+	};
 	searchParams: {
 		[key: string]: string | string[] | undefined;
 	};
 }
 
-export default async function RightSidebarLayout({ searchParams }: RightSidebarLayoutProps) {
+export default async function RightSidebarLayout({ params, searchParams }: RightSidebarLayoutProps) {
+	console.log(searchParams);
+	if (!searchParams || !searchParams.todo) {
+		return <div>Todo Not Found</div>;
+	}
 	const todoId = Number(searchParams.todo);
-	const todo = await getTodoWithCategories(todoId);
+	const todo = await getTodo(todoId);
+
+	if (!todo) {
+		return <div>Todo Not Found</div>;
+	}
 
 	return <RightSidebarPage todo={todo} />;
 }
