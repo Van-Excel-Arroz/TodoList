a4fc962280f7229e28d0f63d7dbf35f7e1628bed
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-02 17:47:48 +0800

    Refactor LeftSidebarWrapper and RightSidebarPage components: remove unnecessary padding classes for cleaner layout and maintain consistent sidebar behavior.

--- src/components/rightSidebar/RightSidebarPage.tsx
+++ src/components/rightSidebar/RightSidebarPage.tsx
@@ -10,7 +10,7 @@ export default function RightSidebarPage() {
 
 	return (
 		<div
-			className={`font-body border-l-2 rounded-l-3xl drop-shadow-xl bg-white pb-9 pt-6 h-screen transition-all duration-200 ease-in-out text-nowrap overflow-hidden ${
+			className={`border-l-2 rounded-l-3xl bg-white h-screen transition-all duration-200 ease-in-out text-nowrap overflow-hidden ${
 				isRightSidebarOpen ? 'w-72' : 'w-0'
 			}`}
 		>
