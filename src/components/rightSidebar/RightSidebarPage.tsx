22887509d84cefff79cb3dc69c67a7c85bfd6682
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-03 13:06:49 +0800

    Enhance RightSidebarPage component: integrate checkbox for todo completion, update selectedTodo state on checkbox change, and improve layout with flexbox for better visual presentation.

--- src/components/rightSidebar/RightSidebarPage.tsx
+++ src/components/rightSidebar/RightSidebarPage.tsx
@@ -3,10 +3,21 @@
 import RightSidebarHeader from './RightSidebarHeader';
 import useTodoStore from '@/context/todoContext';
 import useRightSidebarStore from '@/context/RightSidebarContext';
+import { CheckBox } from '../main/TodoItem';
+import { updateTodoCompletionAction } from '@/actions/todolist-action';
 
 export default function RightSidebarPage() {
-	const { selectedTodo } = useTodoStore();
+	const { selectedTodo, setSelectedTodo } = useTodoStore();
 	const { isRightSidebarOpen } = useRightSidebarStore();
+
+	const handleCheckboxChange = async () => {
+		if (!selectedTodo) return;
+		await updateTodoCompletionAction(selectedTodo.id, !selectedTodo.is_completed, selectedTodo.todo_list_id);
+		setSelectedTodo({
+			...selectedTodo,
+			is_completed: !selectedTodo.is_completed,
+		});
+	};
 
 	return (
 		<div
@@ -14,8 +25,13 @@ export default function RightSidebarPage() {
 				isRightSidebarOpen ? 'w-80' : 'w-0'
 			}`}
 		>
-			<RightSidebarHeader />
-			<p>{selectedTodo?.task_text}</p>
+			<div className="px-6">
+				<RightSidebarHeader />
+				<div className="flex items-center gap-2">
+					<CheckBox isChecked={selectedTodo?.is_completed!} handleOnClick={handleCheckboxChange} />
+					<p className="text-lg">{selectedTodo?.task_text}</p>
+				</div>
+			</div>
 		</div>
 	);
 }
