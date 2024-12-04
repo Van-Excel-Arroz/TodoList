f1566f39d8f556d02f762ce017afecb4863d24a3
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-04 09:25:40 +0800

    Refactor RightSidebarPage: extract TodoDueDate component

--- src/components/rightSidebar/RightSidebarPage.tsx
+++ src/components/rightSidebar/RightSidebarPage.tsx
@@ -33,17 +33,7 @@ export default function RightSidebarPage() {
 					isCompleted={selectedTodo?.is_completed ?? false}
 					handleCheckboxChange={handleCheckboxChange}
 				/>
-				<div className="flex items-center gap-4 bg-slate-100 rounded-md px-4 py-2 border">
-					<Calendar size={20} className="text-slate-800" />
-					<div className="flex flex-col justify-start">
-						<p className="text-sm text-slate-600">Due</p>
-						{selectedTodo?.due_datetime ? (
-							<DueDate dueDatetime={selectedTodo?.due_datetime ?? ''} textSize="base" />
-						) : (
-							<p className="text-slate-600">MM/DD/YYYY</p>
-						)}
-					</div>
-				</div>
+				<TodoDueDate dueDatetime={selectedTodo?.due_datetime ?? ''} />
 				<div className="flex flex-col items-start bg-slate-100 rounded-md px-4 py-2 border">
 					<div className="flex justify-between items-center w-full text-slate-600">
 						<p className="text-sm">Category</p>
@@ -82,5 +72,19 @@ const TodoTitle = ({
 	<div className="flex items-center gap-4 bg-slate-100 rounded-md p-4 border">
 		<CheckBox isChecked={isCompleted} handleOnClick={handleCheckboxChange} />
 		<p className="text-lg overflow-hidden text-wrap">{title}</p>
+	</div>
+);
+
+const TodoDueDate = ({ dueDatetime }: { dueDatetime: string }) => (
+	<div className="flex items-center gap-4 bg-slate-100 rounded-md px-4 py-2 border">
+		<Calendar size={20} className="text-slate-800" />
+		<div className="flex flex-col justify-start">
+			<p className="text-sm text-slate-600">Due</p>
+			{dueDatetime ? (
+				<DueDate dueDatetime={dueDatetime ?? ''} textSize="base" />
+			) : (
+				<p className="text-slate-600">MM/DD/YYYY</p>
+			)}
+		</div>
 	</div>
 );
