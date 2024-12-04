d0ce0135debef7dd8c0d9e60b8ac93ac877bd0d1
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-04 16:35:50 +0800

    Enhance TodoCategories component: integrate react-hook-form for category submission and improve form structure

--- src/components/rightSidebar/TodoCategories.tsx
+++ src/components/rightSidebar/TodoCategories.tsx
@@ -3,23 +3,35 @@
 import { Plus, SendHorizontal, X } from 'lucide-react';
 import { Category } from '@/types';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+interface CategoryFormInputs {
+	category_title: string;
+	hex_color: string;
+}
 
 export default function TodoCategories({ categories }: { categories: Category[] }) {
 	const [isAddingCategory, setIsAddingCategory] = useState(false);
+	const { register, handleSubmit, reset } = useForm<CategoryFormInputs>();
 
 	const handleAddCategory = (val: boolean) => {
 		setIsAddingCategory(val);
+	};
+
+	const onSubmit = async (data: CategoryFormInputs) => {
+		console.log(data);
 	};
 	return (
 		<div className="flex flex-col items-start bg-slate-100 rounded-md px-4 py-2 border">
 			<div className="flex justify-between items-center w-full text-slate-600">
 				<p className="text-sm">Categories</p>
 				{isAddingCategory ? (
-					<div className="flex items-center gap-2 justify-end">
-						<input type="color" className="w-5 h-5 cursor-pointer" defaultValue="#000000" />
+					<form onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-2 justify-end">
+						<input type="color" className="w-5 h-5 cursor-pointer" defaultValue="#000000" {...register('hex_color')} />
 						<input
 							type="text"
 							className="border rounded-md w-1/2 p-1 text-sm focus:outline-none focus:border-slate-400 hover:border-slate-400"
+							{...register('category_title', { maxLength: 20 })}
 						/>
 						<button className="hover:bg-slate-200 rounded-md p-1" aria-label="Add Category">
 							<SendHorizontal size={20} />
@@ -31,7 +43,7 @@ export default function TodoCategories({ categories }: { categories: Category[] 
 						>
 							<X size={20} />
 						</button>
-					</div>
+					</form>
 				) : (
 					<button
 						className="block hover:bg-slate-200 rounded-md p-1"
