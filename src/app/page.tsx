59b9ba170b7f13f2342d608c21a21012ea3a6614
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-04 19:54:12 +0800

    Refactor page layout and styling: update welcome message size, container width, and link styling for improved responsiveness and user experience in the main page and home page components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 export default function page() {
 	return (
 		<>
-			<div className="container mx-auto h-screen flex flex-col justify-center items-center">
-				<p className="text-9xl mb-5 align-top">Welcome</p>
+			<div className="container mx-auto w-5/6 h-screen flex flex-col justify-center items-center">
+				<p className="text-7xl md:text-9xl mb-5 align-top">Welcome</p>
 				<Link
 					href={'/tasks/home'}
-					className="block bg-black text-white text-center text-3xl px-14 py-7 mt-5 rounded-2xl"
+					className="block bg-black text-white text-center text-xl md:text-3xl px-7 py-4 mt-5 rounded-2xl"
 				>
 					Create Your Tasks!
 				</Link>
--- src/app/tasks/@main/home/page.tsx
+++ src/app/tasks/@main/home/page.tsx
@@ -1,4 +1,4 @@
-import NoTodoListSelected from '@/components/NoTodolistSelected';
+import NoTodoListSelected from '@/components/NoTodoListSelected';
 
 export default function HomePage() {
 	return <NoTodoListSelected />;
