aaef21e523ca8c1128915fc8b260627d39e887e0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-04 10:19:12 +0800

    Enhance TodoCategories component: add category input field and toggle functionality for adding new categories

--- src/components/rightSidebar/TodoCategories.tsx
+++ src/components/rightSidebar/TodoCategories.tsx
@@ -1,16 +1,36 @@
 'use client';
 
-import { Plus } from 'lucide-react';
+import { Plus, Send, SendHorizontal, X } from 'lucide-react';
 import { Category } from '@/types';
+import { useState } from 'react';
 
 export default function TodoCategories({ categories }: { categories: Category[] }) {
+	const [isAddingCategory, setIsAddingCategory] = useState(false);
+
+	const handleAddCategory = (val: boolean) => {
+		setIsAddingCategory(val);
+	};
 	return (
 		<div className="flex flex-col items-start bg-slate-100 rounded-md px-4 py-2 border">
 			<div className="flex justify-between items-center w-full text-slate-600">
 				<p className="text-sm">Categories</p>
-				<button className="block hover:bg-slate-200 rounded-md p-1" aria-label="Add Category">
-					<Plus size={20} />
-				</button>
+				{isAddingCategory ? (
+					<div className="flex items-center gap-2 justify-end">
+						<input type="text" className="border rounded-md w-1/2" />
+						<SendHorizontal size={20} />
+						<button onClick={() => handleAddCategory(false)}>
+							<X size={20} />
+						</button>
+					</div>
+				) : (
+					<button
+						className="block hover:bg-slate-200 rounded-md p-1"
+						aria-label="Add Category"
+						onClick={() => handleAddCategory(true)}
+					>
+						<Plus size={20} />
+					</button>
+				)}
 			</div>
 			<div className={`flex flex-wrap items-cente gap-2 ${categories.length === 0 ? 'py-0' : 'py-2'}`}>
 				{categories.map(category => (
