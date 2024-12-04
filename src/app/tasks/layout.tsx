5bc046f53b195057b49d84bb5d939944b9360f4b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-04 19:11:02 +0800

    Refactor layout component: update left sidebar styling for improved positioning and responsiveness, ensuring consistent layout behavior across different screen sizes.

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ main, leftSidebar, rightSidebar }: LayoutProps)
 		<>
 			<Navbar />
 			<div className="flex relative h-screen bg-slate-100">
-				<div className="lg:sticky md:absolute sm:absolute xs:absolute top-0 left-0 z-30">{leftSidebar}</div>
+				<div className="absolute lg:sticky top-0 left-0 z-30">{leftSidebar}</div>
 				<div className="w-full overflow-y-auto">{main}</div>
 				<div>{rightSidebar}</div>
 			</div>
