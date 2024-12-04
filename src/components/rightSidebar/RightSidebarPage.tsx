37a2978256d21e44676e89cd3db9abb9e7f33399
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-04 09:28:38 +0800

    Refactor RightSidebarPage: extract TodoCategories component

--- src/components/rightSidebar/RightSidebarPage.tsx
+++ src/components/rightSidebar/RightSidebarPage.tsx
@@ -6,6 +6,7 @@ import useTodoStore from '@/context/todoContext';
 import useRightSidebarStore from '@/context/RightSidebarContext';
 import RightSidebarHeader from './RightSidebarHeader';
 import { CheckBox, DueDate } from '../main/TodoItem';
+import { Category } from '@/types';
 
 export default function RightSidebarPage() {
 	const { selectedTodo, setSelectedTodo } = useTodoStore();
@@ -34,27 +35,7 @@ export default function RightSidebarPage() {
 					handleCheckboxChange={handleCheckboxChange}
 				/>
 				<TodoDueDate dueDatetime={selectedTodo?.due_datetime ?? ''} />
-				<div className="flex flex-col items-start bg-slate-100 rounded-md px-4 py-2 border">
-					<div className="flex justify-between items-center w-full text-slate-600">
-						<p className="text-sm">Category</p>
-						<button className="block hover:bg-slate-200 rounded-md p-1" aria-label="Add Category">
-							<Plus size={20} />
-						</button>
-					</div>
-					<div
-						className={`flex flex-wrap items-center  gap-2 ${selectedTodo?.categories?.length === 0 ? 'py-0' : 'py-2'}`}
-					>
-						{selectedTodo?.categories?.map(category => (
-							<span
-								key={category.id}
-								className={`border rounded py-1 px-2 shadow-md hover:bg-slate-10`}
-								style={{ color: category.hex_color }}
-							>
-								{category.category_title}
-							</span>
-						))}
-					</div>
-				</div>
+				<TodoCategories categories={selectedTodo?.categories ?? []} />
 			</div>
 		</div>
 	);
@@ -85,6 +66,28 @@ const TodoDueDate = ({ dueDatetime }: { dueDatetime: string }) => (
 			) : (
 				<p className="text-slate-600">MM/DD/YYYY</p>
 			)}
+		</div>
+	</div>
+);
+
+const TodoCategories = ({ categories }: { categories: Category[] }) => (
+	<div className="flex flex-col items-start bg-slate-100 rounded-md px-4 py-2 border">
+		<div className="flex justify-between items-center w-full text-slate-600">
+			<p className="text-sm">Category</p>
+			<button className="block hover:bg-slate-200 rounded-md p-1" aria-label="Add Category">
+				<Plus size={20} />
+			</button>
+		</div>
+		<div className={`flex flex-wrap items-center  gap-2 ${categories.length === 0 ? 'py-0' : 'py-2'}`}>
+			{categories.map(category => (
+				<span
+					key={category.id}
+					className={`border rounded py-1 px-2 shadow-md hover:bg-slate-10`}
+					style={{ color: category.hex_color }}
+				>
+					{category.category_title}
+				</span>
+			))}
 		</div>
 	</div>
 );
