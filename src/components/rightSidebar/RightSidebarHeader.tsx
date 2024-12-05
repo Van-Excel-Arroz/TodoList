befec2a4f2e5115f78e80ed381b2b63af3a0d80a
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-05 17:35:47 +0800

    Refactor RightSidebarHeader component: adjust layout by centering the title and adding spacing

--- src/components/rightSidebar/RightSidebarHeader.tsx
+++ src/components/rightSidebar/RightSidebarHeader.tsx
@@ -13,11 +13,12 @@ export default function RightSidebarHeader() {
 		setSelectedTodo(null);
 	};
 	return (
-		<div className="flex items-center justify-between mt-1">
+		<div className="flex items-center mt-1">
 			<button onClick={handleCloseRightSidebar} aria-label="Close Todo Menu">
 				<ArrowBigRightDashIcon />
 			</button>
-			<p className="text-xl font-medium">Details</p>
+			<p className="flex-1 text-xl font-medium text-center px-6">Details</p>
+			<div className="flex-none w-[24px]"></div>
 		</div>
 	);
 }
