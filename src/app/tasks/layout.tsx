c3070e24bae696f46e4d01d15c6a863b93df35f6
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-05 10:11:12 +0800

    Refactor layout and Navbar components: restructure layout for improved sidebar positioning and enhance Navbar with backdrop blur and opacity effects for better visual integration.

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -9,11 +9,13 @@ interface LayoutProps {
 export default function Layout({ main, leftSidebar, rightSidebar }: LayoutProps) {
 	return (
 		<>
-			<Navbar />
-			<div className="flex relative h-screen bg-slate-100">
-				<div className="absolute lg:relative top-0 left-0 z-30">{leftSidebar}</div>
-				<div className="w-full overflow-y-auto">{main}</div>
-				<div className="absolute lg:relative top-0 right-0 z-40">{rightSidebar}</div>
+			<div className="flex flex-col bg-slate-100">
+				<Navbar />
+				<div className="flex relative h-screen">
+					<div className="absolute lg:relative top-0 left-0 z-30">{leftSidebar}</div>
+					<div className="w-full overflow-y-auto">{main}</div>
+					<div className="absolute lg:relative top-0 right-0 z-40">{rightSidebar}</div>
+				</div>
 			</div>
 		</>
 	);
