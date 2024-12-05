7421c06a2e73ff79f0e956676ba2f0bccec29637
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-05 10:25:36 +0800

    Refactor layout and Navbar components: correct background color in layout and enhance Navbar with backdrop blur and opacity effects for improved visual hierarchy and user experience.

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -9,11 +9,11 @@ interface LayoutProps {
 export default function Layout({ main, leftSidebar, rightSidebar }: LayoutProps) {
 	return (
 		<>
-			<div className="flex flex-col bg-slate-100">
+			<div className="flex flex-col bg-slates-100">
 				<Navbar />
 				<div className="flex relative h-screen">
 					<div className="absolute lg:relative top-0 left-0 z-30">{leftSidebar}</div>
-					<div className="w-full overflow-y-auto">{main}</div>
+					<div className="w-full overflow-y-auto z-10">{main}</div>
 					<div className="absolute lg:relative top-0 right-0 z-40">{rightSidebar}</div>
 				</div>
 			</div>
