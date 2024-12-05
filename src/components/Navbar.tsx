644b54758bd0000a113013e6a9dc7081aef484cf
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-05 09:43:22 +0800

    Enhance Navbar component: integrate right sidebar functionality by closing it when the home link is clicked, improving user interaction and navigation experience.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
+'use client';
+
+import useRightSidebarStore from '@/context/RightSidebarContext';
 import { House } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Navbar() {
+	const { closeRightSidebar } = useRightSidebarStore();
 	return (
 		<div className=" border-b-2 border-b-slate-200 drop-shadow-md bg-white h-14 px-6 flex items-center gap-4">
-			<Link href="/" className="text-lg font-bold" aria-label="home">
+			<Link
+				href="/"
+				className="text-lg font-bold"
+				aria-label="home"
+				onClick={() => {
+					closeRightSidebar();
+				}}
+			>
 				<House />
 			</Link>
 			<p>TodoList App</p>
