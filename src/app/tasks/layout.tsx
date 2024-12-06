de486c145da3690d224a8af0971b3fc34bcd1c5f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-06 16:46:59 +0800

    Refactor Layout and Navbar components for improved structure and styling. Updated Layout to include sticky positioning for the Navbar and adjusted sidebar elements for better layout control. Enhanced Navbar styling by simplifying the background and maintaining consistent visibility.

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -9,14 +9,16 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
 	return (
 		<>
-			<body className="flex flex-col bg-slates-100 bg-slate-100 overflow-hidden">
-				<Navbar />
-				<div className="flex relative h-screen">
+			<body className="flex flex-col bg-slates-100 overflow-hidden">
+				<div className="sticky top-0 left-0 z-20 w-full">
+					<Navbar />
+				</div>
+				<div className="flex h-screen">
 					<div className="absolute lg:sticky top-0 left-0 z-30">
 						<LeftSidebarPage />
 					</div>
 					<main className="w-full overflow-y-auto z-10">{children}</main>
-					<div className="absolute lg:relative top-0 right-0 z-40">
+					<div className="absolute lg:sticky top-0 right-0 z-40">
 						<RightSidebarPage />
 					</div>
 				</div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 export default function Navbar() {
 	const { closeRightSidebar } = useRightSidebarStore();
 	return (
-		<nav className="z-20 bg-white border-b-2 border-b-slate-200 drop-shadow-md h-14 px-6 flex items-center gap-4 sticky top-0 w-full">
+		<nav className="bg-white/20 backdrop-blur-sm border-b-2 border-b-slate-200 drop-shadow-md h-14 px-6 flex items-center gap-4">
 			<Link
 				href="/"
 				className="text-lg font-bold"
