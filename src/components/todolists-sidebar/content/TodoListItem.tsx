712294f7397d62f9e19c1a0dec7b088fbfc104ca
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-07 17:28:01 +0800

    Refactor EditTodolistForm: move EditTodolistForm component from a separate file to TodoListItem component for improved organization and maintainability. This change consolidates related functionality and enhances code clarity within the TodoListItem component.

--- src/components/todolists-sidebar/content/TodoListItem.tsx
+++ src/components/todolists-sidebar/content/TodoListItem.tsx
@@ -6,9 +6,10 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { deleteTodolistAction } from '@/actions/todolist-action';
 import { TodoList } from '@/types';
 import { CircleX, Pencil, Trash2 } from 'lucide-react';
-import EditTodolistForm from './EditTodolistForm';
 import useTodoStore from '@/context/TodoContext';
 import useTodoDetailsPanelStore from '@/context/TodoDetailsPanelContext';
+import { updateTodolistAction } from '@/actions/todolist-action';
+import { useForm } from 'react-hook-form';
 
 interface TodoListItemProps {
 	todolist: TodoList;
@@ -122,6 +123,49 @@ const DeleteButton = ({ todolistId, isActive }: { todolistId: number; isActive: 
 			>
 				<Trash2 size={15} />
 			</button>
+		</form>
+	);
+};
+
+interface FormInputs {
+	title: string;
+}
+
+interface EditTodolistFormProps {
+	todolist: TodoList;
+	handleEditClick: (val: boolean) => void;
+}
+
+const EditTodolistForm = ({ todolist, handleEditClick }: EditTodolistFormProps) => {
+	const { register, handleSubmit, reset } = useForm<FormInputs>({
+		defaultValues: {
+			title: todolist.title,
+		},
+	});
+
+	const onSubmit = async (data: FormInputs) => {
+		if (todolist.title !== data.title) {
+			await updateTodolistAction(todolist.id, data.title);
+		}
+		handleEditClick(false);
+		reset();
+	};
+
+	const handleInputBlur = () => {
+		handleEditClick(false);
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<input
+				{...register('title')}
+				type="text"
+				placeholder={todolist.title}
+				className="bg-transparent focus:outline-none border-b border-slate-950 my-3 mx-5 w-full"
+				autoFocus
+				onBlur={handleInputBlur}
+				defaultValue={todolist.title}
+			/>
 		</form>
 	);
 };
