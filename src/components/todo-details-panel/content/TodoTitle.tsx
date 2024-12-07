55b80356defa870347664d50aa254345249144df
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-07 16:08:28 +0800

    Enhance TodoTitle component: add editing functionality with a pencil icon and improve layout for better user interaction and readability.

--- src/components/todo-details-panel/content/TodoTitle.tsx
+++ src/components/todo-details-panel/content/TodoTitle.tsx
@@ -3,6 +3,8 @@
 import { updateTodoCompletionAction } from '@/actions/todolist-action';
 import { CheckBox } from '@/components/todos/content/TodoItem';
 import useTodoStore from '@/context/TodoContext';
+import { Pencil } from 'lucide-react';
+import { useState } from 'react';
 
 interface TodoTitleProps {
 	title: string;
@@ -11,6 +13,8 @@ interface TodoTitleProps {
 
 export default function TodoTitle({ title, isCompleted }: TodoTitleProps) {
 	const { selectedTodo, setSelectedTodo } = useTodoStore();
+	const [isEditing, setIsEditing] = useState(false);
+
 	const handleCheckboxChange = async () => {
 		if (!selectedTodo) return;
 		await updateTodoCompletionAction(selectedTodo.id, !selectedTodo.is_completed, selectedTodo.todo_list_id);
@@ -20,10 +24,21 @@ export default function TodoTitle({ title, isCompleted }: TodoTitleProps) {
 		});
 	};
 
+	const handleEditClick = (val: boolean) => {
+		setIsEditing(val);
+	};
 	return (
-		<div className="flex items-center gap-4 bg-slate-100 rounded-md p-4 border border-slate-300">
-			<CheckBox isChecked={isCompleted} handleOnClick={handleCheckboxChange} />
-			<p className="text-lg overflow-hidden text-wrap">{title}</p>
+		<div className="flex justify-between bg-slate-100 rounded-md p-4 border border-slate-300">
+			<div className="flex items-center gap-4">
+				<CheckBox isChecked={isCompleted} handleOnClick={handleCheckboxChange} />
+				<p className="text-lg overflow-hidden text-wrap break-all">{title}</p>
+			</div>
+			<button
+				className="hover:bg-slate-200 active:bg-slate-200 rounded-md p-1 text-slate-600"
+				onClick={() => handleEditClick(true)}
+			>
+				<Pencil size={18} />
+			</button>
 		</div>
 	);
 }
