63bc7b99c15c028d7ae25a6248f7aaa93fdc4c7c
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-08 10:40:31 +0800

    Refactor SelectedCategories and TodoSection components: - Add Filter icon to SelectedCategories for improved visual context. - Adjust layout in SelectedCategories to enhance spacing and organization. - Update margin in TodoSection for consistent styling. These changes improve user interface clarity and maintainability of the components.

--- src/components/todos/content/SelectedCategories.tsx
+++ src/components/todos/content/SelectedCategories.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { X } from 'lucide-react';
+import { Filter, X } from 'lucide-react';
 import { Category } from '@/types';
 import { updateIsSelectedCategoryColorsAction } from '@/actions/category-action';
 
@@ -16,30 +16,33 @@ export default function SelectedCategories({ selectedCategories, todoListId }: S
 
 	return (
 		<>
-			{selectedCategories.length > 0 ? (
-				<div className="flex flex-row gap-2 pt-6">
-					{selectedCategories.map(selectedCategory => (
-						<span
-							key={selectedCategory.id}
-							className="rounded-md flex items-center gap-2 px-2 py-1"
-							style={{
-								color: selectedCategory.hex_color,
-								backgroundColor: `${selectedCategory.hex_color}20`,
-								border: `1px solid ${selectedCategory.hex_color}`,
-							}}
-						>
-							<p>{selectedCategory.category_title}</p>
-							<button
-								className={`hover:bg-white active:bg-white rounded-md p-1`}
-								onClick={() => handleCategoryClick(selectedCategory.category_title)}
-								aria-label={`Remove ${selectedCategory.category_title} category`}
+			<div className="flex items-center gap-2 my-6">
+				<Filter />
+				{selectedCategories.length > 0 ? (
+					<div className="flex flex-row gap-2">
+						{selectedCategories.map(selectedCategory => (
+							<span
+								key={selectedCategory.id}
+								className="rounded-md flex items-center gap-2 px-2 py-1"
+								style={{
+									color: selectedCategory.hex_color,
+									backgroundColor: `${selectedCategory.hex_color}20`,
+									border: `1px solid ${selectedCategory.hex_color}`,
+								}}
 							>
-								<X size={16} />
-							</button>
-						</span>
-					))}
-				</div>
-			) : null}
+								<p>{selectedCategory.category_title}</p>
+								<button
+									className={`hover:bg-white active:bg-white rounded-md p-1`}
+									onClick={() => handleCategoryClick(selectedCategory.category_title)}
+									aria-label={`Remove ${selectedCategory.category_title} category`}
+								>
+									<X size={16} />
+								</button>
+							</span>
+						))}
+					</div>
+				) : null}
+			</div>
 		</>
 	);
 }
