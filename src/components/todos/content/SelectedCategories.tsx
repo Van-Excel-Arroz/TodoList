e0ff74e7199feb87e74ce53952ceae4026caa255
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-08 10:49:41 +0800

    Refactor SelectedCategories component: - Replace the existing category display with a simplified layout that includes a "Categories" label and an ArrowDown icon for better visual clarity. - Remove the previous mapping of selected categories to streamline the component's structure and enhance maintainability. These changes improve the user interface and simplify the component's code.

--- src/components/todos/content/SelectedCategories.tsx
+++ src/components/todos/content/SelectedCategories.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Filter, X } from 'lucide-react';
+import { ArrowDown, Filter, X } from 'lucide-react';
 import { Category } from '@/types';
 import { updateIsSelectedCategoryColorsAction } from '@/actions/category-action';
 
@@ -18,30 +18,10 @@ export default function SelectedCategories({ selectedCategories, todoListId }: S
 		<>
 			<div className="flex items-center gap-2 my-6">
 				<Filter />
-				{selectedCategories.length > 0 ? (
-					<div className="flex flex-row gap-2">
-						{selectedCategories.map(selectedCategory => (
-							<span
-								key={selectedCategory.id}
-								className="rounded-md flex items-center gap-2 px-2 py-1"
-								style={{
-									color: selectedCategory.hex_color,
-									backgroundColor: `${selectedCategory.hex_color}20`,
-									border: `1px solid ${selectedCategory.hex_color}`,
-								}}
-							>
-								<p>{selectedCategory.category_title}</p>
-								<button
-									className={`hover:bg-white active:bg-white rounded-md p-1`}
-									onClick={() => handleCategoryClick(selectedCategory.category_title)}
-									aria-label={`Remove ${selectedCategory.category_title} category`}
-								>
-									<X size={16} />
-								</button>
-							</span>
-						))}
-					</div>
-				) : null}
+				<div className="flex items-center gap-2 bg-white border border-slate-300 px-2 py-1 rounded-m">
+					<p className="text-sm d">Categories</p>
+					<ArrowDown size={14} />
+				</div>
 			</div>
 		</>
 	);
