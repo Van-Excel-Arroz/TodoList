18a17007949c43045007f7f26c3616f85626e340
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-09 15:17:40 +0800

    Refactor SelectedCategories component to improve code organization and readability by extracting category rendering into a new SelectedCategoryTags subcomponent. Updated category display and button functionality for enhanced user interaction.

--- src/components/todos/content/SelectedCategories.tsx
+++ src/components/todos/content/SelectedCategories.tsx
@@ -23,29 +23,8 @@ export default function SelectedCategories({ selectedCategories, todoListId }: S
 			<div className="flex items-center gap-2 my-6">
 				<Filter />
 				<div className="flex items-center gap-2 bg-white border border-slate-300 px-2 py-1 rounded-md">
-					<p className="text-sm d">Categories</p>
-					<div className="flex gap-2">
-						{selectedCategories.map(selectedCategory => (
-							<span
-								key={selectedCategory.id}
-								className="rounded-md flex items-center gap-1 pl-1"
-								style={{
-									color: selectedCategory.hex_color,
-									backgroundColor: `${selectedCategory.hex_color}20`,
-									border: `1px solid ${selectedCategory.hex_color}70`,
-								}}
-							>
-								<p className="text-sm">{selectedCategory.category_title}</p>
-								<button
-									className=" active:bg-white rounded-md p-1 hover:bg-white hover:outline hover:outline-1"
-									onClick={() => handleCategoryClick(selectedCategory.category_title)}
-									aria-label={`Remove ${selectedCategory.category_title} category`}
-								>
-									<X size={12} />
-								</button>
-							</span>
-						))}
-					</div>
+					<p className="text-sm d">Categories:</p>
+					<SelectedCategoryTags categories={selectedCategories} handleCategoryClick={handleCategoryClick} />
 					<Button ariaLabel="Open Categories Dropdown Filter" onClick={() => setIsOpen(prev => !prev)}>
 						<ChevronRight size={14} />
 					</Button>
@@ -54,3 +33,39 @@ export default function SelectedCategories({ selectedCategories, todoListId }: S
 		</>
 	);
 }
+
+// ------------------------------------------------------------------------------------------------ //
+// COMPONENTS
+// ------------------------------------------------------------------------------------------------ //
+
+interface SelectedCategoryTagsProps {
+	categories: Category[];
+	handleCategoryClick: (categoryTitle: string) => Promise<void>;
+}
+
+const SelectedCategoryTags = ({ categories, handleCategoryClick }: SelectedCategoryTagsProps) => {
+	return (
+		<div className="flex gap-2">
+			{categories.map(category => (
+				<span
+					key={category.id}
+					className="rounded-md flex items-center gap-1 pl-1"
+					style={{
+						color: category.hex_color,
+						backgroundColor: `${category.hex_color}20`,
+						border: `1px solid ${category.hex_color}70`,
+					}}
+				>
+					<p className="text-sm">{category.category_title}</p>
+					<button
+						className=" active:bg-white rounded-md p-1 hover:bg-white hover:outline hover:outline-1"
+						onClick={() => handleCategoryClick(category.category_title)}
+						aria-label={`Remove ${category.category_title} category`}
+					>
+						<X size={12} />
+					</button>
+				</span>
+			))}
+		</div>
+	);
+};
