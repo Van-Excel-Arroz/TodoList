b2021ed63e7980d8c1b2beab807919e710821ae7
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-09 14:58:46 +0800

    Refactor SelectedCategories component to improve UI and functionality by updating button icons, adjusting styles for selected categories, and enhancing the dropdown toggle mechanism.

--- src/components/todos/content/SelectedCategories.tsx
+++ src/components/todos/content/SelectedCategories.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChevronDown, Filter, X } from 'lucide-react';
+import { ChevronDown, ChevronRight, Filter, X } from 'lucide-react';
 import { Category } from '@/types';
 import { updateIsSelectedCategoryColorsAction } from '@/actions/category-action';
 import { useState } from 'react';
@@ -24,34 +24,32 @@ export default function SelectedCategories({ selectedCategories, todoListId }: S
 				<Filter />
 				<div className="flex items-center gap-2 bg-white border border-slate-300 px-2 py-1 rounded-md">
 					<p className="text-sm d">Categories</p>
-					<Button ariaLabel="Open Categories Dropdown Filter" onClick={() => setIsOpen(prev => !prev)}>
-						<ChevronDown size={14} />
-					</Button>
-				</div>
-				{isOpen && (
-					<div className="flex flex-row gap-2">
+					<div className="flex gap-2">
 						{selectedCategories.map(selectedCategory => (
 							<span
 								key={selectedCategory.id}
-								className="rounded-md flex items-center gap-2 px-2 py-1"
+								className="rounded-md flex items-center gap-1 pl-1"
 								style={{
 									color: selectedCategory.hex_color,
 									backgroundColor: `${selectedCategory.hex_color}20`,
-									border: `1px solid ${selectedCategory.hex_color}`,
+									border: `1px solid ${selectedCategory.hex_color}70`,
 								}}
 							>
-								<p>{selectedCategory.category_title}</p>
+								<p className="text-sm">{selectedCategory.category_title}</p>
 								<button
 									className={`hover:bg-white active:bg-white rounded-md p-1`}
 									onClick={() => handleCategoryClick(selectedCategory.category_title)}
 									aria-label={`Remove ${selectedCategory.category_title} category`}
 								>
-									<X size={16} />
+									<X size={12} />
 								</button>
 							</span>
 						))}
 					</div>
-				)}
+					<Button ariaLabel="Open Categories Dropdown Filter" onClick={() => setIsOpen(prev => !prev)}>
+						<ChevronRight size={14} />
+					</Button>
+				</div>
 			</div>
 		</>
 	);
