9d22e4422aaf2ffe7c6567435d613bb51ed453fd
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-09 16:01:21 +0800

    Enhance SelectedCategories component by adding smooth transition effects for width changes, improving user interaction. Updated styles for better responsiveness and visual feedback during category selection.

--- src/components/todos/content/SelectedCategories.tsx
+++ src/components/todos/content/SelectedCategories.tsx
@@ -22,7 +22,11 @@ export default function SelectedCategories({ selectedCategories, todoListId }: S
 		<>
 			<div className="flex items-center gap-2 my-6">
 				<Filter />
-				<div className={`flex items-center gap-2 bg-white border border-slate-300 px-2 py-1 rounded-lg group w-fit`}>
+				<div
+					className={`flex items-center gap-2 bg-white border border-slate-300 px-2 py-1 rounded-lg group transition-all duration-1000 ease-in-out ${
+						isOpen ? 'w-auto' : 'w-fit '
+					}`}
+				>
 					<p className="text-sm">Categories:</p>
 					<div
 						className={`flex gap-2 transition-all duration-1000 ease-in-out ${
