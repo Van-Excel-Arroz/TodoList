1904d5b3c3ea8439d1f7314b2caf000bf90dfcae
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-10 18:12:42 +0800

    Refactor TodoListItem component styling to enhance visual feedback and user interaction. Updated hover states and removed unnecessary active background class for improved clarity and responsiveness.

--- src/components/todolists-sidebar/content/TodoListItem.tsx
+++ src/components/todolists-sidebar/content/TodoListItem.tsx
@@ -53,10 +53,10 @@ function TodoListItem({ todolist }: { todolist: TodoList }) {
 
 	return (
 		<div
-			className={`flex items-center pr-5 ml-5 group relative w-11/12 active:bg-sky-50 pl-4  ${
+			className={`flex items-center pr-5 ml-5 group relative w-11/12 pl-4  ${
 				isSelectedPath
 					? 'border-l-4  border-slate-400 bg-sky-100'
-					: 'border-l-4  border-slate-200 hover:border-slate-300'
+					: 'border-l-4  border-slate-200 hover:border-slate-300 hover:bg-sky-50 '
 			}`}
 			onBlur={handleInputBlur}
 			tabIndex={-1}
