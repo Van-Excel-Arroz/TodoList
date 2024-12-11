debaac4449524c861fc6b68b2929fb1614925bbd
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-11 19:41:25 +0800

    Refactor SelectedCategories component: Improved category count display by adjusting opacity for better visibility and user interaction. Simplified the structure of the component while maintaining functionality.

--- src/components/todos/content/SelectedCategories.tsx
+++ src/components/todos/content/SelectedCategories.tsx
@@ -19,11 +19,15 @@ export default function SelectedCategories({ selectedCategories, todoListId }: S
 		<>
 			<div className="flex items-center border-b-2 border-slate-200 gap-2 mt-3 py-2 px-2 w-full">
 				<Filter size={16} className="text-slate-600" />
-				<div className="flex items-center relative group cursor-pointer">
-					<p className="text-sm">Categories</p>
-					{isSelectedCategoriesEmpty && (
-						<p className="px-2 border scale-75 border-sky-300 bg-sky-100 rounded-full">{selectedCategories.length}</p>
-					)}
+				<div className="flex items-center relative group">
+					<p className="text-sm cursor-pointer">Categories</p>
+					<p
+						className={`px-2 border scale-75 border-sky-300 bg-sky-100 rounded-full opacity-0 ${
+							isSelectedCategoriesEmpty && 'opacity-100'
+						}`}
+					>
+						{selectedCategories.length}
+					</p>
 
 					{isSelectedCategoriesEmpty && (
 						<div className="z-50 absolute top-6 -left-8 hidden group-hover:block hover:block">
