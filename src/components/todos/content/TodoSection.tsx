c7b7cf29148fc0cf2a19a76f677fce0e07e6182c
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-11 10:00:19 +0800

    Refactor TodoSection component to improve button state management and enhance user interaction. Updated the disabled state logic for the button based on the todos list length, improving code readability. Added a conditional class for the icon rotation to provide visual feedback when the section is toggled, contributing to a more dynamic user experience.

--- src/components/todos/content/TodoSection.tsx
+++ src/components/todos/content/TodoSection.tsx
@@ -13,13 +13,15 @@ interface TodoSectionProps {
 
 export default function TodoSection({ title, todos }: TodoSectionProps) {
 	const [isOpen, setIsOpen] = useState(false);
-	let disabledButton = todos.length === 0;
+	let isTodosEmpty = todos.length === 0;
 
 	return (
 		<div className={`border-b-2 border-slate-200 overflow-hidden transition-all duration-200 ease-in-out`}>
 			<div className="flex items-center py-2">
-				<Button ariaLabel="Toggle Todo Section" onClick={() => setIsOpen(prev => !prev)} disabled={disabledButton}>
-					<ChevronDown size={20} />
+				<Button ariaLabel="Toggle Todo Section" onClick={() => setIsOpen(prev => !prev)} disabled={isTodosEmpty}>
+					<div className={`${isOpen && !isTodosEmpty && 'rotate-180'}`}>
+						<ChevronDown size={20} />
+					</div>
 				</Button>
 				<p className="pl-2 font-semibold">{title}</p>
 			</div>
