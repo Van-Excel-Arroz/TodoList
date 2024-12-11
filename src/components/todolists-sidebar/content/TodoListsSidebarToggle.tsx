23f7eae099ea7ca8dc4bdd6cff962cc17ede3c40
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-11 20:19:13 +0800

    Refactor TodoListsSidebarToggle component: Updated Menu icon to List icon for improved visual consistency in the sidebar toggle button. This change enhances the user interface by providing a clearer representation of the toggle action.

--- src/components/todolists-sidebar/content/TodoListsSidebarToggle.tsx
+++ src/components/todolists-sidebar/content/TodoListsSidebarToggle.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import useTodoListsSidebarStore from '@/context/TodoListsSidebarContext';
-import { ArrowBigLeftDashIcon, Menu } from 'lucide-react';
+import { ArrowBigLeftDashIcon, List } from 'lucide-react';
 
 export default function TodoListsSidebarToggle() {
 	const { toggleTodoListsSidebar, isTodoListsSidebarOpen } = useTodoListsSidebarStore();
 	return (
 		<button onClick={toggleTodoListsSidebar} aria-label="Todolist Menu">
-			{isTodoListsSidebarOpen ? <ArrowBigLeftDashIcon /> : <Menu />}
+			{isTodoListsSidebarOpen ? <ArrowBigLeftDashIcon /> : <List />}
 		</button>
 	);
 }
