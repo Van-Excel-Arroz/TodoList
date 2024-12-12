6fc01c48258e68e78e77e0d670950ebaf9e65133
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-12 09:56:29 +0800

    Added a state for the CategorisFilter

--- src/components/todos/content/TodoFilter.tsx
+++ src/components/todos/content/TodoFilter.tsx
@@ -3,6 +3,7 @@
 import { Filter, X } from 'lucide-react';
 import { Category } from '@/types';
 import { updateIsSelectedCategoryColorsAction } from '@/actions/category-action';
+import { useState } from 'react';
 
 interface SelectedCategoriesProps {
 	selectedCategories: Category[];
@@ -31,15 +32,17 @@ interface CategoriesFilterProps {
 
 const CategoriesFilter = ({ categories, todoListId }: CategoriesFilterProps) => {
 	const isCategoriesEmpty = categories.length > 0;
+	const [isOpen, setIsOpen] = useState(false);
+	console.log(isOpen);
 
 	const handleCategoryClick = async (categoryTitle: string) => {
 		await updateIsSelectedCategoryColorsAction(false, categoryTitle, todoListId);
 	};
 
 	return (
-		<div className="flex items-center relative group">
-			<div className="flex items-center">
-				<p className="text-sm cursor-pointer">Categories</p>
+		<div className="flex items-center relative group hover:bg-slate-100 p-1 rounded-md">
+			<div className="flex items-center  cursor-pointer" onClick={() => setIsOpen(true)}>
+				<p className="text-sm">Categories</p>
 				<p
 					className={`px-2 border scale-75 border-sky-300 bg-sky-100 rounded-full opacity-0 ${
 						isCategoriesEmpty && 'opacity-100'
@@ -50,8 +53,15 @@ const CategoriesFilter = ({ categories, todoListId }: CategoriesFilterProps) => 
 			</div>
 
 			{isCategoriesEmpty && (
-				<div className={`z-50 absolute top-6 -left-8 hidden group-hover:block`}>
-					<div className="w-[50vw] lg:w-[40vw] flex flex-wrap items-start p-4 gap-2 drop-shadow-md rounded-lg bg-white border border-slate-300">
+				<div className={`z-10 absolute top-8 -left-8 ${isOpen ? 'block' : 'hidden'} group-hover:block`}>
+					<div className="w-[50vw] lg:w-[40vw] flex flex-wrap items-start px-4 py-6 gap-2 drop-shadow-md rounded-lg bg-white border border-slate-300 relative">
+						<button
+							className="absolute right-3 top-1 p-1 hover:bg-slate-200 rounded-md"
+							onClick={() => setIsOpen(false)}
+							aria-label="Close Selected Categories Filter"
+						>
+							<X size={18} />
+						</button>
 						{categories.map(category => (
 							<span
 								key={category.id}
