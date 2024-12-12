8434232565abd4388f29269ec4c28ce74fbd87fd
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-12 10:22:41 +0800

    change outline color

--- src/components/todos/content/TodoFilter.tsx
+++ src/components/todos/content/TodoFilter.tsx
@@ -43,8 +43,8 @@ const CategoriesFilter = ({ categories, todoListId }: CategoriesFilterProps) => 
 	return (
 		<div className="flex items-center relative rounded-md">
 			<div
-				className={`flex items-center cursor-pointer rounded-md p-1 hover:outline outline-1 outline-slate-400 ${
-					isOpen && 'outline outline-1 outline-slate-300 rounded-md'
+				className={`flex items-center cursor-pointer rounded-md p-1 hover:outline outline-1 outline-slate-300 ${
+					isOpen && 'outline outline-1 outline-slate-400 rounded-md'
 				}`}
 				onClick={() => setIsOpen(true)}
 			>
