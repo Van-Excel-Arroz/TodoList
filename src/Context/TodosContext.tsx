a83a062f4600c6c0287ec8315ea3987e1f3c785d
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-13 19:38:33 +0800

    Enhance TodosContext: add addCategory method to manage categories for todos. This addition allows users to associate multiple categories with a todo, improving organization and state management within the application.

--- src/Context/TodosContext.tsx
+++ src/Context/TodosContext.tsx
@@ -1,10 +1,11 @@
-import { Todo } from '@/types';
+import { Category, Todo } from '@/types';
 import { create } from 'zustand';
 
 interface TodosContextState {
 	initialTodos: Todo[];
 	setTodos: (todos: Todo[]) => void;
 	addTodo: (newTodo: Todo) => void;
+	addCategory: (todoId: number, newCategory: Category) => void;
 	deleteTodo: (todoId: number) => void;
 	updateTodoCompletion: (todoId: number) => void;
 	updateTodoTitle: (todoId: number, newTitle: string) => void;
@@ -16,6 +17,12 @@ const useTodosStore = create<TodosContextState>()((set: any) => ({
 	addTodo: (newTodo: Todo) =>
 		set((state: TodosContextState) => ({
 			initialTodos: [...state.initialTodos, newTodo],
+		})),
+	addCategory: (todoId: number, newCategory: Category) =>
+		set((state: TodosContextState) => ({
+			initialTodos: state.initialTodos.map(todo =>
+				todo.id === todoId ? { ...todo, categories: [...(todo.categories || []), newCategory] } : todo
+			),
 		})),
 	deleteTodo: (todoId: number) =>
 		set((state: TodosContextState) => ({
