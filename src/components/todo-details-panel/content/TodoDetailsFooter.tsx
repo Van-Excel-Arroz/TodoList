34083bab7cdf8776b4f9c276eafb4c4a64e0910f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-14 17:57:52 +0800

    Update TodoDetailsFooter component: reduce height of footer from 20 to 14 units for improved layout consistency. This change enhances the visual appearance of the todo details panel.

--- src/components/todo-details-panel/content/TodoDetailsFooter.tsx
+++ src/components/todo-details-panel/content/TodoDetailsFooter.tsx
@@ -22,7 +22,7 @@ export default function TodoDetailsFooter({ creationDate, todoId }: RightSidebar
 	};
 
 	return (
-		<div className="absolute border-t-2 border-slate-200 bottom-0 left-0 w-full h-20 pr-6 pl-4 bg-slate-100 flex items-center justify-between">
+		<div className="absolute border-t-2 border-slate-200 bottom-0 left-0 w-full h-14 pr-6 pl-4 bg-slate-100 flex items-center justify-between">
 			{creationDate && (
 				<p className="text-sm text-slate-600">Created at: {new Date(creationDate).toLocaleDateString()} </p>
 			)}
