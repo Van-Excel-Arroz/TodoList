e295e73b941a7bc75b0bfe33bc13f1146098822b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-15 16:49:32 +0800

    Implement real-time clock in TodoListsHeader component. This change introduces a stateful currentTime variable and a useEffect hook to update the displayed time every second, enhancing the user interface with dynamic time representation.

--- src/components/todolists-sidebar/content/TodoListsHeader.tsx
+++ src/components/todolists-sidebar/content/TodoListsHeader.tsx
@@ -3,14 +3,25 @@
 import { usePathname } from 'next/navigation';
 import { format } from 'date-fns';
 import TodoListsSidebarToggle from './TodoListsSidebarToggle';
+import { useEffect, useState } from 'react';
 
 export default function TodoListsHeader() {
 	const pathname = usePathname();
 	const isHome = pathname === '/tasks/home';
+	const [currentTime, setCurrentTime] = useState(new Date());
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCurrentTime(new Date());
+		}, 1000);
+
+		return () => clearInterval(timer);
+	}, []);
+
 	return (
 		<div className="flex justify-between">
 			<div className="flex flex-col">
-				<div className="text-xl flex justify-start font-semibold">{format(new Date(), `h:mm a`)} </div>
+				<div className="text-xl flex justify-start font-semibold">{format(currentTime, `h:mm a`)} </div>
 				<div className="text-sm">{format(new Date(), `EEEE, MMMM d`)}</div>
 			</div>
 			{!isHome && <TodoListsSidebarToggle />}
