ba3d2806669bb5b167a2da56894e130aea6cab53
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-16 16:18:55 +0800

    Enhance TodoListsContext: add addTodolist method for improved state management. This new method allows for adding a new todo list to the existing state, streamlining the process of managing todo lists within the application.

--- src/Context/TodoListsContext.tsx
+++ src/Context/TodoListsContext.tsx
@@ -4,11 +4,16 @@ import { create } from 'zustand';
 interface TodoListContextState {
 	todolists: TodoList[];
 	setTodolists: (todolists: TodoList[]) => void;
+	addTodolist: (newTodolist: TodoList) => void;
 }
 
 const useTodoListsStore = create<TodoListContextState>()((set: any) => ({
 	todolists: [],
 	setTodolists: (todolists: TodoList[]) => set({ todolists: todolists }),
+	addTodolist: (newTodolist: TodoList) =>
+		set((state: TodoListContextState) => ({
+			todolist: [...state.todolists, newTodolist],
+		})),
 }));
 
 export default useTodoListsStore;
