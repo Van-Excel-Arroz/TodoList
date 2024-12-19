e0a25c3dca5553fdea84c0880a73b9061fb5859a
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-19 09:50:54 +0800

    Update TodoTitle component: improve button type handling. This change modifies the button element to default to 'button' type when none is specified, enhancing functionality and preventing unintended form submissions.

--- src/components/todo-details-panel/content/TodoTitle.tsx
+++ src/components/todo-details-panel/content/TodoTitle.tsx
@@ -124,7 +124,7 @@ export function Button({ children, onClick, type, ariaLabel, disabled }: ButtonP
 		<button
 			className="hover:bg-slate-200 active:bg-slate-200 rounded-md p-1 text-slate-600 cursor-pointer"
 			onClick={() => onClick && onClick()}
-			type={type}
+			type={type ? type : 'button'}
 			aria-label={ariaLabel}
 			disabled={disabled}
 		>
