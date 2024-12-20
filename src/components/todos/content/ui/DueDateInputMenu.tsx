05c10cc94b4bd2c503972dadf553387f048b10d3
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-20 20:47:12 +0800

    Update DueDateInputMenu to use react-datetime for date selection. Added react-datetime dependency in package.json and package-lock.json. This change enhances the user experience by providing a more intuitive date selection interface, while also improving code organization and maintainability.

--- src/components/todos/content/ui/DueDateInputMenu.tsx
+++ src/components/todos/content/ui/DueDateInputMenu.tsx
@@ -2,6 +2,8 @@ import { Button } from '@/components/ui/Button';
 import { add, format, isToday, isTomorrow, setHours, setMinutes, setSeconds } from 'date-fns';
 import { Calendar, CalendarPlus, Trash2 } from 'lucide-react';
 import { useState } from 'react';
+import DateTime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
 
 interface DueDateInputProps {
 	dueDate: Date | undefined;
@@ -47,48 +49,52 @@ export default function DueDateInputMenu({ dueDate, setDueDate }: DueDateInputPr
 	};
 
 	return (
-		<div className="relative flex" onBlur={handleInputBlur} tabIndex={-1}>
-			<Button ariaLabel="Add Due Date" onClick={() => setIsOpen(prev => !prev)}>
+		<>
+			<div className="relative flex" onBlur={handleInputBlur} tabIndex={-1}>
 				{dueDate ? (
 					<p className="text-sm flex items-center gap-2">
-						<Calendar size={18} /> {handleDateFormat(dueDate)}
+						<Button ariaLabel="Add Due Date" onClick={() => setIsOpen(prev => !prev)}>
+							<Calendar size={18} />
+						</Button>
+						<DateTime initialValue={dueDate} />
 					</p>
 				) : (
-					<CalendarPlus size={18} />
+					<Button ariaLabel="Add Due Date" onClick={() => setIsOpen(prev => !prev)}>
+						<CalendarPlus size={18} />
+					</Button>
 				)}
-			</Button>
-
-			<div
-				className={`absolute top-10 -left-4 bg-white text-center text-black text-sm rounded-lg 
+				<div
+					className={`absolute top-10 -left-4 bg-white text-center text-black text-sm rounded-lg 
     flex flex-col w-44 border border-gray-300 shadow-lg
     before:content-[''] before:absolute before:-top-2 before:left-5 before:w-4 before:h-4 
     before:bg-white before:border-t before:border-l before:border-gray-300 before:rotate-45
     ${isOpen ? 'block' : 'hidden'}`}
-			>
-				<p className="border-b border-gray-200 p-2 font-medium">
-					{dueDate ? format(dueDate, 'MM/dd/yy hh:mm:ss a') : 'Select Due Date'}
-				</p>
-				<p className={menuItemStyle} onClick={() => handleSetDueDate('today')}>
-					Today ({format(new Date(), 'EEE')})
-				</p>
-				<p className={menuItemStyle} onClick={() => handleSetDueDate('tomorrow')}>
-					Tommorow ({format(add(new Date(), { days: 1 }), 'EEE')})
-				</p>
-				<p className={menuItemStyle} onClick={() => handleSetDueDate('next week')}>
-					Next {format(add(new Date(), { days: 7 }), 'EEEE')}
-				</p>
-				<p className={menuItemStyle}>Custom</p>
-
-				<button
-					aria-label="Clear Due Date"
-					type="button"
-					onClick={() => handleSetDueDate()}
-					className={`flex items-center justify-center gap-2 border-t border-slate-300 ${menuItemStyle}`}
 				>
-					<Trash2 size={16} />
-					<p>Clear</p>
-				</button>
+					<p className="border-b border-gray-200 p-2 font-medium">
+						{dueDate ? format(dueDate, 'MM/dd/yy hh:mm:ss a') : 'Select Due Date'}
+					</p>
+					<p className={menuItemStyle} onClick={() => handleSetDueDate('today')}>
+						Today ({format(new Date(), 'EEE')})
+					</p>
+					<p className={menuItemStyle} onClick={() => handleSetDueDate('tomorrow')}>
+						Tommorow ({format(add(new Date(), { days: 1 }), 'EEE')})
+					</p>
+					<p className={menuItemStyle} onClick={() => handleSetDueDate('next week')}>
+						Next {format(add(new Date(), { days: 7 }), 'EEEE')}
+					</p>
+					<p className={menuItemStyle}>Custom</p>
+
+					<button
+						aria-label="Clear Due Date"
+						type="button"
+						onClick={() => handleSetDueDate()}
+						className={`flex items-center justify-center gap-2 border-t border-slate-300 ${menuItemStyle}`}
+					>
+						<Trash2 size={16} />
+						<p>Clear</p>
+					</button>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
