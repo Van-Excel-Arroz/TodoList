a7f84141d74341b7d5f27fe5fa3123409a592b21
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-20 10:04:21 +0800

    Refactor TodoListsSidebarWrapper and Navbar components: update sidebar width transition and remove unnecessary border style. These changes improve the visual consistency and user experience of the task management interface by enhancing the sidebar's responsiveness and simplifying the Navbar's appearance.

--- src/components/todolists-sidebar/content/TodoListsSidebarWrapper.tsx
+++ src/components/todolists-sidebar/content/TodoListsSidebarWrapper.tsx
@@ -14,7 +14,7 @@ export default function TodoListsSidebarWrapper({ children }: { children: React.
 				onClick={toggleTodoListsSidebar}
 			/>
 			<div
-				className={`fixed lg:relative left-0 border-r border-slate-300 top-0 drop-shadow-xl bg-white pb-9 pt-3 lg:h-[calc(100vh-2.5rem)] h-screen transition-all duration-200 ease-in-out text-nowrap overflow-hidden backdrop-blur-sm bg-opacity-80 ${
+				className={`fixed lg:relative left-0 border-r border-slate-300 top-0 bg-white pb-9 pt-3 lg:h-[calc(100vh-2.5rem)] h-screen transition-all duration-200 ease-in-out text-nowrap overflow-hidden backdrop-blur-sm bg-opacity-80 ${
 					isTodoListsSidebarOpen ? 'w-72' : 'w-0'
 				}`}
 			>
