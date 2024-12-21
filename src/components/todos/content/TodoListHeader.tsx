dd750aaa5683a8790405c82d7b8ab1217b4022de
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-21 16:51:27 +0800

    Refactor TodoListHeader component: implement useEffect for setting the current todo list and update title rendering for improved state management.

--- src/components/todos/content/TodoListHeader.tsx
+++ src/components/todos/content/TodoListHeader.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import { TodoList } from '@/types';
 import useTodoListsSidebarStore from '@/context/TodoListsSidebarContext';
 import TodoListsSidebarToggle from '../../todolists-sidebar/content/TodoListsSidebarToggle';
@@ -17,14 +17,16 @@ interface TodoListHeaderProps {
 function TodoListHeader({ todolist }: TodoListHeaderProps) {
 	const { isTodoListsSidebarOpen } = useTodoListsSidebarStore();
 	const { setTodoList, todolist: currentTodolist } = useTodoListStore();
-	setTodoList(todolist);
+	useEffect(() => {
+		if (todolist) setTodoList(todolist);
+	}, [todolist, setTodoList]);
 
 	return (
 		<div className="sticky top-0 bg-white z-50 px-6">
 			<div className="flex justify-between pt-5 py-2">
 				<div className="flex items-center gap-2">
 					{!isTodoListsSidebarOpen ? <TodoListsSidebarToggle /> : null}
-					<p className="text-lg font-bold">{currentTodolist!.title}</p>
+					<p className="text-lg font-bold">{currentTodolist?.title}</p>
 				</div>
 				<div className="flex items-center gap-2">
 					<Button ariaLabel="Sort">
