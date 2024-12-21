73ae2626fa5b53e44e1370e0e40feb9b756f867d
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-21 17:40:46 +0800

    Refactor DueDateInputMenu component: implement click outside functionality to close menus for improved usability

--- src/components/todos/content/ui/DueDateInputMenu.tsx
+++ src/components/todos/content/ui/DueDateInputMenu.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/Button';
 import { add, format, setHours, setMinutes, setSeconds } from 'date-fns';
 import { Calendar, CalendarPlus, Trash2, X } from 'lucide-react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import DateTime from 'react-datetime';
 import 'react-datetime/css/react-datetime.css';
 
@@ -18,17 +18,28 @@ export default function DueDateInputMenu({ dueDate, setDueDate }: DueDateInputPr
 	const notch =
 		"before:content-[''] before:absolute before:w-4 before:h-4 before:bg-white before:border-t before:border-l before:border-gray-300 before:rotate-45";
 
-	const handleSelectMenuInputBlur = (e: React.FocusEvent<HTMLDivElement>) => {
-		if (!e.currentTarget.contains(e.relatedTarget as Node)) {
-			setIsOpen(false);
-		}
-	};
+	const selectMenuRef = useRef<HTMLDivElement>(null);
+	const customDatePickerRef = useRef<HTMLDivElement>(null);
 
-	const handleCustomeDatePickerInputBlur = (e: React.FocusEvent<HTMLDivElement>) => {
-		if (!e.currentTarget.contains(e.relatedTarget as Node)) {
-			setShowCustomDatePickerMenu(false);
-		}
-	};
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (isOpen && selectMenuRef.current && !selectMenuRef.current.contains(event.target as Node)) {
+				setIsOpen(false);
+			}
+			if (
+				showCustomDatePickerMenu &&
+				customDatePickerRef.current &&
+				!customDatePickerRef.current.contains(event.target as Node)
+			) {
+				setShowCustomDatePickerMenu(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen, showCustomDatePickerMenu]);
 
 	const handleSetDueDate = (date?: 'today' | 'tomorrow' | 'next week') => {
 		if (!date) {
@@ -61,103 +72,101 @@ export default function DueDateInputMenu({ dueDate, setDueDate }: DueDateInputPr
 	};
 
 	return (
-		<>
-			<div className="relative flex" onBlur={handleSelectMenuInputBlur} tabIndex={-1}>
-				{dueDate || showCustomDatePicker ? (
-					<div className="flex items-center py-0">
-						<Button
-							ariaLabel="Edit Due Date"
-							onClick={() => {
-								setIsOpen(prev => !prev);
-								setShowCustomDatePickerMenu(false);
-							}}
-						>
-							<Calendar size={20} />
-						</Button>
-						<Button
-							ariaLabel="Due Date"
-							onClick={() => {
-								setShowCustomDatePickerMenu(prev => !prev);
-								setIsOpen(false);
-							}}
-						>
-							<p>{dueDate ? format(dueDate, 'MM/dd/yy hh:mm a') : 'MM/DD/YY HH:MM a'}</p>
-						</Button>
-					</div>
-				) : (
+		<div className="relative flex">
+			{dueDate || showCustomDatePicker ? (
+				<div className="flex items-center py-0">
 					<Button
-						ariaLabel="Add Due Date"
+						ariaLabel="Edit Due Date"
 						onClick={() => {
 							setIsOpen(prev => !prev);
+							setShowCustomDatePickerMenu(false);
 						}}
 					>
-						<CalendarPlus size={20} />
+						<Calendar size={20} />
 					</Button>
-				)}
-
-				<div
-					className={`absolute top-10 -left-4 bg-white text-center text-black text-sm rounded-lg
-                    flex flex-col w-44 border border-gray-300 shadow-lg  before:-top-2 before:left-5 ${notch}
-                    ${isOpen ? 'block' : 'hidden'}`}
-				>
-					<p className="border-b border-gray-200 p-2 font-medium">
-						{dueDate ? format(dueDate, 'MM/dd/yy hh:mm:ss a') : 'Select Due Date'}
-					</p>
-					<p className={menuItemStyle} onClick={() => handleSetDueDate('today')}>
-						Today ({format(new Date(), 'EEE')})
-					</p>
-					<p className={menuItemStyle} onClick={() => handleSetDueDate('tomorrow')}>
-						Tomorrow ({format(add(new Date(), { days: 1 }), 'EEE')})
-					</p>
-					<p className={menuItemStyle} onClick={() => handleSetDueDate('next week')}>
-						Next {format(add(new Date(), { days: 7 }), 'EEEE')}
-					</p>
-					<p
-						className={menuItemStyle}
+					<Button
+						ariaLabel="Due Date"
 						onClick={() => {
-							setShowCustomDatePicker(true);
-							setShowCustomDatePickerMenu(true);
+							setShowCustomDatePickerMenu(prev => !prev);
 							setIsOpen(false);
 						}}
 					>
-						Custom
-					</p>
+						<p>{dueDate ? format(dueDate, 'MM/dd/yy hh:mm a') : 'MM/DD/YY HH:MM a'}</p>
+					</Button>
+				</div>
+			) : (
+				<Button
+					ariaLabel="Add Due Date"
+					onClick={() => {
+						setIsOpen(prev => !prev);
+					}}
+				>
+					<CalendarPlus size={20} />
+				</Button>
+			)}
+
+			<div
+				ref={selectMenuRef}
+				className={`absolute top-10 -left-4 bg-white text-center text-black text-sm rounded-lg
+                  flex flex-col w-44 border border-gray-300 shadow-lg before:-top-2 before:left-5 ${notch}
+                  ${isOpen ? 'block' : 'hidden'}`}
+			>
+				<p className="border-b border-gray-200 p-2 font-medium">
+					{dueDate ? format(dueDate, 'MM/dd/yy hh:mm:ss a') : 'Select Due Date'}
+				</p>
+				<p className={menuItemStyle} onClick={() => handleSetDueDate('today')}>
+					Today ({format(new Date(), 'EEE')})
+				</p>
+				<p className={menuItemStyle} onClick={() => handleSetDueDate('tomorrow')}>
+					Tomorrow ({format(add(new Date(), { days: 1 }), 'EEE')})
+				</p>
+				<p className={menuItemStyle} onClick={() => handleSetDueDate('next week')}>
+					Next {format(add(new Date(), { days: 7 }), 'EEEE')}
+				</p>
+				<p
+					className={menuItemStyle}
+					onClick={() => {
+						setShowCustomDatePicker(true);
+						setShowCustomDatePickerMenu(true);
+						setIsOpen(false);
+					}}
+				>
+					Custom
+				</p>
+				<button
+					aria-label="Clear Due Date"
+					type="button"
+					onClick={() => handleSetDueDate()}
+					className={`flex items-center justify-center gap-2 border-t border-slate-300 ${menuItemStyle}`}
+				>
+					<Trash2 size={16} />
+					Clear
+				</button>
+			</div>
+
+			<div
+				ref={customDatePickerRef}
+				className={`absolute top-10 left-0 border border-gray-300 shadow-md rounded-md before:-top-2 before:left-20 bg-white ${notch} ${
+					showCustomDatePickerMenu ? 'block' : 'hidden'
+				}`}
+			>
+				<div className="relative">
+					<DateTime
+						value={dueDate}
+						open={showCustomDatePickerMenu}
+						onChange={handleDateTimeChange}
+						closeOnSelect={true}
+						input={false}
+					/>
 					<button
-						aria-label="Clear Due Date"
-						type="button"
-						onClick={() => handleSetDueDate()}
-						className={`flex items-center justify-center gap-2 border-t border-slate-300 ${menuItemStyle}`}
+						aria-label="Close Custom Date Picker"
+						className="absolute -top-3 -right-3 bg-white p-1 border rounded-full border-gray-300 hover:bg-slate-200"
+						onClick={() => setShowCustomDatePickerMenu(false)}
 					>
-						<Trash2 size={16} />
-						Clear
+						<X size={18} />
 					</button>
 				</div>
-
-				<div
-					className={`absolute top-10 left-0 border border-gray-300 shadow-md rounded-md before:-top-2 before:left-20 bg-white ${notch} ${
-						showCustomDatePickerMenu ? 'block' : 'hidden'
-					}`}
-					onBlur={handleCustomeDatePickerInputBlur}
-					tabIndex={-1}
-				>
-					<div className="relative">
-						<DateTime
-							value={dueDate}
-							open={showCustomDatePickerMenu}
-							onChange={handleDateTimeChange}
-							closeOnSelect={true}
-							input={false}
-						/>
-						<button
-							aria-label="Close Custom Date Picker"
-							className="absolute -top-3 -right-3 bg-white p-1 border rounded-full border-gray-300 hover:bg-slate-200"
-							onClick={() => setShowCustomDatePickerMenu(false)}
-						>
-							<X size={18} />
-						</button>
-					</div>
-				</div>
 			</div>
-		</>
+		</div>
 	);
 }
