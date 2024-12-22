c63f2ea17d3aa6ca3fd0a5a8aabd495c85725f73
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-22 10:37:51 +0800

    Refactor TodoDueDate component: initialize initialDate from dueDatetime for improved date handling, and streamline rendering by removing conditional logic for better clarity and maintainability.

--- src/components/details-panel/components/TodoDueDate.tsx
+++ src/components/details-panel/components/TodoDueDate.tsx
@@ -7,6 +7,7 @@ import { Calendar } from 'lucide-react';
 
 export default function TodoDueDate({ dueDatetime }: { dueDatetime: string }) {
 	const { updateSelectedTodoDueDate } = useSelectedTodoStore();
+	const initialDate = dueDatetime ? new Date(dueDatetime) : undefined;
 	return (
 		<div className="flex items-center gap-4 bg-slate-100 rounded-md px-4 py-2 border border-slate-300">
 			<div className="flex flex-col justify-start">
@@ -14,14 +15,10 @@ export default function TodoDueDate({ dueDatetime }: { dueDatetime: string }) {
 					<Calendar size={16} />
 					<p className="text-sm">Due</p>
 				</div>
-				{dueDatetime ? (
-					<div>
-						<DueDate dueDatetime={dueDatetime ?? ''} textSize="base" />
-						<DatePicker dueDate={new Date(dueDatetime)} setDueDate={updateSelectedTodoDueDate} />
-					</div>
-				) : (
-					<p className="text-slate-600">MM/DD/YYYY</p>
-				)}
+
+				<div>
+					<DatePicker dueDate={initialDate} setDueDate={updateSelectedTodoDueDate} />
+				</div>
 			</div>
 		</div>
 	);
