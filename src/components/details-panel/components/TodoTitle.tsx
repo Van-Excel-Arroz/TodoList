82e1b1e243c1e068622b0164abb80938219576f9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-22 18:17:10 +0800

    Refactor TodoTitle component: replace EditTodoTitleForm with inline editing functionality, integrating form handling with react-hook-form for improved user experience. Enhance title editing with a textarea for better input handling and streamline state updates for selected todo titles.

--- src/components/details-panel/components/TodoTitle.tsx
+++ src/components/details-panel/components/TodoTitle.tsx
@@ -1,23 +1,54 @@
 'use client';
 
+import { updateTodoTitleAction } from '@/actions/todo-action';
 import useSelectedTodoStore from '@/context/SelectedTodoContext';
-import EditTodoTitleForm from '../ui/EditTodoTitleForm';
+import useTodosStore from '@/context/TodosContext';
+import { useForm } from 'react-hook-form';
 
 interface TodoTitleProps {
 	title: string;
 }
 
+interface EditTodoFormProps {
+	title: string;
+
+	todoId: number;
+	updateSelectedTodoTitle: (newTitle: string) => void;
+}
+
 export default function TodoTitle({ title }: TodoTitleProps) {
 	const { selectedTodo, updateSelectedTodoTitle } = useSelectedTodoStore();
-
+	const { register, handleSubmit, reset } = useForm<{ title: string }>();
+	const { updateTodoTitle } = useTodosStore();
+	const onSubmit = async (data: { title: string }) => {
+		if (!selectedTodo) return;
+		if (title !== data.title) {
+			await updateTodoTitleAction(selectedTodo.id, data.title);
+			updateSelectedTodoTitle(data.title);
+			updateTodoTitle(selectedTodo.id, data.title);
+		}
+		reset();
+	};
 	return (
 		<div className="flex items-start gap-2 flex-col w-full">
 			<p className="text-slate-600">Title</p>
-			<EditTodoTitleForm
-				title={title}
-				todoId={selectedTodo?.id ?? 0}
-				updateSelectedTodoTitle={updateSelectedTodoTitle}
-			/>
+			<form className="flex items-center justify-between w-full" onSubmit={handleSubmit(onSubmit)}>
+				<textarea
+					{...register('title')}
+					className="rounded-lg py-2 px-2 w-full border border-slate-300 hover:border-slate-400 focus:border-slate-400 focus:outline-none"
+					autoFocus
+					placeholder={title}
+					defaultValue={title}
+				/>
+				{/* <div className="flex gap-1">
+				<Button type="submit" ariaLabel="Save New Todo Title">
+					<Check size={18} />
+				</Button>
+				<Button ariaLabel="Cancel Editing Todo Title">
+					<X size={18} />
+				</Button>
+			</div> */}
+			</form>
 		</div>
 	);
 }
