7e95441147fb313bc577ee41c89ae019dd4605d4
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-22 18:44:13 +0800

    Enhance TodoTitle component: add editing state management and dynamic button visibility. Implement useState to track editing status and conditionally render the Save button based on user interaction. Improve user experience by ensuring the title input reflects changes and integrates better with the form handling logic.

--- src/components/details-panel/components/TodoTitle.tsx
+++ src/components/details-panel/components/TodoTitle.tsx
@@ -5,13 +5,14 @@ import { Button } from '@/components/ui-shared/Button';
 import useSelectedTodoStore from '@/context/SelectedTodoContext';
 import useTodosStore from '@/context/TodosContext';
 import { Save } from 'lucide-react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function TodoTitle() {
 	const { selectedTodo, updateSelectedTodoTitle } = useSelectedTodoStore();
-	const { register, handleSubmit, reset, setValue } = useForm<{ title: string }>();
+	const { register, handleSubmit, reset, setValue, watch } = useForm<{ title: string }>();
 	const { updateTodoTitle } = useTodosStore();
+	const [isEditing, setIsEditing] = useState(false);
 
 	const onSubmit = async (data: { title: string }) => {
 		if (!selectedTodo) return;
@@ -21,26 +22,36 @@ export default function TodoTitle() {
 			updateTodoTitle(selectedTodo.id, data.title);
 		}
 		reset();
+		setIsEditing(false);
 	};
 
 	const handleInputBlur = (e: React.FocusEvent<HTMLElement>) => {
 		if (!e.currentTarget.contains(e.relatedTarget as Node)) {
 			setValue('title', selectedTodo?.task_text || '');
+			setIsEditing(false);
 		}
 	};
 
+	const watchedTitle = watch('title');
+
 	useEffect(() => {
 		setValue('title', selectedTodo?.task_text || '');
-	}, [selectedTodo?.task_text]);
+	}, [selectedTodo?.task_text, setValue]);
+
+	useEffect(() => {
+		setIsEditing(watchedTitle !== selectedTodo?.task_text);
+	}, [watchedTitle, selectedTodo?.task_text]);
 
 	return (
 		<div onBlur={handleInputBlur} tabIndex={-1}>
 			<form className="flex items-start gap-2 flex-col w-full" onSubmit={handleSubmit(onSubmit)}>
 				<div className="flex items-center justify-between w-full">
 					<p className="text-slate-600">Title</p>
-					<Button type="submit" ariaLabel="Save New Todo Title">
-						<Save size={18} />
-					</Button>
+					{isEditing && (
+						<Button type="submit" ariaLabel="Save New Todo Title">
+							<Save size={18} />
+						</Button>
+					)}
 				</div>
 				<textarea
 					{...register('title')}
