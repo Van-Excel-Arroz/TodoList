17b4831022774640ebfe9494a8060c634d29a9e5
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-22 10:59:23 +0800

    Enhance TodoDueDate component: add Save button for improved user interaction and update layout for better alignment of DatePicker and button elements.

--- src/components/details-panel/components/TodoDueDate.tsx
+++ src/components/details-panel/components/TodoDueDate.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import DatePicker from '@/components/todo-list/ui/DatePicker';
+import { Button } from '@/components/ui-shared/Button';
 import useSelectedTodoStore from '@/context/SelectedTodoContext';
 
 export default function TodoDueDate({ dueDatetime }: { dueDatetime: string }) {
 	const { updateSelectedTodoDueDate } = useSelectedTodoStore();
 	const initialDate = dueDatetime ? new Date(dueDatetime) : undefined;
 	return (
-		<div className="flex items-center gap-4 bg-slate-100 rounded-md px-4 py-2 border border-slate-300">
+		<div className="flex items-center justify-between gap-4 bg-slate-100 rounded-md px-4 py-2 border border-slate-300">
 			<DatePicker dueDate={initialDate} setDueDate={updateSelectedTodoDueDate} defaultEmptyText={true} />
+			<Button ariaLabel="Save Due Date">Save</Button>
 		</div>
 	);
 }
