ddeb16659a18da6681485b869b98600e918889d5
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-22 18:10:52 +0800

    Enhance TodoDetailsPanel: add descriptive text next to completion checkbox to improve user clarity on todo status. Update layout for better visual organization.

--- src/components/details-panel/TodoDetailsPanel.tsx
+++ src/components/details-panel/TodoDetailsPanel.tsx
@@ -41,7 +41,10 @@ export default function TodoDetailsPanel() {
 			>
 				<div className="flex flex-col gap-4 px-6">
 					<TodoDetailsHeader todoId={selectedTodo?.id ?? 0} />
-					<CheckBox isChecked={selectedTodo?.is_completed ?? false} handleOnClick={handleCheckboxChange} />
+					<div className="flex items-center gap-2">
+						<CheckBox isChecked={selectedTodo?.is_completed ?? false} handleOnClick={handleCheckboxChange} />
+						<p>{selectedTodo?.is_completed ? 'Completed' : 'Mark as completed'}</p>
+					</div>
 					<TodoTitle title={selectedTodo?.task_text ?? ''} />
 					<TodoDueDate dueDatetime={selectedTodo?.due_datetime ?? ''} todoId={selectedTodo?.id ?? 0} />
 					<TodoCategories categories={selectedTodo?.categories ?? []} todoId={selectedTodo?.id ?? 0} />
