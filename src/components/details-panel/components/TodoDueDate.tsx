5d9875951e43441da0e33185c12a7c3cefec9e6b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-22 10:59:53 +0800

    Enhance TodoDueDate component: integrate Save icon into the button for improved visual feedback and user interaction when saving the due date.

--- src/components/details-panel/components/TodoDueDate.tsx
+++ src/components/details-panel/components/TodoDueDate.tsx
@@ -3,6 +3,7 @@
 import DatePicker from '@/components/todo-list/ui/DatePicker';
 import { Button } from '@/components/ui-shared/Button';
 import useSelectedTodoStore from '@/context/SelectedTodoContext';
+import { Save } from 'lucide-react';
 
 export default function TodoDueDate({ dueDatetime }: { dueDatetime: string }) {
 	const { updateSelectedTodoDueDate } = useSelectedTodoStore();
@@ -10,7 +11,9 @@ export default function TodoDueDate({ dueDatetime }: { dueDatetime: string }) {
 	return (
 		<div className="flex items-center justify-between gap-4 bg-slate-100 rounded-md px-4 py-2 border border-slate-300">
 			<DatePicker dueDate={initialDate} setDueDate={updateSelectedTodoDueDate} defaultEmptyText={true} />
-			<Button ariaLabel="Save Due Date">Save</Button>
+			<Button ariaLabel="Save Due Date">
+				<Save size={18} />
+			</Button>
 		</div>
 	);
 }
