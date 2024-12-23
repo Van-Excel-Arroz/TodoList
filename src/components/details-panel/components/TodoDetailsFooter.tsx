744ed97d4df545fdd2c5f0147ba0b8b90037cccb
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-23 11:10:53 +0800

    Refactor TodoDetailsFooter and CategoryForm components: update date formatting in TodoDetailsFooter for improved clarity and streamline color input styling in CategoryForm for enhanced user experience.

--- src/components/details-panel/components/TodoDetailsFooter.tsx
+++ src/components/details-panel/components/TodoDetailsFooter.tsx
@@ -1,13 +1,13 @@
+import { format } from 'date-fns';
+
 interface RightSidebarFooterProps {
 	creationDate: string;
 }
 
 export default function TodoDetailsFooter({ creationDate }: RightSidebarFooterProps) {
 	return (
-		<div className="absolute border-t-2 border-slate-200 bottom-0 left-0 w-full h-14 pr-6 pl-4 bg-slate-100 flex items-center justify-between">
-			{creationDate && (
-				<p className="text-sm text-slate-600">Created at: {new Date(creationDate).toLocaleDateString()} </p>
-			)}
+		<div className="flex items-center justify-between">
+			{creationDate && <p className="text-sm text-slate-600">Created: {format(creationDate, 'MM/dd/yyyy')} </p>}
 		</div>
 	);
 }
