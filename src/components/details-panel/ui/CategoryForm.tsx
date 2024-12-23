20637cb64e738af4c9a068ad19c901db5cce4fa4
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-23 11:18:46 +0800

    Refactor CategoryForm component: remove unused imports and add 'use client' directive for improved performance and clarity in component behavior.

--- src/components/details-panel/ui/CategoryForm.tsx
+++ src/components/details-panel/ui/CategoryForm.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { Button } from '@/components/ui-shared/Button';
-import { Plus, SendHorizontal, X } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 
 interface CategoryFormProps {
