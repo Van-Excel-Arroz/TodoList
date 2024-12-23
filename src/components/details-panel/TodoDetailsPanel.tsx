f0af54cea82874a67f31c4333a745992e9b732c6
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-23 18:50:11 +0800

    Add description field to TodoDetailsPanel: include a textarea for user input, enhancing task detail management and improving overall user experience.

--- src/components/details-panel/TodoDetailsPanel.tsx
+++ src/components/details-panel/TodoDetailsPanel.tsx
@@ -49,6 +49,10 @@ export default function TodoDetailsPanel() {
 						<TodoTitle />
 						<TodoDueDate todoId={selectedTodo?.id ?? 0} />
 						<TodoCategories categories={selectedTodo?.categories ?? []} todoId={selectedTodo?.id ?? 0} />
+						<div className="flex flex-col items-start">
+							<p className="text-slate-600 pb-2">Description</p>
+							<textarea className="w-full border border-slate-300 p-2 rounded-md focus:outline-none" />
+						</div>
 					</div>
 					<TodoDetailsFooter creationDate={selectedTodo?.creation_date ?? ''} />
 				</div>
