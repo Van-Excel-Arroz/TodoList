20b8952c064bce7b2728ee30c4a91ec554a89a3d
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-23 08:53:49 +0800

    Refactor TodoDueDate component: introduce state management for detecting changes in due date. Conditionally render save and cancel buttons based on whether the due date has been modified, enhancing user experience and interaction clarity.

--- src/components/details-panel/components/TodoDueDate.tsx
+++ src/components/details-panel/components/TodoDueDate.tsx
@@ -5,6 +5,7 @@ import DatePicker from '@/components/ui-shared/DatePicker';
 import useSelectedTodoStore from '@/context/SelectedTodoContext';
 import useTodosStore from '@/context/TodosContext';
 import { Save, Trash2 } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 interface TodoDueDateProps {
 	dueDatetime: string;
@@ -14,32 +15,38 @@ interface TodoDueDateProps {
 export default function TodoDueDate({ dueDatetime, todoId }: TodoDueDateProps) {
 	const { updateSelectedTodoDueDate } = useSelectedTodoStore();
 	const { updateDueDate, deleteDueDate } = useTodosStore();
+	const [isChanged, setIsChanged] = useState(false);
 	const initialDate = dueDatetime ? new Date(dueDatetime) : undefined;
 
 	const handleOnSubmit = () => {
 		if (!initialDate) return;
 		updateDueDate(todoId, initialDate.toISOString());
 	};
+	useEffect(() => {
+		setIsChanged(dueDatetime !== initialDate?.toString());
+	}, [initialDate]);
 
 	return (
 		<div className="flex flex-col">
 			<div className="flex items-center justify-between">
 				<p className="py-2 text-slate-600">Due Date</p>
-				<div className="flex items-center gap-1">
-					<Button ariaLabel="Save Due Date" type="submit" onClick={() => handleOnSubmit()}>
-						<Save size={18} />
-					</Button>
-					<Button
-						ariaLabel="Cancel Due Date Edit"
-						type="submit"
-						onClick={() => {
-							updateSelectedTodoDueDate(undefined);
-							deleteDueDate(todoId);
-						}}
-					>
-						<Trash2 size={18} />
-					</Button>
-				</div>
+				{isChanged && (
+					<div className="flex items-center gap-1">
+						<Button ariaLabel="Save Due Date" type="submit" onClick={() => handleOnSubmit()}>
+							<Save size={18} />
+						</Button>
+						<Button
+							ariaLabel="Cancel Due Date Edit"
+							type="submit"
+							onClick={() => {
+								updateSelectedTodoDueDate(undefined);
+								deleteDueDate(todoId);
+							}}
+						>
+							<Trash2 size={18} />
+						</Button>
+					</div>
+				)}
 			</div>
 			<div className="flex items-center justify-between gap-4 rounded-md px-1 py-2 border bg-white border-slate-300 hover:border-slate-400">
 				<DatePicker dueDate={initialDate} setDueDate={updateSelectedTodoDueDate} defaultEmptyText={true} />
