d3c8ae3c5bf36f90766b4d0a84b49fbb27476576
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-25 15:51:34 +0800

    Enhance TodoDescription component: Implemented editing functionality for todo descriptions, including form handling and state management. Added input change detection and blur handling to improve user experience and ensure accurate updates to the selected todo's description.

--- src/components/details-panel/components/TodoDescription.tsx
+++ src/components/details-panel/components/TodoDescription.tsx
@@ -1,8 +1,63 @@
+'use client';
+
+import { Button } from '@/components/ui-shared/Button';
+import useSelectedTodoStore from '@/context/SelectedTodoContext';
+import { Save } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 export default function TodoDescription() {
+	const { selectedTodo } = useSelectedTodoStore();
+	const [isEditing, setIsEditing] = useState(false);
+	const { register, handleSubmit, reset } = useForm<{ description: string }>({
+		defaultValues: { description: selectedTodo?.description ?? '' },
+	});
+
+	useEffect(() => {
+		reset({ description: selectedTodo?.description ?? '' });
+		setIsEditing(false);
+	}, [selectedTodo, reset]);
+
+	if (!selectedTodo) return null;
+
+	const onSubmit = async (data: { description: string }) => {
+		if (!data.description.trim()) return;
+		if (selectedTodo.task_text !== data.description) {
+		}
+		setIsEditing(false);
+	};
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setIsEditing(e.target.value !== selectedTodo.task_text);
+	};
+
+	const handleInputBlur = (e: React.FocusEvent<HTMLElement>) => {
+		if (!e.currentTarget.contains(e.relatedTarget as Node)) {
+			reset();
+			setIsEditing(false);
+		}
+	};
+
 	return (
-		<div className="flex flex-col items-start">
-			<p className="text-slate-600 pb-2">Description</p>
-			<textarea className="w-full border border-slate-300 p-2 rounded-md focus:outline-none" />
+		<div onBlur={handleInputBlur} tabIndex={-1}>
+			<form className="flex items-start flex-col w-full focus:outline-none" onSubmit={handleSubmit(onSubmit)}>
+				<div className="flex items-center justify-between w-full">
+					<p className="text-slate-600 pb-2">Title</p>
+					{isEditing && (
+						<Button type="submit" ariaLabel="Save New Todo Title">
+							<Save size={18} />
+						</Button>
+					)}
+				</div>
+				<textarea
+					{...register('description')}
+					className="rounded-lg py-2 px-2 w-full border border-slate-300 hover:border-slate-400 focus:border-slate-400 focus:outline-none"
+					autoFocus
+					aria-label="Todo Title Input"
+					placeholder={selectedTodo?.task_text}
+					onChange={handleInputChange}
+				/>
+			</form>
 		</div>
 	);
 }
