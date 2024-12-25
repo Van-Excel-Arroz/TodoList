48767079d366492b3ff51c5c941a936c03259417
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-25 14:23:45 +0800

    Enhance TodoDetailsFooter component: Added a null check for creationDate to prevent rendering issues when no date is available. This improves the component's robustness and user experience by ensuring it only displays when relevant data is present.

--- src/components/details-panel/components/TodoDetailsFooter.tsx
+++ src/components/details-panel/components/TodoDetailsFooter.tsx
@@ -5,6 +5,8 @@ export default function TodoDetailsFooter() {
 	const { selectedTodo } = useSelectedTodoStore();
 	const creationDate = selectedTodo?.creation_date ?? '';
 
+	if (!creationDate) return null;
+
 	return (
 		<div className="sticky bottom-0 py-5 border-t border-slate-300  bg-white  w-full  flex items-center justify-between">
 			<div className="flex items-center justify-between text-xs text-slate-600 w-full">
