78c4c692981b292162e439425998f29bcfc789be
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-27 09:56:00 +0800

    Enhance TodoImportance component: Integrate updateTodoImportanceAction and toggle functions to manage importance state. Update handleImportanceChange to reflect changes in selectedTodo importance, improving user interaction and task management capabilities.

--- src/components/details-panel/components/TodoImportance.tsx
+++ src/components/details-panel/components/TodoImportance.tsx
@@ -1,13 +1,22 @@
+import { updateTodoImportanceAction } from '@/actions/todo-action';
 import Importance from '@/components/ui-shared/Importance';
 import useSelectedTodoStore from '@/context/SelectedTodoContext';
+import useTodosStore from '@/context/TodosContext';
 
 export default function TodoImportance() {
-	const { selectedTodo } = useSelectedTodoStore();
+	const { selectedTodo, toggleSelectedTodoImportance } = useSelectedTodoStore();
+	const { toggleTodoImportance } = useTodosStore();
 	const isImportant = selectedTodo?.is_important ?? false;
 
+	const handleImportanceChange = async () => {
+		if (!selectedTodo) return;
+		await updateTodoImportanceAction(selectedTodo.id, !isImportant);
+		toggleTodoImportance(selectedTodo.id);
+		toggleSelectedTodoImportance(selectedTodo.id);
+	};
 	return (
 		<div className="flex items-center">
-			<Importance isImportant={isImportant} />
+			<Importance isImportant={isImportant} handleOnClick={handleImportanceChange} />
 		</div>
 	);
 }
