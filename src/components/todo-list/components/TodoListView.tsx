c332978a21bb90fe7587e9c5235ddf8a5bfd5701
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-12-31 09:44:20 +0800

    Remove unnecessary blank line in TodoListView component for improved code cleanliness. This change enhances the overall readability of the component without affecting functionality.

--- src/components/todo-list/components/TodoListView.tsx
+++ src/components/todo-list/components/TodoListView.tsx
@@ -21,7 +21,6 @@ export default function TodoListView({ todos }: TodoListViewProps) {
 	return (
 		<div>
 			<TodoSection title="Todos" todos={incompletedTodos} />
-
 			<motion.div layout transition={{ duration: 0.15 }}>
 				<TodoSection title="Completed Todos" todos={completedTodos} />
 			</motion.div>
