9aeaf7ca648d7299cd2ac92fc46b412d451e7c16
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-01 18:12:44 +0800

    Refactor Layout component: Simplify JSX structure for improved readability. This change enhances the organization of the layout code by removing unnecessary fragments, making it easier to maintain and understand the component's structure.

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -8,23 +8,21 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps): JSX.Element {
 	return (
-		<>
-			<div className="flex flex-col h-screen overflow-hidden">
-				<header className="sticky top-0 left-0 z-50 w-full">
-					<Navbar />
-				</header>
-				<div className="flex h-screen">
-					<aside className="absolute lg:sticky top-0 left-0 z-30">
-						<TodoListsSidebar />
-					</aside>
+		<div className="flex flex-col h-screen overflow-hidden">
+			<header className="sticky top-0 left-0 z-50 w-full">
+				<Navbar />
+			</header>
+			<div className="flex h-screen">
+				<aside className="absolute lg:sticky top-0 left-0 z-30">
+					<TodoListsSidebar />
+				</aside>
 
-					<main className="w-full  overflow-y-scroll z-10">{children}</main>
+				<main className="w-full  overflow-y-scroll z-10">{children}</main>
 
-					<aside className="absolute lg:sticky top-0 right-0 z-40">
-						<TodoDetailsPanel />
-					</aside>
-				</div>
+				<aside className="absolute lg:sticky top-0 right-0 z-40">
+					<TodoDetailsPanel />
+				</aside>
 			</div>
-		</>
+		</div>
 	);
 }
