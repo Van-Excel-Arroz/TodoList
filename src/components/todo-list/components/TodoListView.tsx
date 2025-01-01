aaa0fe1db01150784fdc7a37fa9db5c186eaa00e
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-01 18:43:56 +0800

    Refactor TodoListView component: Enhance sorting logic to prioritize todos by importance, selected categories, due date, and creation date. This update improves task organization and user experience by ensuring todos are displayed in a more intuitive order, taking into account multiple criteria for effective management.

--- src/components/todo-list/components/TodoListView.tsx
+++ src/components/todo-list/components/TodoListView.tsx
@@ -11,26 +11,31 @@ export default function TodoListView({ todos }: TodoListViewProps) {
 	const selectedCategories = new Set(['coding', 'UI/UX', 'try']);
 
 	const sortedTodos = [...todos].sort((a, b) => {
+		// if there is an important todo, ranked it first
 		if (a.is_important !== b.is_important) {
 			return a.is_important ? -1 : 1;
 		}
 
+		// if both have categories, ranked them by how many categories matches in the selected categories
 		const a_categories = (a.categories ?? []).filter(cat => selectedCategories.has(cat.category_title));
 		const b_categories = (b.categories ?? []).filter(cat => selectedCategories.has(cat.category_title));
 
 		if (a_categories.length < b_categories.length) return 1;
 		if (a_categories.length > b_categories.length) return -1;
 
+		// if both have due dates, compare them which has shorter due date
 		if (a.due_datetime && b.due_datetime) {
 			return compareAsc(new Date(a.due_datetime), new Date(b.due_datetime));
 		}
 
+		// if one of them have due date, ranked it above the other that doesnt have it
+		if (a.due_datetime) return -1;
+		if (b.due_datetime) return 1;
+
+		// if it just a plain todo, ranked it by creation date
 		if (a.creation_date && b.creation_date) {
 			return compareDesc(new Date(a.creation_date), new Date(b.creation_date));
 		}
-
-		if (a.due_datetime) return -1;
-		if (b.due_datetime) return 1;
 
 		return 0;
 	});
