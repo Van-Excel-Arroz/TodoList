cacba75715e62b4e37300a60530a76a9b45a44b9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-02 12:32:18 +0800

    Enhance Menu component: Add posLeft and posRight props for dynamic positioning. This update allows for more flexible menu placement by enabling left offset adjustments, improving the overall usability and layout of the Menu component.

--- src/components/ui-shared/Menu.tsx
+++ src/components/ui-shared/Menu.tsx
@@ -4,15 +4,19 @@ interface MenuProps {
 	open: boolean;
 	ref: LegacyRef<HTMLDivElement> | undefined;
 	children: React.ReactNode;
+	posLeft?: number;
+	posRight?: number;
 	leftNotch?: number;
 	right?: number;
 }
 
-export default function Menu({ open, ref, children, leftNotch, right }: MenuProps) {
+export default function Menu({ open, ref, children, posLeft, posRight, leftNotch, right }: MenuProps) {
 	return (
 		<div
 			ref={ref}
-			className={`absolute top-10 -left-14 z-20 bg-white text-center text-black text-sm rounded-lg
+			className={`absolute top-10 ${
+				posLeft && `-left-${posLeft}`
+			} z-20 bg-white text-center text-black text-sm rounded-lg
                   flex flex-col w-44 border border-gray-300 shadow-lg before:-top-2 ${
 										leftNotch && `before:left-${leftNotch}`
 									}
