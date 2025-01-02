b8b30ebae545e8683e6b8ccd4638dae57b73bedc
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-02 13:09:02 +0800

    Refactor TodoFilter component: Integrate Menu for enhanced filtering options and improved layout. This change simplifies the component structure, adds a reference for the Menu, and maintains responsiveness in the todo list interface.

--- src/components/todo-list/components/TodoFilter.tsx
+++ src/components/todo-list/components/TodoFilter.tsx
@@ -1,16 +1,21 @@
 'use client';
 
 import { Button } from '@/components/ui-shared/Button';
+import Menu from '@/components/ui-shared/Menu';
 import { Filter } from 'lucide-react';
+import { useRef } from 'react';
 
 export default function TodoFilter() {
+	const FilterMenuRef = useRef<HTMLDivElement>(null);
+
 	return (
-		<>
-			<div className="text-xs 2xl:text-sm flex items-center gap-2 mr-4">
-				<Button ariaLabel="Filter">
-					<Filter size={20} className="text-slate-600" />
-				</Button>
-			</div>
-		</>
+		<div className="relative flex items-center mr-4">
+			<Button ariaLabel="Filter">
+				<Filter size={20} className="text-slate-600" />
+			</Button>
+			<Menu ref={FilterMenuRef} open={true} width={44} rightNotch={6} posRight={5}>
+				HELLO WORLD
+			</Menu>
+		</div>
 	);
 }
