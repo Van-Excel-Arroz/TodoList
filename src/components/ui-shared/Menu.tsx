0e7fd16bff626fe154cca770d0aa92e85938b5a0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-02 12:41:53 +0800

    Refactor Menu component: Replace 'right' prop with 'rightNotch' for improved menu positioning. This update enhances the layout flexibility by allowing for dynamic right offset adjustments, contributing to a more responsive design and better usability in the date selection interface.

--- src/components/ui-shared/Menu.tsx
+++ src/components/ui-shared/Menu.tsx
@@ -7,18 +7,20 @@ interface MenuProps {
 	posLeft?: number;
 	posRight?: number;
 	leftNotch?: number;
-	right?: number;
+	rightNotch?: number;
 	width?: number;
 }
 
-export default function Menu({ open, ref, children, posLeft, posRight, leftNotch, right, width }: MenuProps) {
+export default function Menu({ open, ref, children, posLeft, posRight, leftNotch, rightNotch, width }: MenuProps) {
 	return (
 		<div
 			ref={ref}
 			className={`absolute top-10 z-20 bg-white text-center text-black text-sm rounded-lg flex flex-col border border-gray-300 shadow-lg before:-top-2 before:content-[''] before:absolute before:w-4 before:h-4 before:bg-white before:border-t before:border-l before:border-gray-300 before:rotate-45
                   ${width ? `w-${width}` : 'w-fit'}
                   ${leftNotch && `before:left-${leftNotch}`}
+                  ${rightNotch && `before:left-${rightNotch}`}
                   ${posLeft && `-left-${posLeft}`} 
+                  ${posRight && `-left-${posRight}`} 
                   ${open ? 'block' : 'hidden'}`}
 		>
 			{children}
