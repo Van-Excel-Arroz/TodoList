02d3efd038397bf87c34002593625fd63a730115
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-02 14:26:09 +0800

    Refactor TodoFilter component: Remove unused useRef and simplify Menu integration by adding onClose prop for improved functionality. This change enhances code clarity and maintains responsiveness in the todo list filtering interface.

--- src/components/todo-list/components/TodoFilter.tsx
+++ src/components/todo-list/components/TodoFilter.tsx
@@ -6,14 +6,12 @@ import { Filter } from 'lucide-react';
 import { useRef } from 'react';
 
 export default function TodoFilter() {
-	const FilterMenuRef = useRef<HTMLDivElement>(null);
-
 	return (
 		<div className="relative">
 			<Button ariaLabel="Filter">
 				<Filter size={20} className="text-slate-600" />
 			</Button>
-			<Menu ref={FilterMenuRef} open={true} posX="-right-5" posXNotch="before:right-6" width="w-44">
+			<Menu open={true} onClose={() => console.log('lol')} posX="-right-5" posXNotch="before:right-6" width="w-44">
 				HELLO WORLD
 			</Menu>
 		</div>
