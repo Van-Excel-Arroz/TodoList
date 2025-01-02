cbe754130dcef9b3ffde89be2bde0b28e21517c9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-02 12:17:27 +0800

    Refactor Menu component: Simplify notch styling by extracting CSS classes into a variable. This change improves code readability and maintainability by centralizing the notch styling logic, making future adjustments easier.

--- src/components/ui-shared/Menu.tsx
+++ src/components/ui-shared/Menu.tsx
@@ -7,13 +7,14 @@ interface MenuProps {
 }
 
 export default function Menu({ open, ref, children }: MenuProps) {
+	const notch =
+		"before:-top-2 before:left-5 before:content-[''] before:absolute before:w-4 before:h-4 before:bg-white before:border-t before:border-l before:border-gray-300 before:rotate-45";
 	return (
 		<div
 			ref={ref}
 			className={`absolute top-10 -left-4 z-20 bg-white text-center text-black text-sm rounded-lg
-                  flex flex-col w-44 border border-gray-300 shadow-lg before:content-[''] before:absolute 
-                  before:w-4 before:h-4 before:bg-white before:border-t before:border-l before:border-gray-300 
-                  before:rotate-45 ${open ? 'block' : 'hidden'}`}
+                  flex flex-col w-44 border border-gray-300 shadow-lg ${notch}
+                  ${open ? 'block' : 'hidden'}`}
 		>
 			{children}
 		</div>
