8b21c59e6414bf68cb297ea2fe5a6668ee8a633c
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-03 15:53:15 +0800

    Refactor TodoControls component: Enhance sorting icon logic and labels for improved clarity. This change introduces additional sorting icons for alphabetical and importance fields, ensuring users can easily distinguish between different sorting options. The conditional rendering of icons and labels has been updated to provide a clearer understanding of the current sort order, aligning with existing functionality for due date and creation date sorting.

--- src/components/todo-list/components/TodoControls.tsx
+++ src/components/todo-list/components/TodoControls.tsx
@@ -1,5 +1,13 @@
 import { Button } from '@/components/ui-shared/Button';
-import { CalendarArrowDown, CalendarArrowUp, X } from 'lucide-react';
+import {
+	ArrowDownAZ,
+	ArrowDownWideNarrow,
+	ArrowUpWideNarrow,
+	ArrowUpZA,
+	CalendarArrowDown,
+	CalendarArrowUp,
+	X,
+} from 'lucide-react';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 
@@ -25,12 +33,29 @@ export default function TodoControls() {
 		<div className={`${field ? 'inline-block' : 'hidden'} mb-3 p-1 bg-slate-200 text-slate-700 rounded-lg`}>
 			<div className="flex items-center gap-1">
 				<Button ariaLabel="Reverse Sort Order" onClick={handleSortToggle}>
-					{order === 'desc' ? <CalendarArrowDown size={14} /> : <CalendarArrowUp size={14} />}
+					{field === 'dueDate' || field === 'creationDate' ? (
+						order === 'desc' ? (
+							<CalendarArrowDown size={14} />
+						) : (
+							<CalendarArrowUp size={14} />
+						)
+					) : field === 'alphabetical' ? (
+						order === 'desc' ? (
+							<ArrowDownAZ size={14} />
+						) : (
+							<ArrowUpZA size={14} />
+						)
+					) : order === 'desc' ? (
+						<ArrowDownWideNarrow size={14} />
+					) : (
+						<ArrowUpWideNarrow size={14} />
+					)}
 				</Button>
 				{field === 'dueDate' && <p className="text-xs">Due Date ({order === 'desc' ? 'Latest' : 'Earlier'} first)</p>}
 				{field === 'creationDate' && (
 					<p className="text-xs">Creation Date ({order === 'desc' ? 'Newest' : 'Oldest'} first)</p>
 				)}
+				{field === 'importance' && <p className="text-xs">Importance</p>}
 				<Button ariaLabel="Remove Sort" onClick={handleRemoveSort}>
 					<X size={12} />
 				</Button>
