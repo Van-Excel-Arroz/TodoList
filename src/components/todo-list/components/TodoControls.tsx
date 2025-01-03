43becebdfe445a47fa860c5aa9091a08a97d3fe0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-03 15:22:38 +0800

    Refactor TodoListHeader component: Remove sorting logic and integrate TodoControls for improved UI clarity. This change simplifies the component by eliminating unnecessary sorting handlers and enhances user experience by providing a dedicated controls section for task management.

---
+++ src/components/todo-list/components/TodoControls.tsx
@@ -0,0 +1,43 @@
+import { Button } from '@/components/ui-shared/Button';
+import { ArrowUpDown, X } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+
+export default function TodoControls() {
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const [field, order] = searchParams.get('sort')?.split(':') || [];
+
+	const handleSortToggle = () => {
+		const newOrder = order === 'desc' ? 'asc' : 'desc';
+		const params = new URLSearchParams(searchParams.toString());
+		params.set('sort', `${field}:${newOrder}`);
+		router.push(`/tasks/?${params.toString()}`);
+	};
+
+	const handleRemoveSort = () => {
+		const params = new URLSearchParams(searchParams.toString());
+		params.delete('sort');
+		router.push(`/tasks/?${params.toString()}`);
+	};
+	return (
+		<>
+			{field && (
+				<div className="inline-block mb-3 p-1 bg-slate-200 text-slate-700 rounded-lg">
+					<div className="flex items-center gap-1">
+						<Button ariaLabel="Reverse Sort Order" onClick={handleSortToggle}>
+							<ArrowUpDown size={14} />
+						</Button>
+						{field === 'dueDate' && (
+							<p className="text-xs">Due Date ({order === 'desc' ? 'Latest' : 'Earlier'} first)</p>
+						)}
+						{field === 'creationDate' && <p className="text-xs">Creation Date</p>}
+						<Button ariaLabel="Remove Due Date Sort" onClick={handleRemoveSort}>
+							<X size={12} />
+						</Button>
+					</div>
+				</div>
+			)}
+		</>
+	);
+}
--- src/components/todo-list/components/TodoListHeader.tsx
+++ src/components/todo-list/components/TodoListHeader.tsx
@@ -8,34 +8,17 @@ import TodoForm from './TodoForm';
 import TodoFilter from './TodoFilter';
 import useTodoListStore from '@/context/TodoListContext';
 import TodoSort from './TodoSort';
-import { ArrowUpDown, Search, X } from 'lucide-react';
+import { Search } from 'lucide-react';
 import { Button } from '@/components/ui-shared/Button';
-import { useSearchParams, useRouter } from 'next/navigation';
+import TodoControls from './TodoControls';
 
 function TodoListHeader({ todolist }: { todolist: TodoList }) {
 	const { isTodoListsSidebarOpen } = useTodoListsSidebarStore();
 	const { setTodoList, todolist: currentTodolist } = useTodoListStore();
-	const router = useRouter();
-	const searchParams = useSearchParams();
-
-	const [field, order] = searchParams.get('sort')?.split(':') || [];
 
 	useEffect(() => {
 		if (todolist) setTodoList(todolist);
 	}, [todolist, setTodoList]);
-
-	const handleSortToggle = () => {
-		const newOrder = order === 'desc' ? 'asc' : 'desc';
-		const params = new URLSearchParams(searchParams.toString());
-		params.set('sort', `${field}:${newOrder}`);
-		router.push(`/tasks/?${params.toString()}`);
-	};
-
-	const handleRemoveSort = () => {
-		const params = new URLSearchParams(searchParams.toString());
-		params.delete('sort');
-		router.push(`/tasks/?${params.toString()}`);
-	};
 
 	return (
 		<div className="sticky top-0 bg-slate-100 z-50 px-6">
@@ -52,23 +35,7 @@ function TodoListHeader({ todolist }: { todolist: TodoList }) {
 					<TodoFilter todolistId={todolist.id} />
 				</div>
 			</div>
-			{field && (
-				<div className="inline-block mb-3 p-1 bg-slate-200 text-slate-700 rounded-lg">
-					<div className="flex items-center gap-1">
-						<Button ariaLabel="Reverse Sort Order" onClick={handleSortToggle}>
-							<ArrowUpDown size={14} />
-						</Button>
-						{field === 'dueDate' && (
-							<p className="text-xs">Due Date ({order === 'desc' ? 'Latest' : 'Earlier'} first)</p>
-						)}
-						{field === 'creationDate' && <p className="text-xs">Creation Date</p>}
-						<Button ariaLabel="Remove Due Date Sort" onClick={handleRemoveSort}>
-							<X size={12} />
-						</Button>
-					</div>
-				</div>
-			)}
-
+			<TodoControls />
 			<TodoForm todolistId={todolist.id} />
 		</div>
 	);
