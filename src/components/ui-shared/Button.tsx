8f18cdcdc80120cc9e374fee5d2b2e4203f1de95
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-06 07:55:28 +0800

    Enhance Button component: Add className prop for customizable styling. This update allows users to pass a custom className to the Button component, improving flexibility in styling while maintaining existing functionality.

--- src/components/ui-shared/Button.tsx
+++ src/components/ui-shared/Button.tsx
@@ -4,12 +4,13 @@ interface ButtonProps {
 	type?: 'submit';
 	ariaLabel: string;
 	disabled?: boolean;
+	className?: string;
 }
 
-export function Button({ children, onClick, type, ariaLabel, disabled }: ButtonProps) {
+export function Button({ children, onClick, type, ariaLabel, disabled, className = '' }: ButtonProps) {
 	return (
 		<button
-			className="relative hover:bg-slate-200 active:bg-slate-300 rounded-md p-1 text-slate-600 cursor-pointer"
+			className={`${className} relative hover:bg-slate-200 active:bg-slate-300 rounded-md p-1 text-slate-600 cursor-pointer`}
 			onClick={() => onClick && onClick()}
 			type={type ? type : 'button'}
 			aria-label={ariaLabel}
