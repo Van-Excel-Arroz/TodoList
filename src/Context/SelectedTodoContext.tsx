2888ffd89776c40490f74fb41a57996203bcfb8a
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-05 21:05:02 +0800

    Refactor SelectedTodoContext: Remove unused todo update functions for cleaner state management. This change simplifies the context by eliminating functions related to updating the title, category, and completion status of the selected todo, streamlining the state handling process and improving overall code clarity.

--- src/Context/SelectedTodoContext.tsx
+++ src/Context/SelectedTodoContext.tsx
@@ -1,60 +1,18 @@
-import { Category, Todo } from '@/types';
+import { Todo } from '@/types';
 import { create } from 'zustand';
 
 interface SelectedTodoContextState {
 	selectedTodo: Todo | null;
 	setSelectedTodo: (todo: Todo | null) => void;
-	updateSelectedTodoTitle: (newTitle: string) => void;
-	updateSelectedTodoCategory: (newCategory: Category) => void;
-	removeSelectedTodoCategory: (categoryId: number) => void;
-	toggleSelectedTodoCompletion: (todoId: number) => void;
-	toggleSelectedTodoImportance: (todoId: number) => void;
 	updateSelectedTodoDueDate: (newDate: Date | undefined) => void;
-	updateSelectedTodoDescription: (newDescription: string | null) => void;
 }
 
 const useSelectedTodoStore = create<SelectedTodoContextState>()((set: any) => ({
 	selectedTodo: null,
 	setSelectedTodo: (todo: Todo | null) => set({ selectedTodo: todo }),
-	updateSelectedTodoTitle: (newTitle: string) =>
-		set((state: SelectedTodoContextState) => ({
-			selectedTodo: { ...state.selectedTodo, task_text: newTitle },
-		})),
-	updateSelectedTodoCategory: (newCategory: Category) =>
-		set((state: SelectedTodoContextState) => ({
-			selectedTodo: {
-				...state.selectedTodo,
-				categories: [...(state.selectedTodo!.categories || []), newCategory],
-			},
-		})),
-	removeSelectedTodoCategory: (categoryId: number) =>
-		set((state: SelectedTodoContextState) => ({
-			selectedTodo: {
-				...state.selectedTodo,
-				categories: state.selectedTodo!.categories?.filter(category => category.id !== categoryId),
-			},
-		})),
-	toggleSelectedTodoCompletion: (todoId: number) =>
-		set((state: SelectedTodoContextState) => ({
-			selectedTodo:
-				state.selectedTodo && state.selectedTodo.id === todoId
-					? { ...state.selectedTodo, is_completed: !state.selectedTodo.is_completed }
-					: state.selectedTodo,
-		})),
-	toggleSelectedTodoImportance: (todoId: number) =>
-		set((state: SelectedTodoContextState) => ({
-			selectedTodo:
-				state.selectedTodo && state.selectedTodo.id === todoId
-					? { ...state.selectedTodo, is_important: !state.selectedTodo.is_important }
-					: state.selectedTodo,
-		})),
 	updateSelectedTodoDueDate: (newDate: Date | undefined) =>
 		set((state: SelectedTodoContextState) => ({
 			selectedTodo: { ...state.selectedTodo, due_datetime: newDate },
-		})),
-	updateSelectedTodoDescription: (newDescription: string | null) =>
-		set((state: SelectedTodoContextState) => ({
-			selectedTodo: { ...state.selectedTodo, description: newDescription },
 		})),
 }));
 
