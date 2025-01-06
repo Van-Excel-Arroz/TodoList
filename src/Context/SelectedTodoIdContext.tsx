0b8b385aad7e555990d277348909f179d9c4041f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-06 17:35:50 +0800

    Refactor SelectedTodoIdContext: Update setSelectedTodoId method to correctly set selectedTodoId instead of selectedTodo. This change enhances clarity and consistency in state management across todo components.

--- src/Context/SelectedTodoIdContext.tsx
+++ src/Context/SelectedTodoIdContext.tsx
@@ -7,7 +7,7 @@ interface SelectedTodoIdContextState {
 
 const useSelectedTodoIdStore = create<SelectedTodoIdContextState>()((set: any) => ({
 	selectedTodoId: 0,
-	setSelectedTodoId: (todoId: number) => set({ selectedTodo: todoId }),
+	setSelectedTodoId: (todoId: number) => set({ selectedTodoId: todoId }),
 }));
 
 export default useSelectedTodoIdStore;
