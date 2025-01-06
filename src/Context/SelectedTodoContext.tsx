7d40be5a7a128e2c33f74ca88c0b9d35c326123a
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-06 17:06:42 +0800

    Refactor SelectedTodoContext: Remove updateSelectedTodoDueDate function to streamline state management. This change enhances clarity by simplifying the context API and focusing on the selectedTodo state handling.

--- src/Context/SelectedTodoContext.tsx
+++ src/Context/SelectedTodoContext.tsx
@@ -4,16 +4,11 @@ import { create } from 'zustand';
 interface SelectedTodoContextState {
 	selectedTodo: Todo | null;
 	setSelectedTodo: (todo: Todo | null) => void;
-	updateSelectedTodoDueDate: (newDate: Date | undefined) => void;
 }
 
 const useSelectedTodoStore = create<SelectedTodoContextState>()((set: any) => ({
 	selectedTodo: null,
 	setSelectedTodo: (todo: Todo | null) => set({ selectedTodo: todo }),
-	updateSelectedTodoDueDate: (newDate: Date | undefined) =>
-		set((state: SelectedTodoContextState) => ({
-			selectedTodo: { ...state.selectedTodo, due_datetime: newDate },
-		})),
 }));
 
 export default useSelectedTodoStore;
