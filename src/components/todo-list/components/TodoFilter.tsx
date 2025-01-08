b35bf9015c3b6ade7ff1d8ec02d05849bafdd6bf
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-08 18:58:26 +0800

    Update TodoFilter component: Adjust button size for unselecting categories. This change enhances the user interface by ensuring the button is appropriately sized, improving usability when toggling category selection in the todo list.

--- src/components/todo-list/components/TodoFilter.tsx
+++ src/components/todo-list/components/TodoFilter.tsx
@@ -85,7 +85,7 @@ export default function TodoFilter({ todolistId, categories }: TodoFilterProps) 
 								<p className="text-base">{category.category_title}</p>
 							</div>
 
-							<Button ariaLabel="Unselect category" className="hover:bg-slate-300">
+							<Button ariaLabel="Unselect category" className="hover:bg-slate-300 w-5 h-5">
 								<CheckIcon size={16} strokeWidth={2} className={`${category.is_selected ? 'block' : 'hidden'} `} />
 							</Button>
 						</MenuItem>
