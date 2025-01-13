698cc2159793a8625bceae19fde53bc6e48c2ea4
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-13 18:09:32 +0800

    Refactor TodoFilter component: Implement dynamic menu toggling for filter options. This change enhances user interaction by allowing users to easily open and close the date and category filters, improving the overall filtering experience and code maintainability.

--- src/components/todo-list/components/TodoFilter.tsx
+++ src/components/todo-list/components/TodoFilter.tsx
@@ -20,17 +20,34 @@ export default function TodoFilter({ initialCategories }: { initialCategories: C
 	const [isCategoryFilterOpen, setIsCategoryFilterOpen] = useState(false);
 	const [isDateFilterOpen, setIsDateFilterOpen] = useState(false);
 
+	filterIcons.dueDate.menuOpen = setIsDateFilterOpen;
+	filterIcons.categories.menuOpen = setIsCategoryFilterOpen;
+
 	const searchParams = useSearchParams();
 	const updateSearchParams = useUpdateSearchParams();
 	const [filterField, filterValue] = searchParams.get('filter')?.split(':') || [];
 	const Icon = filterIcons[filterField] ?? Filter;
+
+	const getMenuOpener = (field: string) => {
+		if (field === 'dueDate') return setIsDateFilterOpen;
+		if (field === 'categories') return setIsCategoryFilterOpen;
+	};
+
+	const handleMenuToggle = () => {
+		const menuOpener = getMenuOpener(filterField);
+		if (menuOpener) menuOpener(prev => !prev);
+	};
 
 	return (
 		<div className="relative">
 			{filterField ? (
 				<div className="flex items-center text-xs gap-1 p-1 bg-slate-200 text-slate-700 rounded-lg border border-slate-300 hover:border-slate-400">
 					<p className=" pl-1">Filter by:</p>
-					<Button ariaLabel="Change Filter" className="flex items-center gap-1 hover:bg-slate-300 active:bg-slate-400">
+					<Button
+						ariaLabel="Change Filter"
+						onClick={handleMenuToggle}
+						className="flex items-center gap-1 hover:bg-slate-300 active:bg-slate-400"
+					>
 						<Icon size={12} />
 						<p>{filterField === 'categories' ? 'Category' : filterValue}</p>
 					</Button>
