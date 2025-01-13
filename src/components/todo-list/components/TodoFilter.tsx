24eedf90a86bf1cd4fb6b6dc63bcf4c5c3ad1cee
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-13 17:52:06 +0800

    Refactor TodoFilter component: Replace filter display with a Button for improved user interaction and accessibility. This change enhances the overall user experience by making the filter options more interactive and visually appealing.

--- src/components/todo-list/components/TodoFilter.tsx
+++ src/components/todo-list/components/TodoFilter.tsx
@@ -28,8 +28,10 @@ export default function TodoFilter({ initialCategories }: { initialCategories: C
 			{filterField ? (
 				<div className="flex items-center text-xs gap-1 p-1 bg-slate-200 text-slate-700 rounded-lg border border-slate-300 hover:border-slate-400">
 					<p className=" pl-1">Filter by:</p>
-					<Icon size={12} />
-					<p>{filterField === 'categories' ? 'Category' : filterValue}</p>
+					<Button ariaLabel="Change Filter" className="flex items-center gap-1 hover:bg-slate-300 active:bg-slate-400">
+						<Icon size={12} />
+						<p>{filterField === 'categories' ? 'Category' : filterValue}</p>
+					</Button>
 				</div>
 			) : (
 				<>
