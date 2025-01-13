eab3132fcf485774d36184027221427ba62d9d83
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-13 17:26:57 +0800

    Refactor TodoFilter component: Replace Menu with FilterMenu for improved filter management. This change enhances code organization and user experience by streamlining the filter functionality and consolidating filter options into a dedicated component.

--- src/components/todo-list/components/TodoFilter.tsx
+++ src/components/todo-list/components/TodoFilter.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import { Button } from '@/components/ui-shared/Button';
-import Menu from '@/components/ui-shared/Menu';
-import MenuItem from '@/components/ui-shared/MenuItem';
 import { Category } from '@/types';
-import { CalendarDays, Filter, Tag } from 'lucide-react';
+import { Filter } from 'lucide-react';
 import { useState } from 'react';
 import CategoryFilterMenu from '../ui/CategoryFilterMenu';
 import DateFilterMenu from '../ui/DateFilterMenu';
+import FilterMenu from '../ui/FilterMenu';
 
 export default function TodoFilter({ initialCategories }: { initialCategories: Category[] }) {
 	const [isFilterMenuOpen, setIsFilterMenuOpen] = useState(false);
@@ -19,35 +18,13 @@ export default function TodoFilter({ initialCategories }: { initialCategories: C
 			<Button ariaLabel="Filter" onClick={() => setIsFilterMenuOpen(prev => !prev)}>
 				<Filter size={20} className="text-slate-600" />
 			</Button>
-			<Menu
-				open={isFilterMenuOpen}
-				onClose={() => setIsFilterMenuOpen(false)}
-				posX="-right-5"
-				posXNotch="before:right-6"
-				width="w-44"
-			>
-				<MenuItem className="border-b border-gray-200 font-bold" clickable={false}>
-					<p>Filter by</p>
-				</MenuItem>
-				<MenuItem
-					onClick={() => {
-						setIsCategoryFilterOpen(true);
-						setIsFilterMenuOpen(false);
-					}}
-				>
-					<Tag className="text-slate-600" size={18} />
-					<p>Categories</p>
-				</MenuItem>
-				<MenuItem
-					onClick={() => {
-						setIsDateFilterOpen(true);
-						setIsFilterMenuOpen(false);
-					}}
-				>
-					<CalendarDays className="text-slate-600" size={18} />
-					<p>Due Date</p>
-				</MenuItem>
-			</Menu>
+			<FilterMenu
+				isOpen={isFilterMenuOpen}
+				setIsOpen={setIsFilterMenuOpen}
+				setIsCategoryFilterOpen={setIsCategoryFilterOpen}
+				setIsDateFilterOpen={setIsDateFilterOpen}
+			/>
+
 			<CategoryFilterMenu
 				initialCategories={initialCategories}
 				isOpen={isCategoryFilterOpen}
---
+++ src/components/todo-list/ui/FilterMenu.tsx
@@ -0,0 +1,42 @@
+import Menu from '@/components/ui-shared/Menu';
+import MenuItem from '@/components/ui-shared/MenuItem';
+import { MenuOpenProps } from '@/types';
+import { CalendarDays, Tag } from 'lucide-react';
+
+interface FilterMenuProps extends MenuOpenProps {
+	setIsCategoryFilterOpen: (val: boolean) => void;
+	setIsDateFilterOpen: (val: boolean) => void;
+}
+
+export default function FilterMenu({
+	isOpen,
+	setIsOpen,
+	setIsCategoryFilterOpen,
+	setIsDateFilterOpen,
+}: FilterMenuProps) {
+	return (
+		<Menu open={isOpen} onClose={() => setIsOpen(false)} posX="-right-5" posXNotch="before:right-6" width="w-44">
+			<MenuItem className="border-b border-gray-200 font-bold" clickable={false}>
+				<p>Filter by</p>
+			</MenuItem>
+			<MenuItem
+				onClick={() => {
+					setIsCategoryFilterOpen(true);
+					setIsOpen(false);
+				}}
+			>
+				<Tag className="text-slate-600" size={18} />
+				<p>Categories</p>
+			</MenuItem>
+			<MenuItem
+				onClick={() => {
+					setIsDateFilterOpen(true);
+					setIsOpen(false);
+				}}
+			>
+				<CalendarDays className="text-slate-600" size={18} />
+				<p>Due Date</p>
+			</MenuItem>
+		</Menu>
+	);
+}
