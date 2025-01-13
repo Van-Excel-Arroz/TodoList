faccf01e5c98d12dcb85fdb79c2b919a24080aca
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-13 16:03:50 +0800

    Update DueDate component: Change text color for overdue dates from 'text-red-500' to 'text-red-700' to enhance visual distinction of past due items.

--- src/components/todo-list/ui/DueDate.tsx
+++ src/components/todo-list/ui/DueDate.tsx
@@ -7,7 +7,7 @@ interface DueDateProps {
 }
 
 export default function DueDate({ dueDatetime, textSize = 'xs' }: DueDateProps) {
-	const textStyle = `text-${textSize} ${dueDatetime && isPast(dueDatetime) ? 'text-red-500' : 'text-slate-800'}`;
+	const textStyle = `text-${textSize} ${dueDatetime && isPast(dueDatetime) ? 'text-red-700' : 'text-slate-800'}`;
 
 	return (
 		<div className="flex items-center gap-1">
