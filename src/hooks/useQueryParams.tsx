31d1efaed4924cdb54188eaa1cafa28c684584a5
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-14 17:25:14 +0800

    Remove unused hooks: Delete useQueryParam and useUpdateSearchParams hooks to streamline codebase. This change enhances maintainability by eliminating redundant code and aligns with recent refactoring efforts across related components.

--- src/hooks/useQueryParams.tsx
+++ src/hooks/useQueryParams.tsx
@@ -1,14 +1,21 @@
 import { useSearchParams, useRouter } from 'next/navigation';
 
-export default function useUpdateSearchParams() {
-	const router = useRouter();
+export default function useQueryParams() {
 	const searchParams = useSearchParams();
+	const router = useRouter();
 
+	const getQueryParam = (paramName: string) => {
+		const value = searchParams.get(paramName);
+		return value?.split(':') || [];
+	};
 	const updateSearchParams = (field: string, value: string | null) => {
 		const params = new URLSearchParams(searchParams.toString());
 		value ? params.set(`${field}`, value) : params.delete(`${field}`);
 		router.push(`/tasks/?${params.toString()}`);
 	};
 
-	return updateSearchParams;
+	return {
+		getQueryParam,
+		updateSearchParams,
+	};
 }
