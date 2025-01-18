7041787ed1a31deeaf48bf1c3c71b90f254feda0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-18 10:50:26 +0800

    Refactor DueDate component: Remove unused date-fns imports and simplify due date display logic. This change enhances code clarity by focusing on relevant functionality and improving maintainability in line with recent refactoring efforts across related components.

--- src/components/todo-list/ui/DueDate.tsx
+++ src/components/todo-list/ui/DueDate.tsx
@@ -1,5 +1,5 @@
-import { format, formatDistanceToNow, isPast, isThisYear, isToday, isTomorrow } from 'date-fns';
-import { Calendar, Clock3 } from 'lucide-react';
+import { formatDistanceToNow, isPast } from 'date-fns';
+import { Clock3 } from 'lucide-react';
 
 interface DueDateProps {
 	dueDatetime: string;
@@ -11,21 +11,8 @@ export default function DueDate({ dueDatetime, textSize = 'xs' }: DueDateProps) 
 
 	return (
 		<div className="flex items-center gap-1">
-			{/* <Calendar size={12} className="text-slate-800" />
-			<p className={textStyle}>
-				{dueDatetime
-					? isToday(dueDatetime)
-						? 'Today' + format(dueDatetime, ` \'at\'`)
-						: isTomorrow(dueDatetime)
-						? 'Tomorrow' + format(dueDatetime, ` \'at\'`)
-						: isThisYear(dueDatetime)
-						? format(dueDatetime, `EEE, MMMM d \'at\'`)
-						: format(dueDatetime, `EEE, MMMM d yyyy \'at\'`)
-					: '-'}
-			</p> */}
 			<Clock3 size={12} className="text-slate-800" />
-			{/* <p className={textStyle}>{dueDatetime ? format(dueDatetime, 'h:mm a') : ''}</p> */}
-			<p className={textStyle}>{!isPast(dueDatetime) ? `${formatDistanceToNow(dueDatetime)} Left` : 'Past Due'} </p>
+			<p className={textStyle}>{!isPast(dueDatetime) ? `${formatDistanceToNow(dueDatetime)} Left` : `Past due`}</p>
 		</div>
 	);
 }
