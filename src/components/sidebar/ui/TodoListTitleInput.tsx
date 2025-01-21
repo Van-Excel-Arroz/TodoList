75439b74cb0a492976d4856c0f20a11b00cbb413
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-21 17:43:36 +0800

    Update TodoListTitleInput component: Modify input field hover and focus border colors for improved visibility. This change enhances the user experience by providing clearer visual feedback during interactions, contributing to a more accessible interface.

--- src/components/sidebar/ui/TodoListTitleInput.tsx
+++ src/components/sidebar/ui/TodoListTitleInput.tsx
@@ -6,7 +6,7 @@ export default function TodoListTitleInput({ register }: { register: any }) {
 			})}
 			type="text"
 			placeholder="New list"
-			className=" rounded-lg p-2 w-full border border-slate-300 hover:border-slate-400 focus:border-slate-400 focus:outline-none"
+			className=" rounded-lg p-2 w-full border border-slate-300 hover:border-slate-500 focus:border-slate-500 focus:outline-none"
 		/>
 	);
 }
