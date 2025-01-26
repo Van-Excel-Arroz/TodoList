7f50e4d5a1859169ea16541e844bb74337f1523b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-26 09:18:50 +0800

    Remove commented-out Navbar import and header in tasks layout

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -1,6 +1,5 @@
 import TodoDetailsPanel from '@/components/details-panel/TodoDetailsPanel';
 import TodoListsSidebar from '@/components/sidebar/TodoListsSidebar';
-import Navbar from '@/components/ui-shared/Navbar';
 
 interface LayoutProps {
 	children: React.ReactNode;
@@ -9,16 +8,11 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps): JSX.Element {
 	return (
 		<div className="flex flex-col h-screen overflow-hidden">
-			{/* <header className="sticky top-0 left-0 z-20 w-full">
-				<Navbar />
-			</header> */}
 			<div className="flex flex-1 overflow-hidden">
 				<aside className="absolute lg:sticky top-0 left-0 z-30">
 					<TodoListsSidebar />
 				</aside>
-
 				<main className="w-full z-10">{children}</main>
-
 				<aside className="absolute lg:sticky top-0 right-0 z-40">
 					<TodoDetailsPanel />
 				</aside>
