e943bf4c09a984fc83575f6d2b8233d4b6854939
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-29 13:56:38 +0800

    Update NewTodolistButton with dark mode prop

--- src/components/sidebar/components/NewTodolistButton.tsx
+++ src/components/sidebar/components/NewTodolistButton.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui-shared/Button';
 
 export default function NewTodolistButton() {
 	return (
-		<Button ariaLabel="add new list" className="bg-slate-700 text-white w-10/12 h-9 mx-auto">
+		<Button ariaLabel="add new list" darkMode={true} className="w-full h-9 mx-auto">
 			New List
 		</Button>
 	);
