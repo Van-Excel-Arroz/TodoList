d2cba29016bde2afe56a4d586fa297553bf2d0b0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-01-30 19:05:45 +0800

    Implement dynamic NewTodolistButton with TodolistForm toggle

--- src/components/sidebar/components/NewTodolistButton.tsx
+++ src/components/sidebar/components/NewTodolistButton.tsx
@@ -1,11 +1,26 @@
 'use client';
 
 import { Button } from '@/components/ui-shared/Button';
+import { useState } from 'react';
+import TodolistForm from './TodolistForm';
 
 export default function NewTodolistButton() {
+	const [isAddingList, setIsAddingList] = useState(false);
+
 	return (
-		<Button ariaLabel="add new list" darkMode={true} className="w-full h-9 mx-auto">
-			New List
-		</Button>
+		<>
+			{isAddingList ? (
+				<TodolistForm />
+			) : (
+				<Button
+					ariaLabel="add new list"
+					darkMode={true}
+					onClick={() => setIsAddingList(prev => !prev)}
+					className="w-full h-9 mx-auto"
+				>
+					New List
+				</Button>
+			)}
+		</>
 	);
 }
