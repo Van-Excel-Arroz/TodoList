063f063f65d9e7df8a35f743cbd692ef08ca1dc8
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-08 17:20:59 +0800

    Remove updated date from TodoDetailsFooter

--- src/components/details-panel/components/TodoDetailsFooter.tsx
+++ src/components/details-panel/components/TodoDetailsFooter.tsx
@@ -7,7 +7,6 @@ export default function TodoDetailsFooter({ creationDate }: { creationDate: stri
 		<div className="sticky bottom-0 py-5 border-t border-slate-300 w-full flex items-center justify-between bg-white">
 			<div className="flex items-center justify-between text-xs text-slate-600 w-full">
 				<p>Created: {format(creationDate, 'MMM dd, yyyy')} </p>
-				<p>Updated: {format(creationDate, 'MMM dd, yyyy')}</p>
 			</div>
 		</div>
 	);
