10cccf8f7efde57777dac7c9d69d846113fbd71b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-08 17:27:30 +0800

    Add home button to TodoListsHeader

--- src/components/sidebar/components/TodoListsHeader.tsx
+++ src/components/sidebar/components/TodoListsHeader.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-
 import TodoListsSidebarToggle from '../ui/TodoListsSidebarToggle';
+import { Home } from 'lucide-react';
+import { Button } from '@/components/ui-shared/Button';
 
 export default function TodoListsHeader() {
 	const pathname = usePathname();
@@ -10,7 +11,11 @@ export default function TodoListsHeader() {
 
 	return (
 		<div className="flex items-center justify-between py-4 border-b border-slate-300">
-			<div className="flex flex-col"></div>
+			<div className="flex items-center gap-2">
+				<Button ariaLabel="Home">
+					<Home />
+				</Button>
+			</div>
 			{!isHome && <TodoListsSidebarToggle />}
 		</div>
 	);
