25da597a0430190f90ccd05b667e5f8306ff6388
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-11 07:43:46 +0800

    Reset selected todo when navigating to home page

--- src/components/sidebar/components/TodoListsHeader.tsx
+++ src/components/sidebar/components/TodoListsHeader.tsx
@@ -5,14 +5,16 @@ import TodoListsSidebarToggle from '../ui/TodoListsSidebarToggle';
 import { Home } from 'lucide-react';
 import { Button } from '@/components/ui-shared/Button';
 import Link from 'next/link';
+import useSelectedTodoIdStore from '@/context/SelectedTodoIdContext';
 
 export default function TodoListsHeader() {
 	const pathname = usePathname();
 	const isHome = pathname === '/tasks/home';
+	const { setSelectedTodoId } = useSelectedTodoIdStore();
 
 	return (
 		<div className="flex items-center justify-between py-4 border-b border-slate-300">
-			<Link href={'/'} className="flex items-center gap-2">
+			<Link href={'/'} className="flex items-center gap-2" onClick={() => setSelectedTodoId(0)}>
 				<Button ariaLabel="Home">
 					<Home />
 				</Button>
