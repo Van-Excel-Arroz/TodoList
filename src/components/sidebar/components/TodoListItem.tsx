d37957630e035e6880dbad3c9f9b200ea72b316a
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-14 07:55:33 +0800

    Remove edit and delete buttons from TodoListItem

--- src/components/sidebar/components/TodoListItem.tsx
+++ src/components/sidebar/components/TodoListItem.tsx
@@ -1,26 +1,20 @@
 'use client';
 
-import { memo, useEffect, useState } from 'react';
+import { memo, useState } from 'react';
 import Link from 'next/link';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { deleteTodolistAction } from '@/actions/todolist-action';
+import { useSearchParams } from 'next/navigation';
 import { TodoList } from '@/types';
-import { Pencil, Trash2 } from 'lucide-react';
 import useTodoListsSidebarStore from '@/context/TodoListsSidebarContext';
 import useSelectedTodoIdStore from '@/context/SelectedTodoIdContext';
-import useTodoListsStore from '@/context/TodoListsContext';
-import { Button } from '@/components/ui-shared/Button';
 import EditTodoListForm from '../ui/EditTodoListForm';
 
 function TodoListItem({ todolist }: { todolist: TodoList }) {
-	const router = useRouter();
 	const searchParams = useSearchParams();
 	const currentId = searchParams.get('id');
 	const isSelectedPath = currentId === todolist.id.toString();
 	const [isEditing, setIsEditing] = useState(false);
 	const { setSelectedTodoId } = useSelectedTodoIdStore();
 	const { toggleTodoListsSidebar } = useTodoListsSidebarStore();
-	const { deleteTodolist } = useTodoListsStore();
 	const urlWithSearchParams = localStorage.getItem(`searchParams-${todolist.id}`);
 
 	const handleClick = () => {
@@ -33,15 +27,6 @@ function TodoListItem({ todolist }: { todolist: TodoList }) {
 
 	const handleEdit = (val: boolean) => {
 		setIsEditing(val);
-	};
-
-	const handleOnSubmit = async () => {
-		await deleteTodolistAction(todolist.id);
-		deleteTodolist(todolist.id);
-		if (currentId === todolist.id.toString()) {
-			setSelectedTodoId(0);
-			router.push('/tasks/');
-		}
 	};
 
 	const handleInputBlur = (e: React.FocusEvent<HTMLDivElement>) => {
@@ -72,17 +57,6 @@ function TodoListItem({ todolist }: { todolist: TodoList }) {
 				>
 					{todolist.title}
 				</Link>
-			)}
-
-			{!isEditing && (
-				<div className="flex items-center gap-2 opacity-0 group-hover:opacity-100 absolute right-3">
-					<Button onClick={() => handleEdit(true)} ariaLabel="Edit Todolist Title">
-						<Pencil size={15} />
-					</Button>
-					<Button type="submit" ariaLabel="Delete Todolist" onClick={handleOnSubmit}>
-						<Trash2 size={15} />
-					</Button>
-				</div>
 			)}
 		</div>
 	);
