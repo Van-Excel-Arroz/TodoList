da3d8ee5a51baa5656e47fc5af33440dc6bebd96
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-15 14:51:37 +0800

    Add toggle button and improve SmartTodolistContainer header layout

--- src/components/sidebar/components/SmartTodolistContainer.tsx
+++ src/components/sidebar/components/SmartTodolistContainer.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { Button } from '@/components/ui-shared/Button';
+import { ChevronDown } from 'lucide-react';
+
 export default function SmartTodolistContainer() {
 	return (
 		<div className="flex items-center gap-2 mb-2 w-full mx-auto">
-			<p>Smart Lists</p> <hr className="border border-slate-200 w-full" />
+			<Button ariaLabel="Toggle Show List Container">
+				<ChevronDown size={20} />
+			</Button>
+			<p className="mr-2">Smart Lists</p> <hr className="border border-slate-200 w-full" />
 		</div>
 	);
 }
