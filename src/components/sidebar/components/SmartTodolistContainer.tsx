8cfe5b032e86d33517283faa023b3e5074ae1e89
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-15 14:55:41 +0800

    Prepare SmartTodolistContainer for future list content

--- src/components/sidebar/components/SmartTodolistContainer.tsx
+++ src/components/sidebar/components/SmartTodolistContainer.tsx
@@ -5,11 +5,14 @@ import { ChevronDown } from 'lucide-react';
 
 export default function SmartTodolistContainer() {
 	return (
-		<div className="flex items-center gap-2 mb-2 w-full mx-auto">
-			<Button ariaLabel="Toggle Show List Container">
-				<ChevronDown size={20} />
-			</Button>
-			<p className="mr-2">Smart Lists</p> <hr className="border border-slate-200 w-full" />
-		</div>
+		<>
+			<div className="flex items-center gap-2 mb-2 w-full mx-auto">
+				<Button ariaLabel="Toggle Show List Container">
+					<ChevronDown size={20} />
+				</Button>
+				<p className="mr-2">Smart Lists</p> <hr className="border border-slate-200 w-full" />
+			</div>
+			<div></div>
+		</>
 	);
 }
