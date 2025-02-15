c42127b75e9cc4a11eb9dc9cb1913be6e95c2579
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-15 14:48:56 +0800

    Integrate SmartTodolistContainer into TodoListsSidebar

--- src/components/sidebar/TodoListsSidebar.tsx
+++ src/components/sidebar/TodoListsSidebar.tsx
@@ -4,6 +4,7 @@ import TodoListsSidebarWrapper from './components/TodoListsSidebarWrapper';
 import TodoListsHeader from './components/TodoListsHeader';
 import TodolistContainer from './components/TodolistContainer';
 import NewTodolistButton from './components/NewTodolistButton';
+import SmartTodolistContainer from './components/SmartTodolistContainer';
 
 export default async function TodoListsSidebar() {
 	const todolists: TodoList[] = (await getTodolists(1)) ?? [];
@@ -12,6 +13,7 @@ export default async function TodoListsSidebar() {
 			<TodoListsSidebarWrapper>
 				<div className="flex flex-col gap-4 px-6">
 					<TodoListsHeader />
+					<SmartTodolistContainer />
 					<TodolistContainer initialTodoLists={todolists} />
 					<NewTodolistButton />
 				</div>
