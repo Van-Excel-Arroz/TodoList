a55da9ecc3e7d249ed884d9038540cfb77c45357
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-15 14:58:39 +0800

    Add star icon to SmartTodolistContainer and adjust layout

--- src/components/sidebar/components/SmartTodolistContainer.tsx
+++ src/components/sidebar/components/SmartTodolistContainer.tsx
@@ -1,18 +1,22 @@
 'use client';
 
 import { Button } from '@/components/ui-shared/Button';
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, Star } from 'lucide-react';
 
 export default function SmartTodolistContainer() {
 	return (
 		<>
-			<div className="flex items-center gap-2 mb-2 w-full mx-auto">
-				<Button ariaLabel="Toggle Show List Container">
-					<ChevronDown size={20} />
-				</Button>
-				<p className="mr-2">Smart Lists</p> <hr className="border border-slate-200 w-full" />
+			<div className="flex flex-col items-start gap-2">
+				<div className="flex items-center w-full mx-auto">
+					<Button ariaLabel="Toggle Show List Container">
+						<ChevronDown size={20} />
+					</Button>
+					<p className="mr-2">Smart Lists</p> <hr className="border border-slate-200 w-full" />
+				</div>
+				<div className="py-1">
+					<Star size={20} />
+				</div>
 			</div>
-			<div></div>
 		</>
 	);
 }
