fa74cd95d0bb7964f485314f9b28dbfa146d8912
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-15 15:08:00 +0800

    Simplify SmartTodolistContainer markup and remove unnecessary fragment

--- src/components/sidebar/components/SmartTodolistContainer.tsx
+++ src/components/sidebar/components/SmartTodolistContainer.tsx
@@ -5,19 +5,17 @@ import { ChevronDown, Star } from 'lucide-react';
 
 export default function SmartTodolistContainer() {
 	return (
-		<>
-			<div className="flex flex-col items-start gap-2">
-				<div className="flex items-center w-full mx-auto">
-					<Button ariaLabel="Toggle Show List Container">
-						<ChevronDown size={20} />
-					</Button>
-					<p className="mr-2">Smart Lists</p> <hr className="border border-slate-200 w-full" />
-				</div>
-				<div className="flex items-center gap-2 py-1 px-2">
-					<Star className="text-slate-600" size={20} />
-					<p>Priority</p>
-				</div>
+		<div className="flex flex-col items-start gap-1">
+			<div className="flex items-center w-full mx-auto">
+				<Button ariaLabel="Toggle Show List Container">
+					<ChevronDown size={20} />
+				</Button>
+				<p className="mr-2">Smart Lists</p> <hr className="border border-slate-200 w-full" />
 			</div>
-		</>
+			<div className="flex items-center gap-2 py-1 px-2">
+				<Star className="text-slate-600" size={20} />
+				<p>Priority</p>
+			</div>
+		</div>
 	);
 }
