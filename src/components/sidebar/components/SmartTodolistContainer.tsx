e89accbcfcde8b0d7b563c89ccf1e90a38ace9c4
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-15 14:47:56 +0800

    Create SmartTodolistContainer component

---
+++ src/components/sidebar/components/SmartTodolistContainer.tsx
@@ -0,0 +1,7 @@
+export default function SmartTodolistContainer() {
+	return (
+		<div className="flex items-center gap-2 mb-2 w-full mx-auto">
+			<p>Smart Lists</p> <hr className="border border-slate-200 w-full" />
+		</div>
+	);
+}
