581943434970fbd25ca914f929742a421f5213a0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-16 13:14:12 +0800

    Refactor ExpandableSection component type and imports

--- src/components/sidebar/ui/ExpandableSection.tsx
+++ src/components/sidebar/ui/ExpandableSection.tsx
@@ -1,42 +1,35 @@
-import { Button } from "@/components/ui-shared/Button"
-import { ChevronDown } from "lucide-react"
-import {motion} from 'framer-motion'
+import { Button } from '@/components/ui-shared/Button';
+import { ChevronDown } from 'lucide-react';
+import { motion } from 'framer-motion';
+import React from 'react';
 
-interface ExpandableSectionProps{
-  children: React.ReactNode
-  isOpen: boolean:
-  setIsOpen: (prev: boolean) => boolean
-  isEmpty: boolean
+interface ExpandableSectionProps {
+	children: React.ReactNode;
+	isOpen: boolean;
+	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	isEmpty: boolean;
 }
 
-export default function ExpandableSection({children, isOpen, setIsOpen, isEmpty}: ExpandableSectionProps) {
-  return (
-    <div className="flex flex-col items-start gap-1 w-full mx-auto">
-						<div className="flex items-center w-full mx-auto">
-							<Button
-								ariaLabel="Toggle Todo Section"
-								onClick={() => setIsOpen(prev => !prev)}
-								disabled={isEmpty}
-							>
-								<div
-									className={`transition-transform duration-200 ease-in-out ${
-										isOpen && !isEmpty ? 'rotate-180' : ''
-									}`}
-								>
-									<ChevronDown size={20} />
-								</div>
-							</Button>
-							<p className="mr-3 ml-1 text-slate-600">Lists</p> <hr className="border border-slate-200 w-full" />
-						</div>
-
-						<motion.div
-							className="overflow-hidden"
-							initial={{ height: isOpen && !isEmpty ? 'auto' : 0 }}
-							animate={{ height: isOpen && !isEmpty ? 'auto' : 0 }}
-							transition={{ duration: 0.25 }}
-						>
-            {children}
-						</motion.div>
+export default function ExpandableSection({ children, isOpen, setIsOpen, isEmpty }: ExpandableSectionProps) {
+	return (
+		<div className="flex flex-col items-start gap-1 w-full mx-auto">
+			<div className="flex items-center w-full mx-auto">
+				<Button ariaLabel="Toggle Todo Section" onClick={() => setIsOpen(prev => !prev)} disabled={isEmpty}>
+					<div className={`transition-transform duration-200 ease-in-out ${isOpen && !isEmpty ? 'rotate-180' : ''}`}>
+						<ChevronDown size={20} />
 					</div>
-  )
-};
+				</Button>
+				<p className="mr-3 ml-1 text-slate-600">Lists</p> <hr className="border border-slate-200 w-full" />
+			</div>
+
+			<motion.div
+				className="overflow-hidden"
+				initial={{ height: isOpen && !isEmpty ? 'auto' : 0 }}
+				animate={{ height: isOpen && !isEmpty ? 'auto' : 0 }}
+				transition={{ duration: 0.25 }}
+			>
+				{children}
+			</motion.div>
+		</div>
+	);
+}
