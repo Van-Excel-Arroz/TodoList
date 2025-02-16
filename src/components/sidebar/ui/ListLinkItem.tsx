a5621bd662112e03bb80bb4e76ed2d61d5699d71
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-16 14:40:48 +0800

    Adjust ListLinkItem transition duration

--- src/components/sidebar/ui/ListLinkItem.tsx
+++ src/components/sidebar/ui/ListLinkItem.tsx
@@ -38,7 +38,7 @@ export default function ListLinkItem({ children, queryParam, value, Icon }: List
 
 	return (
 		<div
-			className={`flex items-center relative mx-auto transition-all rounded-md duration-200  ${
+			className={`flex items-center relative mx-auto rounded-md transition-all duration-100  ${
 				isSelectedPath
 					? 'border-slate-500 bg-slate-200'
 					: 'border-slate-200 hover:bg-slate-200 hover:border-slate-300 active:bg-slate-300'
