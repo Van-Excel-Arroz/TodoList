0e756925655b7e6c7118f58bbe43d26ff461ec33
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-16 14:22:42 +0800

    Refactor TodoSection to use ExpandableSection component

--- src/components/todo-list/components/TodoSection.tsx
+++ src/components/todo-list/components/TodoSection.tsx
@@ -2,11 +2,10 @@
 
 import { Todo } from '@/types';
 import TodoItem from './TodoItem';
-import { ChevronDown } from 'lucide-react';
-import { Button } from '@/components/ui-shared/Button';
-import { useState, useMemo, memo } from 'react';
+import { useMemo, memo } from 'react';
 import { motion } from 'framer-motion';
 import { itemVariants } from '@/utils/framer-motion';
+import ExpandableSection from '@/components/sidebar/ui/ExpandableSection';
 
 interface TodoSectionProps {
 	title: string;
@@ -14,31 +13,11 @@ interface TodoSectionProps {
 }
 
 function TodoSection({ title, todos }: TodoSectionProps) {
-	const [isOpen, setIsOpen] = useState<boolean>(true);
 	const isTodosEmpty = useMemo(() => todos.length === 0, [todos]);
 
 	return (
 		<>
-			<div className="flex items-center py-2 px-6 gap-2">
-				<Button ariaLabel="Toggle Todo Section" onClick={() => setIsOpen(prev => !prev)} disabled={isTodosEmpty}>
-					<div
-						className={`transition-transform duration-200 ease-in-out ${isOpen && !isTodosEmpty ? 'rotate-180' : ''}`}
-					>
-						<ChevronDown size={20} />
-					</div>
-				</Button>
-				<div className="flex items-center w-full">
-					<p className="font-semibold mr-3 text-nowrap">{title}</p>
-					<hr className="border border-slate-200 w-full" />
-				</div>
-			</div>
-
-			<motion.div
-				className="overflow-hidden"
-				initial={{ height: isOpen && !isTodosEmpty ? 'auto' : 0 }}
-				animate={{ height: isOpen && !isTodosEmpty ? 'auto' : 0 }}
-				transition={{ duration: 0.25 }}
-			>
+			<ExpandableSection isEmpty={isTodosEmpty} title={title}>
 				<ul>
 					{todos.map((todo, index) => (
 						<motion.li key={index} layout variants={itemVariants} initial="initial" animate="animate">
@@ -46,7 +25,7 @@ function TodoSection({ title, todos }: TodoSectionProps) {
 						</motion.li>
 					))}
 				</ul>
-			</motion.div>
+			</ExpandableSection>
 		</>
 	);
 }
