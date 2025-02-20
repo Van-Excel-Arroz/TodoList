1c2a2400bc2410e4d422c0b79b671c5753d5de77
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-20 19:46:05 +0800

    Enhance PriorityPage to display important todos within todolists

--- src/components/priority/PriorityPage.tsx
+++ src/components/priority/PriorityPage.tsx
@@ -1,4 +1,5 @@
 import { getImportantTodos } from '@/lib/todo';
+import { Todo } from '@/utils/types';
 
 export default async function PriorityPage() {
 	const important_todos = await getImportantTodos();
@@ -7,7 +8,12 @@ export default async function PriorityPage() {
 		<div className="w-[98%] mx-auto">
 			<p className="text-lg font-bold">Priority Page</p>
 			{important_todos.map(important_todo => (
-				<p key={important_todo.id}>{important_todo.title}</p>
+				<div key={important_todo.id} className="font-bold">
+					<p>{important_todo.title}</p>
+					{important_todo.importantTodos.map((todo: Todo) => (
+						<p className="pl-5">{todo.task_text}</p>
+					))}
+				</div>
 			))}
 		</div>
 	);
