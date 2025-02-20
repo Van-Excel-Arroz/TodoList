38a81a41c2b0c469cfa7c6381dc70844b2878657
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-20 19:26:42 +0800

    Render important todos on PriorityPage using getImportantTodos

--- src/components/priority/PriorityPage.tsx
+++ src/components/priority/PriorityPage.tsx
@@ -1,7 +1,14 @@
-export default function PriorityPage() {
+import { getImportantTodos } from '@/lib/todo';
+
+export default async function PriorityPage() {
+	const important_todos = await getImportantTodos();
+
 	return (
 		<div className="w-[98%] mx-auto">
 			<p className="text-lg font-bold">Priority Page</p>
+			{important_todos.map(important_todo => (
+				<p key={important_todo.id}>{important_todo.todlist_title}</p>
+			))}
 		</div>
 	);
 }
