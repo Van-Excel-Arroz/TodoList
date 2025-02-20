9ce8a2f931eaa696ff027507f3fe0f7493d6f3d1
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-20 19:27:57 +0800

    Fix typo in todolist_title property on PriorityPage

--- src/components/priority/PriorityPage.tsx
+++ src/components/priority/PriorityPage.tsx
@@ -2,12 +2,13 @@ import { getImportantTodos } from '@/lib/todo';
 
 export default async function PriorityPage() {
 	const important_todos = await getImportantTodos();
+	console.log(important_todos);
 
 	return (
 		<div className="w-[98%] mx-auto">
 			<p className="text-lg font-bold">Priority Page</p>
 			{important_todos.map(important_todo => (
-				<p key={important_todo.id}>{important_todo.todlist_title}</p>
+				<p key={important_todo.id}>{important_todo.todolist_title}</p>
 			))}
 		</div>
 	);
