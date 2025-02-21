c16b28b3c21b67ea4823868342b31d5cd9f96371
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-21 19:11:58 +0800

    Remove unused id prop from PriorityContainer

--- src/components/priority/component/PriorityContainer.tsx
+++ src/components/priority/component/PriorityContainer.tsx
@@ -3,7 +3,7 @@
 import ExpandableSection from '@/components/ui-shared/ExpandableSection';
 import { TodoListWithImportantTodos } from '@/utils/types';
 
-export default function PriorityContainer({ title, id, importantTodos }: TodoListWithImportantTodos) {
+export default function PriorityContainer({ title, importantTodos }: TodoListWithImportantTodos) {
 	return (
 		<ExpandableSection isEmpty={false} title={title}>
 			{importantTodos.map((todo, index) => (
