ccc0e979d2d686767a391c5684a35a50704dad47
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-22 13:42:56 +0800

    Add todos to TodosContext in PriorityMain component

--- src/components/priority/component/PriorityMain.tsx
+++ src/components/priority/component/PriorityMain.tsx
@@ -2,13 +2,24 @@
 
 import TodoItem from '@/components/ui-shared/TodoItem';
 import ExpandableSection from '@/components/ui-shared/ExpandableSection';
-import { TodoListWithImportantTodos } from '@/utils/types';
+import { Todo, TodoListWithImportantTodos } from '@/utils/types';
+import { useEffect } from 'react';
+import useTodosStore from '@/context/TodosContext';
 
 interface PriorityMainProps {
 	importantTodosWithTodoList: TodoListWithImportantTodos[];
 }
 
 export default function PriorityMain({ importantTodosWithTodoList }: PriorityMainProps) {
+	const { setTodos } = useTodosStore();
+
+	useEffect(() => {
+		const todos: Todo[] = [];
+		importantTodosWithTodoList.map(todoList => todoList.importantTodos.map(todo => todos.push(todo)));
+
+		setTodos(todos);
+	}, [importantTodosWithTodoList, setTodos]);
+
 	return (
 		<>
 			{importantTodosWithTodoList.map((todoList: TodoListWithImportantTodos) => (
