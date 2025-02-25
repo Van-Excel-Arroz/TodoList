318ececaa0f167bd8c7f6ab03feae1cd5eac5df4
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-02-26 05:52:25 +0800

    Add horizontal padding to PriorityHeader flex container

--- src/components/priority/component/PriorityHeader.tsx
+++ src/components/priority/component/PriorityHeader.tsx
@@ -13,7 +13,7 @@ export default function PriorityHeader() {
 
 	return (
 		<div className="z-50 px-6 bg-white border-b border-slate-300">
-			<div className='flex items-center gap-2'>
+			<div className='flex items-center gap-2 px-1'>
 				{!isTodoListsSidebarOpen ? <TodoListsSidebarToggle /> : null}
 				<p className="text-xl font-bold pt-2">Priority Page</p>
 			</div>
