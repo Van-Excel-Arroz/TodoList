f4bb58a37e2f96c5ebb58f874d129dbea37a4134
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-02 19:22:48 +0800

    Remove PriorityHeader component from Priority page

--- src/components/priority/PriorityPage.tsx
+++ src/components/priority/PriorityPage.tsx
@@ -1,12 +1,9 @@
 import { getImportantTodos } from '@/lib/todo';
-import PriorityHeader from './component/PriorityHeader';
 
 export default async function PriorityPage() {
 	const importantTodosWithTodoList = await getImportantTodos();
 
 	return (
-		<div>
-			<PriorityHeader />
-		</div>
+		<h1>Hello World</h1>
 	);
 }
--- src/components/ui-shared/FilteredTodosHeader.tsx
+++ src/components/ui-shared/FilteredTodosHeader.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui-shared/Button';
 import useTodoListsSidebarStore from '@/context/TodoListsSidebarContext';
 import { Search } from 'lucide-react';
 
-export default function PriorityHeader() {
+export default function FilteredTodosHeader({ title }: { title: string }) {
 	const { isTodoListsSidebarOpen } = useTodoListsSidebarStore();
 
 
@@ -15,7 +15,7 @@ export default function PriorityHeader() {
 		<div className="z-50 px-6 bg-white border-b border-slate-300">
 			<div className='flex items-center gap-2 px-1 pt-2'>
 				{!isTodoListsSidebarOpen ? <TodoListsSidebarToggle /> : null}
-				<p className="text-xl font-bold">Priority</p>
+				<p className="text-xl font-bold">{title}</p>
 			</div>
 			<div className="flex items-center justify-between gap-2">
 				<div className="flex items-center gap-2 w-60 pt-2">
