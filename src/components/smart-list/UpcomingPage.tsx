ffc8825c5b856f050bd43a2d23a378f344cc2b88
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-04 16:06:12 +0800

    Refactor variable name in UpcomingPage component

--- src/components/smart-list/UpcomingPage.tsx
+++ src/components/smart-list/UpcomingPage.tsx
@@ -3,14 +3,14 @@ import FilteredTodosPage from '../ui-shared/FilteredTodosPage';
 import FilteredTodosHeader from '../ui-shared/FilteredTodosHeader';
 
 export default async function UpcomingTodayPage() {
-	const dueTodayTodosWithTodoList = await getDueTodayTodos();
+	const todosWithDue = await getDueTodayTodos();
 
 	return (
 		<>
 			<FilteredTodosHeader title="Upcoming" />
 
-			{dueTodayTodosWithTodoList.length > 0 ? (
-				<FilteredTodosPage filteredTodosWithTodoList={dueTodayTodosWithTodoList} />
+			{todosWithDue.length > 0 ? (
+				<FilteredTodosPage filteredTodosWithTodoList={todosWithDue} />
 			) : (
 				<h1 className="text-center pt-10 text-lg text-slate-700">It looks like you have no todos with due date.</h1>
 			)}
