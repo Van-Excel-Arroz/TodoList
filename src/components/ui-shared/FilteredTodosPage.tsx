30b0221db95404ddb3f39ede4d5fed2ea13fd9f2
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-06 09:04:17 +0800

    Refactor FilteredTodosPage to use incompleteTodos from useTodoDataManagement

--- src/components/ui-shared/FilteredTodosPage.tsx
+++ src/components/ui-shared/FilteredTodosPage.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import useTodosStore from '@/context/TodosContext';
 import { motion } from 'framer-motion';
 import { itemVariants } from '@/utils/framer-motion';
+import { useTodoDataManagement } from '@/hooks/useTodoDataManagement';
 
 interface FilteredTodosPage {
 	filteredTodosWithTodoList: TodoListWithFilteredTodos[];
@@ -14,6 +15,7 @@ interface FilteredTodosPage {
 
 export default function FilteredTodosPage({ filteredTodosWithTodoList }: FilteredTodosPage) {
 	const { todos, setTodos } = useTodosStore();
+	const { incompleteTodos } = useTodoDataManagement(todos);
 
 	useEffect(() => {
 		const todos: Todo[] = [];
@@ -24,12 +26,12 @@ export default function FilteredTodosPage({ filteredTodosWithTodoList }: Filtere
 
 	return (
 		<div className="w-[98%] h-[calc(100vh-100px)] mx-auto mt-3 overflow-y-scroll overflow-x-hidden">
-			<div className='px-5'>
+			<div className="px-5">
 				{filteredTodosWithTodoList.map((todoList, index) => (
 					<ExpandableSection isEmpty={false} title={todoList.title} key={todoList.id}>
 						<ul>
 							<motion.li key={index} layout variants={itemVariants} initial="initial" animate="animate">
-								{todos.map((todo, index) =>
+								{incompleteTodos.map((todo, index) =>
 									todo.todo_list_id === todoList.id ? <TodoItem todo={todo} key={index} /> : null
 								)}
 							</motion.li>
