5977dcb7a4835541a6460db825a6a94ecfd30d34
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-07 19:06:50 +0800

    Add empty state handling for PriorityPage when no important todos exist

--- src/components/smart-list/PriorityPage.tsx
+++ src/components/smart-list/PriorityPage.tsx
@@ -8,7 +8,11 @@ export default async function PriorityPage() {
 	return (
 		<>
 			<FilteredTodosHeader title="Priority" />
-			<FilteredTodosPage filteredTodosWithTodoList={importantTodosWithTodoList} />
+			{importantTodosWithTodoList.length > 0 ? (
+				<FilteredTodosPage filteredTodosWithTodoList={importantTodosWithTodoList} />
+			) : (
+				<h1 className="text-center pt-12 text-lg text-slate-700">It looks like you have no important todos.</h1>
+			)}
 		</>
 	);
 }
