e73967fc13dcfa20f077bbb18ce213ca0cb4f83a
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-08 10:03:35 +0800

    Implement TaggedPage with category-based todo filtering

--- src/components/smart-list/TaggedPage.tsx
+++ src/components/smart-list/TaggedPage.tsx
@@ -1,9 +1,14 @@
+import { getTodosByCategories } from '@/lib/todo';
 import FilteredTodosHeader from '../ui-shared/FilteredTodosHeader';
+import FilteredTodosPage from '../ui-shared/FilteredTodosPage';
 
 export default function TaggedPage() {
+	const todosByCategories = getTodosByCategories();
+
 	return (
 		<>
 			<FilteredTodosHeader title="Tagged" />
+			<FilteredTodosPage filteredTodosWithTodoList={todosByCategories} />
 		</>
 	);
 }
