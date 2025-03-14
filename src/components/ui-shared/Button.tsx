a8c374262e90647d4f625157770efa8834e31e00
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-14 12:59:23 +0800

    Enhance Button component styling by incorporating isActive state for dynamic background color adjustment

--- src/components/ui-shared/Button.tsx
+++ src/components/ui-shared/Button.tsx
@@ -21,10 +21,12 @@ function Button({
 	darkMode = false,
 	isActive = false,
 }: ButtonProps) {
+	const bgStyle = `${isActive ? 'bg-slate-300' : 'bg-white'}`;
+
 	const style = `${
 		darkMode
 			? 'bg-slate-700 text-white hover:bg-slate-500 active:bg-slate-400'
-			: 'hover:bg-slate-200 active:bg-slate-300 text-slate-600'
+			: `hover:bg-slate-200 active:bg-slate-300 text-slate-600 ${bgStyle}`
 	}`;
 
 	return (
