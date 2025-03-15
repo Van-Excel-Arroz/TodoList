947f82d9f882205bde84a6074b217e332dfe3503
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-15 09:49:59 +0800

    Add layout toggle buttons to FilteredTodosHeader for improved user experience

--- src/components/ui-shared/FilteredTodosHeader.tsx
+++ src/components/ui-shared/FilteredTodosHeader.tsx
@@ -4,6 +4,8 @@ import TodoListsSidebarToggle from '@/components/sidebar/ui/TodoListsSidebarTogg
 import TodoFilter from '@/components/todo-list/components/TodoFilter';
 import TodoSort from '@/components/todo-list/components/TodoSort';
 import useTodoListsSidebarStore from '@/context/TodoListsSidebarContext';
+import Button from './Button';
+import { Grid2X2, ListTodo } from 'lucide-react';
 
 export default function FilteredTodosHeader({ title }: { title: string }) {
 	const { isTodoListsSidebarOpen } = useTodoListsSidebarStore();
@@ -13,6 +15,14 @@ export default function FilteredTodosHeader({ title }: { title: string }) {
 			<div className="flex items-center gap-2 px-1">
 				{!isTodoListsSidebarOpen ? <TodoListsSidebarToggle /> : null}
 				<p className="text-xl font-bold">{title}</p>
+				<div className="flex items-center gap-1">
+					<Button ariaLabel="Grid Layout">
+						<Grid2X2 />
+					</Button>
+					<Button ariaLabel="List Layout" isActive={true}>
+						<ListTodo />
+					</Button>
+				</div>
 			</div>
 			<div className="flex items-center gap-4 pt-2">
 				<TodoSort />
