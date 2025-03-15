ec53b8bb070e73c0b5a6fcfa62fb10e1ec47b71b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-15 15:27:59 +0800

    Enhance FilteredTodosHeader to include layout selection logic for Grid and List buttons, improving user experience in layout switching.

--- src/components/ui-shared/FilteredTodosHeader.tsx
+++ src/components/ui-shared/FilteredTodosHeader.tsx
@@ -13,16 +13,30 @@ export default function FilteredTodosHeader({ title }: { title: string }) {
 	const { getQueryParam, updateSearchParams } = useQueryParams();
 	const [smartList] = getQueryParam('smart-list');
 
+	const isLayoutSelected = (layout: string) => {
+		const [view] = getQueryParam('view');
+		if (view === layout) return true;
+		else return false;
+	};
+
 	return (
 		<div className="z-50 px-6 bg-white border-b border-slate-300 flex items-center justify-between py-2">
 			<div className="flex items-center gap-2 px-1">
 				{!isTodoListsSidebarOpen ? <TodoListsSidebarToggle /> : null}
 				<p className="text-xl font-bold">{title}</p>
 				<div className="flex items-center gap-1">
-					<Button ariaLabel="Grid Layout" onClick={() => updateSearchParams('view', 'grid', smartList)}>
+					<Button
+						ariaLabel="Grid Layout"
+						isActive={isLayoutSelected('grid')}
+						onClick={() => updateSearchParams('view', 'grid', smartList)}
+					>
 						<Grid2X2 />
 					</Button>
-					<Button ariaLabel="List Layout" isActive={true} onClick={() => updateSearchParams('view', 'list', smartList)}>
+					<Button
+						ariaLabel="List Layout"
+						isActive={isLayoutSelected('list')}
+						onClick={() => updateSearchParams('view', 'list', smartList)}
+					>
 						<ListTodo />
 					</Button>
 				</div>
