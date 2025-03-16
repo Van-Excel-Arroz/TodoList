65597d4ea86a70fada08ba9f41e8b1daad149a85
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-16 10:19:49 +0800

    Update TodoItem component layout in grid view by adding padding and gap to enhance visual organization and user interaction.

--- src/components/ui-shared/TodoItem.tsx
+++ src/components/ui-shared/TodoItem.tsx
@@ -57,9 +57,9 @@ function TodoItem({ todo }: { todo: Todo }) {
 	return (
 		<>
 			{view === 'grid' ? (
-				<div key={todo.id} className="flex items-center">
+				<div key={todo.id} className="flex items-center gap-2 py-2 px-2">
 					<CheckBox isChecked={todo.is_completed} handleOnClick={handleCheckboxChange} size={20} />
-					<div className="grid grid-cols-12 gap-2 py-2 px-2 w-full">
+					<div className="grid grid-cols-12 gap-2 w-full">
 						<p className="col-span-7 flex justify-start">{todo.task_text}</p>
 						<div className="col-span-1">{todo.due_datetime && <DueDate dueDatetime={todo.due_datetime} />}</div>
 						<div className="col-span-3 flex items-center gap-2">
