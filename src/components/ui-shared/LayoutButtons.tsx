791be4b7f3cf76e7241c89a43e5111bc2ecdc084
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-16 10:49:26 +0800

    Enhance ExpandableSection grid view by adding a footer for improved task detail visibility and layout organization.

---
+++ src/components/ui-shared/LayoutButtons.tsx
@@ -0,0 +1,33 @@
+import useQueryParams from '@/hooks/useQueryParams';
+import Button from './Button';
+import { Grid2X2, ListTodo } from 'lucide-react';
+
+export default function LayoutButtons({ param }: { param: string }) {
+	const { getQueryParam, updateSearchParams } = useQueryParams();
+	const [smartList] = getQueryParam(param);
+
+	const isLayoutSelected = (layout: string) => {
+		const [view] = getQueryParam('view');
+		if (view === layout) return true;
+		else return false;
+	};
+
+	return (
+		<div className="flex items-center gap-1">
+			<Button
+				ariaLabel="Grid Layout"
+				isActive={isLayoutSelected('grid')}
+				onClick={() => updateSearchParams('view', 'grid', smartList)}
+			>
+				<Grid2X2 />
+			</Button>
+			<Button
+				ariaLabel="List Layout"
+				isActive={isLayoutSelected('list')}
+				onClick={() => updateSearchParams('view', 'list', smartList)}
+			>
+				<ListTodo />
+			</Button>
+		</div>
+	);
+}
