019d312214a7a6677d6746d6cc693d184ff33b44
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-16 10:24:09 +0800

    Enhance TodoItem component layout by adding flex-wrap to category tags container and aligning task text vertically, improving visual organization and user interaction.

--- src/components/ui-shared/TodoItem.tsx
+++ src/components/ui-shared/TodoItem.tsx
@@ -60,9 +60,9 @@ function TodoItem({ todo }: { todo: Todo }) {
 				<div key={todo.id} className="flex items-center gap-2 py-2 px-2">
 					<CheckBox isChecked={todo.is_completed} handleOnClick={handleCheckboxChange} size={20} />
 					<div className="grid grid-cols-12 gap-2 w-full">
-						<p className="col-span-7 flex justify-start">{todo.task_text}</p>
+						<p className="col-span-7 flex justify-start items-center">{todo.task_text}</p>
 						<div className="col-span-1">{todo.due_datetime && <DueDate dueDatetime={todo.due_datetime} />}</div>
-						<div className="col-span-3 flex items-center gap-2">
+						<div className="col-span-3 flex items-center gap-2 flex-wrap">
 							{(todo.categories?.length ?? 0) > 0 && (
 								<CategoryTags categories={todo.categories!} handleCategoryClick={handleCategoryClick} />
 							)}
