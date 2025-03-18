76ceac87427cc0e0c682c63bf097f029160b32be
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-18 08:42:53 +0800

    Refactor ExpandableSection grid layout to enhance spacing and adjust column widths for improved readability

--- src/components/ui-shared/ExpandableSection.tsx
+++ src/components/ui-shared/ExpandableSection.tsx
@@ -49,9 +49,9 @@ function ExpandableSection({
 				transition={{ duration: 0.25 }}
 			>
 				{view === 'grid' ? (
-					<div className="grid grid-cols-12 gap-2 w-full text-sm pl-10 pr-3 pb-2 text-slate-600 font-bold">
-						<p className="col-span-6">Task</p>
-						<p className="col-span-2 text-center pl-1">Due Date</p>
+					<div className="grid grid-cols-12 gap-2 w-full text-sm pb-2 pl-40 text-slate-600 font-bold">
+						<p className="col-span-5">Task</p>
+						<p className="col-span-2 text-center">Due Date</p>
 						<p className="col-span-3">Category</p>
 						<p className="col-span-1 text-center">Importance</p>
 					</div>
