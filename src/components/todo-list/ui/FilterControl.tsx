d9bc378ea39d8ac721510bd7fc4897bd41f17799
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-20 04:34:07 +0800

    Refactor FilterControl component to enhance UI by adjusting layout and styling, including changing the border and background for improved visual consistency.

--- src/components/todo-list/ui/FilterControl.tsx
+++ src/components/todo-list/ui/FilterControl.tsx
@@ -32,7 +32,7 @@ export default function FilterControl({ setIsCategoryFilterOpen, setIsDateFilter
 	};
 
 	return (
-		<div className="flex items-center border-b-2 border-slate-300 pb-1  gap-1 hover:border-slate-400">
+		<div className="flex items-center gap-1 border border-slate-200 rounded-md bg-slate-100 px-2 py-1">
 			<Button
 				ariaLabel="Change Filter"
 				onClick={handleMenuToggle}
