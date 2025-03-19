b99635571cfe2e1aa6dc82171ebd43880b66176e
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-19 16:15:46 +0800

    Enhance TodoSearch component by integrating useQueryParams for dynamic search parameter handling

--- src/components/todo-list/ui/TodoSearch.tsx
+++ src/components/todo-list/ui/TodoSearch.tsx
@@ -1,20 +1,30 @@
 import Button from '@/components/ui-shared/Button';
+import useQueryParams from '@/hooks/useQueryParams';
 import { Search } from 'lucide-react';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function TodoSearch() {
-	const { register, handleSubmit, reset } = useForm();
+	const { register, watch } = useForm();
+	const { getQueryParam, updateSearchParams } = useQueryParams();
+
+	const [id] = getQueryParam('id');
+
+	useEffect(() => {
+		// updateSearchParams('search', watch('search'), id);
+	}, [updateSearchParams, watch('search'), id]);
 
 	return (
-		<div className="flex items-center gap-2 w-60 pt-2">
+		<form className="flex items-center gap-2 w-60 pt-2">
 			<Button ariaLabel="Search">
 				<Search size={20} />
 			</Button>
 			<input
+				{...register('search')}
 				type="text"
 				className="px-2 py-1 text-md border-b-2 focus:outline-none focus:border-slate-500 hover:border-slate-400 border-slate-300"
 				placeholder="Search tasks..."
 			/>
-		</div>
+		</form>
 	);
 }
