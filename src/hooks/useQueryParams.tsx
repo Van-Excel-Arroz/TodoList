cbcfc67f56ffcc3586d05bc390d79d76a63e0a87
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-19 16:40:05 +0800

    Refactor useQueryParams hook to utilize useCallback for improved performance and memoization of query parameter handling functions

--- src/hooks/useQueryParams.tsx
+++ src/hooks/useQueryParams.tsx
@@ -1,20 +1,28 @@
 import { useSearchParams, useRouter } from 'next/navigation';
+import { useCallback } from 'react';
 
 export default function useQueryParams() {
 	const searchParams = useSearchParams();
 	const router = useRouter();
 
-	const getQueryParam = (field: string) => {
-		const value = searchParams.get(field);
-		return value?.split(':') || [];
-	};
-	const updateSearchParams = (field: string, value: string | null, key: string) => {
-		const params = new URLSearchParams(searchParams.toString());
-		value ? params.set(`${field}`, value) : params.delete(`${field}`);
-		router.push(`/tasks/?${params.toString()}`);
-		localStorage.setItem(`searchParams-${key}`, `/tasks/?${params.toString()}`);
-		console.log(localStorage);
-	};
+	const getQueryParam = useCallback(
+		(field: string) => {
+			const value = searchParams.get(field);
+			return value?.split(':') || [];
+		},
+		[searchParams]
+	);
+
+	const updateSearchParams = useCallback(
+		(field: string, value: string | null, key: string) => {
+			const params = new URLSearchParams(searchParams.toString());
+			value ? params.set(`${field}`, value) : params.delete(`${field}`);
+			router.push(`/tasks/?${params.toString()}`);
+			localStorage.setItem(`searchParams-${key}`, `/tasks/?${params.toString()}`);
+			console.log(localStorage);
+		},
+		[searchParams, router]
+	);
 
 	return {
 		getQueryParam,
