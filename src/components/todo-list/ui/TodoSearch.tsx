09b380c9190e805f3408c3e2f6acff9641ca852b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-19 16:55:15 +0800

    Update TodoSearch component styles for improved UI, adding background color and adjusting input styling for better user experience.

--- src/components/todo-list/ui/TodoSearch.tsx
+++ src/components/todo-list/ui/TodoSearch.tsx
@@ -13,14 +13,14 @@ export default function TodoSearch() {
 	};
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-2 w-60 pt-2">
+		<form onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-2 w-60 pt-2 bg-slate-200 rounded-md">
 			<Button ariaLabel="Search">
 				<Search size={20} />
 			</Button>
 			<input
 				{...register('search')}
 				type="text"
-				className="px-2 py-1 text-md border-b-2 focus:outline-none focus:border-slate-500 hover:border-slate-400 border-slate-300"
+				className="px-2 py-1 text-md focus:outline-none bg-transparent"
 				placeholder="Search tasks..."
 			/>
 		</form>
