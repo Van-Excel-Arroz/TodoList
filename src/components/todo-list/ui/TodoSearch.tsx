11fe71f84a8f2089a4f281900df16462392ba0a9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-21 09:54:50 +0800

    Add clear search button to TodoSearch component for improved user experience

--- src/components/todo-list/ui/TodoSearch.tsx
+++ src/components/todo-list/ui/TodoSearch.tsx
@@ -1,6 +1,6 @@
 import Button from '@/components/ui-shared/Button';
 import useQueryParams from '@/hooks/useQueryParams';
-import { Search } from 'lucide-react';
+import { Search, X } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 
 export default function TodoSearch() {
@@ -14,21 +14,26 @@ export default function TodoSearch() {
 	};
 
 	return (
-		<form
-			onSubmit={handleSubmit(onSubmit)}
-			className="flex items-center gap-2 px-2 py-1 w-60 bg-slate-100 border border-slate-200 rounded-md"
-		>
-			<Button ariaLabel="Search" type="submit">
-				<Search size={20} />
-			</Button>
-			<input
-				{...register('search')}
-				type="text"
-				autoComplete="off"
-				className="px-2 py-1 text-md focus:outline-none bg-transparent"
-				placeholder={search || 'Search tasks...'}
-				defaultValue={search}
-			/>
-		</form>
+		<>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="flex items-center gap-2 px-2 py-1 bg-slate-100 border border-slate-200 rounded-md"
+			>
+				<Button ariaLabel="Search" type="submit">
+					<Search size={20} />
+				</Button>
+				<input
+					{...register('search')}
+					type="text"
+					autoComplete="off"
+					className="px-2 py-1 text-md focus:outline-none bg-transparent"
+					placeholder={search || 'Search tasks...'}
+					defaultValue={search}
+				/>
+				<Button ariaLabel="Clear Search">
+					<X size={20} />
+				</Button>
+			</form>
+		</>
 	);
 }
