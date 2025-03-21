71ae9d8177cf163494ee5bf40ce087a447b216a1
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-21 10:00:58 +0800

    Enhance Button component by adding disabled state styling for improved visual feedback when the button is inactive.

--- src/components/ui-shared/Button.tsx
+++ src/components/ui-shared/Button.tsx
@@ -25,13 +25,15 @@ function Button({
 		isActive ? 'hover:bg-slate-200 text-slate-800 bg-slate-200' : 'hover:bg-slate-200 active:bg-slate-300'
 	}`;
 
+	const disabledStyle = `${disabled && 'opacity-70'}`;
+
 	const style = `${
 		darkMode ? 'bg-slate-700 text-white hover:bg-slate-500 active:bg-slate-400' : ` text-slate-600 ${bgStyle}`
 	}`;
 
 	return (
 		<button
-			className={`${className} ${style} rounded-md p-1 relative cursor-pointer transition-all duration-400`}
+			className={`${className} ${style} ${disabledStyle} rounded-md p-1 relative cursor-pointer transition-all duration-400`}
 			onClick={() => onClick && onClick()}
 			type={type ? type : 'button'}
 			aria-label={ariaLabel}
