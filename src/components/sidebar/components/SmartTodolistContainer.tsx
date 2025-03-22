454fcb83a5c2a66d532563d308287ac0d828d5c8
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-22 15:56:18 +0800

    Refactor SmartTodolistContainer component to remove framer-motion animations, simplifying the rendering of smart lists.

--- src/components/sidebar/components/SmartTodolistContainer.tsx
+++ src/components/sidebar/components/SmartTodolistContainer.tsx
@@ -2,10 +2,7 @@
 
 import { Clock3, Star, Tags } from 'lucide-react';
 import ExpandableSection from '../../ui-shared/ExpandableSection';
-import { AnimatePresence } from 'framer-motion';
-import { motion } from 'framer-motion';
 import ListLinkItem from '../ui/ListLinkItem';
-import { itemVariants } from '@/utils/framer-motion';
 
 const SmartLists = [
 	{ icon: Star, text: 'Priority' },
@@ -17,15 +14,13 @@ export default function SmartTodolistContainer() {
 	return (
 		<ExpandableSection isEmpty={false} title="Smart Lists" className="mt-2">
 			<ul className="flex flex-col gap-2 w-full">
-				<AnimatePresence>
-					{SmartLists.map((smartList, index) => (
-						<motion.li key={index} layout variants={itemVariants} initial="initial" animate="animate" exit="exit">
-							<ListLinkItem queryParam="smart-list" value={smartList.text.toLowerCase()} Icon={smartList.icon}>
-								{smartList.text}
-							</ListLinkItem>
-						</motion.li>
-					))}
-				</AnimatePresence>
+				{SmartLists.map((smartList, index) => (
+					<li key={index}>
+						<ListLinkItem queryParam="smart-list" value={smartList.text.toLowerCase()} Icon={smartList.icon}>
+							{smartList.text}
+						</ListLinkItem>
+					</li>
+				))}
 			</ul>
 		</ExpandableSection>
 	);
