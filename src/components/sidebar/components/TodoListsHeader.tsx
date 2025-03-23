4dd5cf2b869916c1c28e4364781be6ee0bf1096f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-23 18:22:14 +0800

    Remove unused pathname logic and TodoListsSidebarToggle from TodoListsHeader component, simplifying the code and enhancing maintainability.

--- src/components/sidebar/components/TodoListsHeader.tsx
+++ src/components/sidebar/components/TodoListsHeader.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-import TodoListsSidebarToggle from '../ui/TodoListsSidebarToggle';
 import { Home } from 'lucide-react';
 import Button from '@/components/ui-shared/Button';
 import Link from 'next/link';
 import useSelectedTodoIdStore from '@/context/SelectedTodoIdContext';
 
 export default function TodoListsHeader() {
-	const pathname = usePathname();
-	const isHome = pathname === '/tasks/home';
 	const { setSelectedTodoId } = useSelectedTodoIdStore();
 
 	return (
@@ -20,7 +16,6 @@ export default function TodoListsHeader() {
 				</Button>
 				<p className="text-lg">TodoApp</p>
 			</Link>
-			{!isHome && <TodoListsSidebarToggle />}
 		</div>
 	);
 }
