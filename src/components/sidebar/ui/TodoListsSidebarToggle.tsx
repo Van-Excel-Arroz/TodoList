faa6ed71c1ba21a1e255242c2fda516a6a2182a0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-23 18:21:31 +0800

    Refactor TodoListsSidebarToggle component to remove unused state and update icon, simplifying the code and enhancing visual consistency in the sidebar.

--- src/components/sidebar/ui/TodoListsSidebarToggle.tsx
+++ src/components/sidebar/ui/TodoListsSidebarToggle.tsx
@@ -2,13 +2,13 @@
 
 import Button from '@/components/ui-shared/Button';
 import useTodoListsSidebarStore from '@/context/TodoListsSidebarContext';
-import { Menu, PanelLeftClose } from 'lucide-react';
+import { PanelLeft } from 'lucide-react';
 
 export default function TodoListsSidebarToggle() {
-	const { toggleTodoListsSidebar, isTodoListsSidebarOpen } = useTodoListsSidebarStore();
+	const { toggleTodoListsSidebar } = useTodoListsSidebarStore();
 	return (
 		<Button onClick={toggleTodoListsSidebar} ariaLabel="Todolist Menu">
-			{isTodoListsSidebarOpen ? <PanelLeftClose size={25} /> : <Menu className="text-slate-800" size={20} />}
+			<PanelLeft className="text-slate-800" size={20} />
 		</Button>
 	);
 }
