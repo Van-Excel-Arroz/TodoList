e9c4f99bb197efd7c80498bfdc0a192f415bdea1
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-24 16:51:01 +0800

    Update ExpandableSection button rotation logic for improved visual feedback when toggling sections

--- src/components/ui-shared/ExpandableSection.tsx
+++ src/components/ui-shared/ExpandableSection.tsx
@@ -34,7 +34,7 @@ function ExpandableSection({
 		>
 			<div className="flex items-center w-full mx-1 gap-1">
 				<Button ariaLabel="Toggle Expand Section" onClick={() => setIsOpen(prev => !prev)} disabled={isEmpty}>
-					<div className={`transition-transform duration-200 ease-in-out ${isOpen && !isEmpty ? 'rotate-180' : ''}`}>
+					<div className={`transition-transform duration-200 ease-in-out ${isOpen && !isEmpty ? '' : '-rotate-90'}`}>
 						<ChevronDown size={20} />
 					</div>
 				</Button>
