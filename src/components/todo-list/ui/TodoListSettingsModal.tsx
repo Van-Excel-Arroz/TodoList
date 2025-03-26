b5ca56ff29b94eba9fc1bd5c4bcd70890bb4d9f7
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-26 16:21:40 +0800

    Refactor TodoListSettingsModal to use Selection component for setting navigation, improving code clarity and maintainability.

--- src/components/todo-list/ui/TodoListSettingsModal.tsx
+++ src/components/todo-list/ui/TodoListSettingsModal.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import Button from '@/components/ui-shared/Button';
+import Selection from '@/components/ui-shared/Selection';
 import { X } from 'lucide-react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 interface TodoListSettingsModalProps {
@@ -11,11 +12,10 @@ interface TodoListSettingsModalProps {
 	todolistTitle: string;
 }
 
-type SettingSectionType = 'behavior' | 'appearance' | 'categories';
+const settings = ['Behavior', 'Appearance', 'Categories'];
 
 export default function TodoListSettingsModal({ isOpen, onClose, todolistTitle }: TodoListSettingsModalProps) {
 	const portalRootRef = useRef<HTMLElement | null>(null);
-	const [settingSection, setSettingSection] = useState<SettingSectionType>('behavior');
 
 	useEffect(() => {
 		portalRootRef.current = document.getElementById('modal-root');
@@ -33,32 +33,7 @@ export default function TodoListSettingsModal({ isOpen, onClose, todolistTitle }
 			<div className="relative w-full max-w-2xl bg-white rounded-lg shadow-xl p-6" onClick={e => e.stopPropagation()}>
 				<h2 className="text-xl font-semibold mb-4">TodoList Settings</h2>
 				<p className="text-sm text-slate-600">Customize the appearance and behavior of "{todolistTitle}" list.</p>
-				<div className="w-full grid grid-cols-3 text-center bg-slate-100 rounded-md p-2">
-					<div
-						className={`${
-							settingSection === 'behavior' ? 'text-black bg-white shadow-md rounded-lg' : 'text-slate-600'
-						} col-span-1 py-1 cursor-pointer`}
-						onClick={() => setSettingSection('behavior')}
-					>
-						<p className="text-sm">Behavior</p>
-					</div>
-					<div
-						className={`${
-							settingSection === 'appearance' ? 'text-black bg-white shadow-md rounded-lg' : 'text-slate-600'
-						} col-span-1 py-1 cursor-pointer`}
-						onClick={() => setSettingSection('appearance')}
-					>
-						<p className="text-sm">Appearance</p>
-					</div>
-					<div
-						className={`${
-							settingSection === 'categories' ? 'text-black bg-white shadow-md rounded-lg' : 'text-slate-600'
-						} col-span-1 py-1 cursor-pointer`}
-						onClick={() => setSettingSection('categories')}
-					>
-						<p className="text-sm">Categories</p>
-					</div>
-				</div>
+				<Selection options={settings} />
 				<Button className="absolute top-5 right-4" onClick={onClose} ariaLabel="Close Modal">
 					<X />
 				</Button>
--- src/components/ui-shared/Selection.tsx
+++ src/components/ui-shared/Selection.tsx
@@ -11,8 +11,9 @@ export default function Selection({ options }: SelectionProps) {
 
 	return (
 		<div className={`w-full grid grid-cols-${options.length}  text-center bg-slate-100 rounded-md p-2`}>
-			{options.map(option => (
+			{options.map((option, index) => (
 				<div
+					key={index}
 					className={`${
 						settingSection === option.toLowerCase() ? 'text-black bg-white shadow-md rounded-lg' : 'text-slate-600'
 					} col-span-1 py-1 cursor-pointer`}
