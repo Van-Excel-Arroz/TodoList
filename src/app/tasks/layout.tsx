295619549dd5ef457de3dd75de56f8faa9497e7e
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-26 09:50:40 +0800

    Refactor layout structure in Layout component for improved visual consistency by removing unnecessary wrapper div.

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -7,16 +7,14 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps): JSX.Element {
 	return (
-		<div className="flex flex-col h-screen overflow-hidden">
-			<div className="flex flex-1 overflow-hidden">
-				<aside className="absolute lg:sticky top-0 left-0 z-30">
-					<TodoListsNav />
-				</aside>
-				<main className="w-full z-10">{children}</main>
-				<aside className="absolute lg:sticky top-0 right-0 z-40">
-					<TodoDetailsPanel />
-				</aside>
-			</div>
+		<div className="flex flex-1 overflow-hidden">
+			<aside className="absolute lg:sticky top-0 left-0 z-30">
+				<TodoListsNav />
+			</aside>
+			<main className="w-full z-10">{children}</main>
+			<aside className="absolute lg:sticky top-0 right-0 z-40">
+				<TodoDetailsPanel />
+			</aside>
 		</div>
 	);
 }
