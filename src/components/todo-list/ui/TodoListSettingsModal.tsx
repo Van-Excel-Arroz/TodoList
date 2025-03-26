07a1f405d5232d7a83d1d51826e043eec4e860b9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-26 16:11:56 +0800

    Add section navigation to TodoListSettingsModal for improved user experience by allowing users to switch between behavior, appearance, and categories settings.

--- src/components/todo-list/ui/TodoListSettingsModal.tsx
+++ src/components/todo-list/ui/TodoListSettingsModal.tsx
@@ -2,7 +2,7 @@
 
 import Button from '@/components/ui-shared/Button';
 import { X } from 'lucide-react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 interface TodoListSettingsModalProps {
@@ -11,8 +11,11 @@ interface TodoListSettingsModalProps {
 	todolistTitle: string;
 }
 
+type SettingSectionType = 'behavior' | 'appearance' | 'categories';
+
 export default function TodoListSettingsModal({ isOpen, onClose, todolistTitle }: TodoListSettingsModalProps) {
 	const portalRootRef = useRef<HTMLElement | null>(null);
+	const [settingSection, setSettingSection] = useState<SettingSectionType>('behavior');
 
 	useEffect(() => {
 		portalRootRef.current = document.getElementById('modal-root');
@@ -30,6 +33,32 @@ export default function TodoListSettingsModal({ isOpen, onClose, todolistTitle }
 			<div className="relative w-full max-w-2xl bg-white rounded-lg shadow-xl p-6" onClick={e => e.stopPropagation()}>
 				<h2 className="text-xl font-semibold mb-4">TodoList Settings</h2>
 				<p className="text-sm text-slate-600">Customize the appearance and behavior of "{todolistTitle}" list.</p>
+				<div className="w-full grid grid-cols-3 text-center bg-slate-100 rounded-md p-2">
+					<div
+						className={`${
+							settingSection === 'behavior' ? 'text-black bg-white shadow-md rounded-lg' : 'text-slate-600'
+						} col-span-1 py-1 cursor-pointer`}
+						onClick={() => setSettingSection('behavior')}
+					>
+						<p className="text-sm">Behavior</p>
+					</div>
+					<div
+						className={`${
+							settingSection === 'appearance' ? 'text-black bg-white shadow-md rounded-lg' : 'text-slate-600'
+						} col-span-1 py-1 cursor-pointer`}
+						onClick={() => setSettingSection('appearance')}
+					>
+						<p className="text-sm">Appearance</p>
+					</div>
+					<div
+						className={`${
+							settingSection === 'categories' ? 'text-black bg-white shadow-md rounded-lg' : 'text-slate-600'
+						} col-span-1 py-1 cursor-pointer`}
+						onClick={() => setSettingSection('categories')}
+					>
+						<p className="text-sm">Categories</p>
+					</div>
+				</div>
 				<Button className="absolute top-5 right-4" onClick={onClose} ariaLabel="Close Modal">
 					<X />
 				</Button>
