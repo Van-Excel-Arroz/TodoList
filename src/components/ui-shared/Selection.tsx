93d7d08ece2c6889bb468f78a201404f981ee9f9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-26 16:32:22 +0800

    Refactor Selection component to use flex layout instead of grid, improving responsiveness and visual alignment of options.

--- src/components/ui-shared/Selection.tsx
+++ src/components/ui-shared/Selection.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useState } from 'react';
-
 interface SelectionProps {
 	options: string[];
 	selectedOption: string;
@@ -10,13 +8,13 @@ interface SelectionProps {
 
 export default function Selection({ options, selectedOption, setSelectedOption }: SelectionProps) {
 	return (
-		<div className={`w-full grid grid-cols-${options.length}  text-center bg-slate-100 rounded-md p-2`}>
+		<div className={`w-full flex text-center bg-slate-100 rounded-md p-2`}>
 			{options.map((option, index) => (
 				<div
 					key={index}
 					className={`${
 						selectedOption === option.toLowerCase() ? 'text-black bg-white shadow-md rounded-lg' : 'text-slate-600'
-					} col-span-1 py-1 cursor-pointer`}
+					} flex-1 py-1 cursor-pointer`}
 					onClick={() => setSelectedOption(option.toLowerCase())}
 				>
 					<p className="text-sm">{option}</p>
