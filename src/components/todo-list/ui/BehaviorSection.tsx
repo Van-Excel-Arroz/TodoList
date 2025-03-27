e5eecd7043fa3ef0dfee51278deadfd04e9c5816
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-03-27 17:26:47 +0800

    Update BehaviorSection to improve sort menu usability by adjusting text size for better readability and user interaction.

--- src/components/todo-list/ui/BehaviorSection.tsx
+++ src/components/todo-list/ui/BehaviorSection.tsx
@@ -19,7 +19,7 @@ export default function BehaviorSection() {
 		<div className="space-y-1">
 			<p className="pl-2">Sort tasks by:</p>
 			<div className="border rounded-md px-4 py-2 relative" onClick={() => setIsSortMenuOpen(prev => !prev)}>
-				<div className="flex items-center justify-between cursor-pointer">
+				<div className="flex items-center justify-between cursor-pointer text-sm">
 					{sortField ? <p>{sortLabels[sortField]}</p> : <p className="text-slate-600">Select to sort</p>}
 					<ChevronDown size={20} className="text-slate-600" />
 				</div>
