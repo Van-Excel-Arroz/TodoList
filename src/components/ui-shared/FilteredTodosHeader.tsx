d207b80c90c5d3ba83b6c90e6f560388f2997e37
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-01 10:41:45 +0800

    Update FilteredTodosHeader component to adjust padding for improved layout consistency.

--- src/components/ui-shared/FilteredTodosHeader.tsx
+++ src/components/ui-shared/FilteredTodosHeader.tsx
@@ -10,13 +10,13 @@ export default function FilteredTodosHeader({ title }: { title: string }) {
 	const { isTodoListsSidebarOpen } = useTodoListsSidebarStore();
 
 	return (
-		<div className="z-50 px-6 bg-white border-b border-slate-300 flex items-center justify-between py-2">
+		<div className="z-50 px-6 bg-white border-b border-slate-300 flex items-center justify-between py-[17px]">
 			<div className="flex items-center gap-2 px-1">
 				{!isTodoListsSidebarOpen ? <TodoListsSidebarToggle /> : null}
 				<p className="text-xl font-bold">{title}</p>
 				<LayoutButtons param="smart-list" />
 			</div>
-			<div className="flex items-center gap-4 pt-2">
+			<div className="flex items-center gap-4">
 				<TodoSort />
 				<TodoFilter />
 			</div>
