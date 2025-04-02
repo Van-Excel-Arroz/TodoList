da18690b06a97ad00d1a7f8f5ba4a267b5549ee7
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-02 16:09:51 +0800

    Refactor TodoCategories component by removing category addition functionality and simplifying the UI. The 'Add Category' button is now always visible, enhancing user experience.

--- src/components/details-panel/components/TodoCategories.tsx
+++ src/components/details-panel/components/TodoCategories.tsx
@@ -1,48 +1,20 @@
 'use client';
 
-import { Plus, Tag, Tags } from 'lucide-react';
+import { Plus, Tags } from 'lucide-react';
 import { Category } from '@/utils/types';
-import { addTodoCategoryAction, deleteTodoCategoryAction } from '@/actions/category-action';
+import { deleteTodoCategoryAction } from '@/actions/category-action';
 import useTodosStore from '@/context/TodosContext';
-import CategoryForm from '../ui/CategoryForm';
 import CategoryTags from '../ui/CategoryTags';
 import useSelectedTodoIdStore from '@/context/SelectedTodoIdContext';
-import useCategoriesStore from '@/context/CategoriesContext';
 import Button from '@/components/ui-shared/Button';
-import { useState } from 'react';
-
-interface CategoryFormInputs {
-	category_title: string;
-	hex_color: string;
-}
 
 export default function TodoCategories({ categories, todolistId }: { categories: Category[]; todolistId: number }) {
 	const { selectedTodoId } = useSelectedTodoIdStore();
-	const { addCategory, deleteCategory, updateCategoriesColor } = useTodosStore();
-	const { addCategory: addCategoryFilter } = useCategoriesStore();
-	const [isAddingCategory, setIsAddingCategory] = useState(false);
+	const { deleteCategory } = useTodosStore();
 
 	const handleRemoveCategory = async (categoryId: number) => {
 		await deleteTodoCategoryAction(categoryId);
 		deleteCategory(selectedTodoId, categoryId);
-	};
-
-	const handleOnSubmit = async (data: CategoryFormInputs) => {
-		const categoryId = await addTodoCategoryAction(data.category_title, data.hex_color, todolistId, selectedTodoId);
-
-		if (!categoryId) return;
-
-		const newCategory: Category = {
-			id: categoryId,
-			category_title: data.category_title,
-			hex_color: data.hex_color,
-			is_selected: false,
-			todo_list_id: todolistId,
-		};
-
-		updateCategoriesColor(data.category_title, data.hex_color);
-		addCategoryFilter(newCategory);
-		addCategory(selectedTodoId, newCategory);
 	};
 
 	return (
@@ -55,20 +27,10 @@ export default function TodoCategories({ categories, todolistId }: { categories:
 				{categories.map(category => (
 					<CategoryTags key={category.id} category={category} onRemove={handleRemoveCategory} />
 				))}
-				{!isAddingCategory && (
-					<Button
-						darkMode={true}
-						ariaLabel="Add Category"
-						onClick={() => setIsAddingCategory(true)}
-						className="flex items-center gap-1"
-					>
-						<Plus size={13} />
-						<p className="text-sm">Add Category</p>
-					</Button>
-				)}
-			</div>
-			<div className="flex justify-between items-center w-full text-slate-600">
-				{isAddingCategory && <CategoryForm onSubmit={handleOnSubmit} handleIsAddingCategory={setIsAddingCategory} />}
+				<Button darkMode={true} ariaLabel="Add Category" className="flex items-center gap-1">
+					<Plus size={13} />
+					<p className="text-sm">Add Category</p>
+				</Button>
 			</div>
 		</div>
 	);
