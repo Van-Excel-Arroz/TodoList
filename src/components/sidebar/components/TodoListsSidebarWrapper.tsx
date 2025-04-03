cdd31595419e57f74dfd12dfb671a44ca74adde5
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-03 14:58:55 +0800

    Update TodoListsSidebarWrapper to change sidebar width handling from 'w-0' to 'hidden' for improved visibility control when the sidebar is closed.

--- src/components/sidebar/components/TodoListsSidebarWrapper.tsx
+++ src/components/sidebar/components/TodoListsSidebarWrapper.tsx
@@ -15,7 +15,7 @@ export default function TodoListsSidebarWrapper({ children }: { children: React.
 			/>
 			<div
 				className={`flex flex-col fixed lg:relative left-0 border-r border-slate-300 top-0 bg-white h-screen transition-all duration-200 ease-in-out text-nowrap overflow-hidden ${
-					isTodoListsSidebarOpen ? 'w-72' : 'w-0'
+					isTodoListsSidebarOpen ? 'w-72' : 'hidden'
 				}`}
 			>
 				{children}
