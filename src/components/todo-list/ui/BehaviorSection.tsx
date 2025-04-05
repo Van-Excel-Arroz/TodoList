65481c831555ce3f3283577e7f2d6d7ec3fb68c9
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-05 19:27:42 +0800

    Add FilterMenu to BehaviorSection for task filtering functionality. Updated sort label text for clarity and improved layout with new filter options.

--- src/components/todo-list/ui/BehaviorSection.tsx
+++ src/components/todo-list/ui/BehaviorSection.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import SortMenu from './SortMenu';
 import useQueryParams from '@/hooks/useQueryParams';
 import { ChevronDown } from 'lucide-react';
+import FilterMenu from './FilterMenu';
 
 const sortLabels: any = {
 	dueDate: 'Due Date',
@@ -20,7 +21,7 @@ export default function BehaviorSection() {
 			<p className="pl-2">Sort tasks by:</p>
 			<div className="border rounded-md px-4 py-2 relative" onClick={() => setIsSortMenuOpen(prev => !prev)}>
 				<div className="flex items-center justify-between cursor-pointer text-sm">
-					{sortField ? <p>{sortLabels[sortField]}</p> : <p className="text-slate-600">Select to sort</p>}
+					{sortField ? <p>{sortLabels[sortField]}</p> : <p className="text-slate-600">Select Sort</p>}
 					<ChevronDown size={20} className="text-slate-600" />
 				</div>
 				<SortMenu
@@ -30,6 +31,13 @@ export default function BehaviorSection() {
 					top="top-12"
 					header={false}
 				/>
+			</div>
+			<p className="pl-2">Filter tasks by:</p>
+			<div className="border rounded-md px-4 py-2 relative" onClick={() => setIsSortMenuOpen(prev => !prev)}>
+				<div className="flex items-center justify-between cursor-pointer text-sm">
+					{sortField ? <p>{sortLabels[sortField]}</p> : <p className="text-slate-600">Select Filter</p>}
+					<ChevronDown size={20} className="text-slate-600" />
+				</div>
 			</div>
 		</div>
 	);
