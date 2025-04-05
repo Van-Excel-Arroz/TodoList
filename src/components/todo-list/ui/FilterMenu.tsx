c44373a9e1f81323259bdca597bf8b492e90e657
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-05 19:29:34 +0800

    Refactor FilterMenu component to simplify props and improve layout. Removed unnecessary state handlers and adjusted menu items for better organization and clarity.

--- src/components/todo-list/ui/FilterMenu.tsx
+++ src/components/todo-list/ui/FilterMenu.tsx
@@ -3,39 +3,19 @@ import MenuItem from '@/components/ui-shared/MenuItem';
 import { MenuOpenProps } from '@/utils/types';
 import { CalendarDays, Tag } from 'lucide-react';
 
-interface FilterMenuProps extends MenuOpenProps {
-	setIsCategoryFilterOpen: (val: boolean) => void;
-	setIsDateFilterOpen: (val: boolean) => void;
-}
-
-export default function FilterMenu({
-	isOpen,
-	setIsOpen,
-	setIsCategoryFilterOpen,
-	setIsDateFilterOpen,
-}: FilterMenuProps) {
+export default function FilterMenu({ isOpen, setIsOpen }: MenuOpenProps) {
 	return (
 		<Menu open={isOpen} onClose={() => setIsOpen(false)} width="w-44">
 			<MenuItem className="border-b font-bold justify-center" clickable={false}>
 				<p>Filter by</p>
 			</MenuItem>
-			<MenuItem
-				onClick={() => {
-					setIsCategoryFilterOpen(true);
-					setIsOpen(false);
-				}}
-			>
-				<Tag className="text-slate-600" size={18} />
-				<p>Categories</p>
-			</MenuItem>
-			<MenuItem
-				onClick={() => {
-					setIsDateFilterOpen(true);
-					setIsOpen(false);
-				}}
-			>
+			<MenuItem clickable={false}>
 				<CalendarDays className="text-slate-600" size={18} />
 				<p>Due Date</p>
+			</MenuItem>
+			<MenuItem clickable={false}>
+				<Tag className="text-slate-600" size={18} />
+				<p>Categories</p>
 			</MenuItem>
 		</Menu>
 	);
