9f168b576a1afc2b527cf0a45a07900cd4daf277
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-07 08:48:56 +0800

    Update BehaviorSection to toggle filter menu instead of sort menu, enhancing user interaction for task filtering.

--- src/components/todo-list/ui/BehaviorSection.tsx
+++ src/components/todo-list/ui/BehaviorSection.tsx
@@ -28,7 +28,7 @@ export default function BehaviorSection() {
 				<SortMenu isOpen={isSortMenuOpen} setIsOpen={setIsSortMenuOpen} width="w-full" top="top-12" header={false} />
 			</div>
 			<p className="pl-2">Filter tasks by:</p>
-			<div className="border rounded-md px-4 py-2 relative" onClick={() => setIsSortMenuOpen(prev => !prev)}>
+			<div className="border rounded-md px-4 py-2 relative" onClick={() => setIsFilterMenuOpen(prev => !prev)}>
 				<div className="flex items-center justify-between cursor-pointer text-sm">
 					{sortField ? <p>{sortLabels[sortField]}</p> : <p className="text-slate-600">Select Filter</p>}
 					<ChevronDown size={20} className="text-slate-600" />
