236115bd8f25647058b866aaaf64020344172879
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-07 18:39:15 +0800

    Update TodoDetailsPanel transition effects for improved animation and layout responsiveness. Adjusted width and translation properties for better user experience when toggling the panel.

--- src/components/details-panel/TodoDetailsPanel.tsx
+++ src/components/details-panel/TodoDetailsPanel.tsx
@@ -34,7 +34,8 @@ export default function TodoDetailsPanel() {
 			/>
 			<div
 				className={`flex flex-col fixed lg:relative right-0 top-0 bg-white border-l border-slate-300  h-screen transition-[width] duration-200 ease-in-out z-50 ${
-					isOpen ? 'w-[23rem]' : 'hidden'
+					isOpen ? 'w-96 translate-x-0' : 'w-0 translate-x-10 '
+				}
 				}`}
 			>
 				<TodoDetailsHeader />
