23227aeeba1c5744d7800f27ff192b81fde08724
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-10 10:00:05 +0800

    Add FilterDropDown component to BehaviorSection for enhanced task filtering options

--- src/components/todo-list/ui/BehaviorSection.tsx
+++ src/components/todo-list/ui/BehaviorSection.tsx
@@ -1,3 +1,4 @@
+import FilterDropDown from '@/components/ui-shared/FilterDropDown';
 import SortDropDown from '@/components/ui-shared/SortDropDown';
 
 export default function BehaviorSection() {
@@ -5,8 +6,8 @@ export default function BehaviorSection() {
 		<div className="flex flex-col gap-2">
 			<p className="pl-2">Sort tasks by:</p>
 			<SortDropDown />
-
 			<p className="pl-2">Filter tasks by:</p>
+			<FilterDropDown />
 		</div>
 	);
 }
---
+++ src/components/ui-shared/FilterDropDown.tsx
@@ -0,0 +1,46 @@
+import Button from '@/components/ui-shared/Button';
+import DropDown from '@/components/ui-shared/DropDown';
+import MenuItem from '@/components/ui-shared/MenuItem';
+import { ArrowBigDown, ArrowBigUp, CalendarDays, CalendarX2, Check, X } from 'lucide-react';
+import { useState } from 'react';
+
+export default function FilterDropDown() {
+	const [selectedFilter, setSelectedFilter] = useState<string[]>([]);
+	const [sortOrder, setSortOrder] = useState<'Asc' | 'Desc'>('Asc');
+
+	const handleSortOrder = () => {
+		const newOrder = sortOrder === 'Asc' ? 'Desc' : 'Asc';
+		setSortOrder(newOrder);
+	};
+
+	const DateFilterItems = [
+		{ icon: CalendarDays, label: 'Today' },
+		{ icon: CalendarDays, label: 'Tomorrow' },
+		{ icon: CalendarDays, label: 'This Week' },
+		{ icon: CalendarDays, label: 'This Month' },
+		{ icon: CalendarX2, label: 'No Due Date' },
+	];
+
+	return (
+		<div className="flex flex-row items-center gap-2 mb-4">
+			<DropDown selectedItem={selectedFilter}>
+				{DateFilterItems.map(item => (
+					<MenuItem className="justify-between" onClick={() => setSelectedFilter([item.label])}>
+						<div className="flex gap-2">
+							<item.icon className="text-slate-600" size={18} />
+							<p>{item.label}</p>
+						</div>
+						{<Check size={18} className={`${item.label == selectedFilter[0] ? 'block' : 'hidden'} text-slate-600`} />}
+					</MenuItem>
+				))}
+			</DropDown>
+			<div className="flex items-center gap-2 border px-3 py-2 rounded-md cursor-pointer" onClick={handleSortOrder}>
+				{sortOrder === 'Asc' ? <ArrowBigUp size={20} /> : <ArrowBigDown size={20} />}
+				<p className="select-none text-sm">{sortOrder}</p>
+			</div>
+			<Button ariaLabel="Clear Sorting" onClick={() => setSelectedFilter([])}>
+				<X />
+			</Button>
+		</div>
+	);
+}
