30fe8bfdf5ebe63e4078d9d9b2dff407dc166d52
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-11 15:40:37 +0800

    Add new options for task management in BehaviorSection, including New Tasks Position and Due Date Format, enhancing user customization and functionality.

--- src/components/todo-list/ui/BehaviorSection.tsx
+++ src/components/todo-list/ui/BehaviorSection.tsx
@@ -4,10 +4,16 @@ import SortDropDown from '@/components/ui-shared/SortDropDown';
 
 const headerTextStyle = 'text-lg font-semibold text-slate-700';
 const completedTasksOptions = ['Move to "Completed" Section', 'Hide After', 'Hide immediately'];
+const newTasksPositionOptions = ['Add to Top', 'Add to Bottom'];
+const dueDateFormatOptions = [
+	'Relative (2 days left,  yesterday)',
+	'Short Date (mm/dd/yyyy)',
+	'Long Date (Jan 1, 2025)',
+];
 
 export default function BehaviorSection() {
 	return (
-		<div className="flex flex-col gap-2 px-2 overflow-y-auto">
+		<div className="flex flex-col gap-2 px-2 overflow-y-auto h-[55vh]">
 			<p className={headerTextStyle}>Sort</p>
 			<SortDropDown />
 			<p className={headerTextStyle}>Filter</p>
@@ -15,7 +21,9 @@ export default function BehaviorSection() {
 			<p className={headerTextStyle}>Completed Tasks</p>
 			<RadioButtonGroup options={completedTasksOptions} />
 			<p className={headerTextStyle}>New Tasks Position</p>
+			<RadioButtonGroup options={newTasksPositionOptions} />
 			<p className={headerTextStyle}>Due Date Format</p>
+			<RadioButtonGroup options={dueDateFormatOptions} />
 		</div>
 	);
 }
