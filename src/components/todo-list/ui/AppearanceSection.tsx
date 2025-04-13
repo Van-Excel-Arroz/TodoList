52a002e93e64a8c21665f9b9f3cf73d858c4f162
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-13 10:03:56 +0800

    Enhance AppearanceSection by adding List Icon header and adjusting layout for improved user experience

--- src/components/todo-list/ui/AppearanceSection.tsx
+++ src/components/todo-list/ui/AppearanceSection.tsx
@@ -4,15 +4,17 @@ export default function AppearanceSection({ headerTextStyle }: { headerTextStyle
 	return (
 		<>
 			<p className={headerTextStyle}>List Theme / Accent Color</p>
-			<div className="flex items-center gap-2">
+			<div className="flex items-center gap-2 py-2">
 				{PREDEFINED_THEME_COLORS.map(color => (
 					<div
 						key={color}
-						className="h-10 w-10 rounded-full cursor-pointer hover:scale-110 transition-scale duration-150 ease-out"
+						className="h-10 w-10 rounded-full cursor-pointer hover:scale-110 active:scale-100 transition-scale duration-150 ease-out"
 						style={{ backgroundColor: color }}
 					/>
 				))}
 			</div>
+			<p className={headerTextStyle}>List Icon</p>
+			<div className=""></div>
 		</>
 	);
 }
