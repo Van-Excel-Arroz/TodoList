2851a62d91a797dfb300ea49fa329f02d26d71f3
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-13 16:47:57 +0800

    Add category management features to CategoriesSection with color change and delete options

--- src/components/todo-list/ui/CategoriesSection.tsx
+++ src/components/todo-list/ui/CategoriesSection.tsx
@@ -1,4 +1,6 @@
+import Button from '@/components/ui-shared/Button';
 import useCategoriesStore from '@/context/CategoriesContext';
+import { Palette, Trash2 } from 'lucide-react';
 
 export default function CategoriesSection({ headerTextStyle }: { headerTextStyle: string }) {
 	const { categories } = useCategoriesStore();
@@ -9,17 +11,28 @@ export default function CategoriesSection({ headerTextStyle }: { headerTextStyle
 			<div className="h-[40vh] overflow-hidden overflow-y-auto">
 				{categories.length > 0 ? (
 					categories.map(category => (
-						<div key={category.id} className="flex items-center gap-2 py-1">
-							<p className="text-2xl" style={{ color: category.hex_color }}>
-								●
-							</p>
-							<p>{category.category_title}</p>
+						<div key={category.id} className="flex items-center justify-between px-2 py-1">
+							<div className="flex items-center gap-2">
+								<p className="text-2xl" style={{ color: category.hex_color }}>
+									●
+								</p>
+								<p>{category.category_title}</p>
+							</div>
+							<div className="flex items-center gap-2">
+								<Button ariaLabel="Change Category Color">
+									<Palette size={20} />
+								</Button>
+								<Button ariaLabel="Delete Category">
+									<Trash2 size={20} className="text-red-600" />
+								</Button>
+							</div>
 						</div>
 					))
 				) : (
 					<p className="py-6 px-3 text-slate-600">No available categories.</p>
 				)}
 			</div>
+			<p className={headerTextStyle}>Add New Category</p>
 		</>
 	);
 }
