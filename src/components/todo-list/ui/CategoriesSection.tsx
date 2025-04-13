6858a428729af3c6c3d5e301c4ca5434167cfb7f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-13 16:28:32 +0800

    Implement CategoriesSection component to display category management with dynamic category rendering and improved styling options.

--- src/components/todo-list/ui/CategoriesSection.tsx
+++ src/components/todo-list/ui/CategoriesSection.tsx
@@ -1,3 +1,25 @@
-export default function CategoriesSection() {
-	return <p>Categories Section</p>;
+import useCategoriesStore from '@/context/CategoriesContext';
+
+export default function CategoriesSection({ headerTextStyle }: { headerTextStyle: string }) {
+	const { categories } = useCategoriesStore();
+
+	return (
+		<>
+			<p className={headerTextStyle}>Category Management</p>
+			<div className="h-[40vh] overflow-hidden overflow-y-auto">
+				{categories.length > 0 ? (
+					categories.map(category => (
+						<div key={category.id} className="flex items-center gap-2 py-1">
+							<p className="text-2xl" style={{ color: category.hex_color }}>
+								●
+							</p>
+							<p>{category.category_title}</p>
+						</div>
+					))
+				) : (
+					<p className="py-6 px-3 text-slate-600">No available categories.</p>
+				)}
+			</div>
+		</>
+	);
 }
