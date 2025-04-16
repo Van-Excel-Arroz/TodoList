5ab661c0ed402347d450a878139db3c98048b106
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-16 17:44:00 +0800

    Refactor ColorSelectionMenu to integrate MenuItem component, enhancing layout and styling. Adjusted color selection size and added vertical positioning for improved user experience.

--- src/components/ui-shared/ColorSelectionMenu.tsx
+++ src/components/ui-shared/ColorSelectionMenu.tsx
@@ -3,6 +3,7 @@ import Menu from './Menu';
 import { PREDEFINED_CATEGORY_COLORS } from '@/utils/constants';
 import { useState } from 'react';
 import { Check } from 'lucide-react';
+import MenuItem from './MenuItem';
 
 interface ColorSelectionMenu extends MenuOpenProps {
 	initialColor: string;
@@ -12,22 +13,24 @@ export default function ColorSelectionMenu({ isOpen, setIsOpen, initialColor }: 
 	const [selectedColor, setSelectedColor] = useState(initialColor);
 
 	return (
-		<Menu width="w-fit" open={isOpen} onClose={() => setIsOpen(false)}>
-			{PREDEFINED_CATEGORY_COLORS.map(color => {
-				const isSelected = selectedColor === color;
-				return (
-					<div
-						key={color}
-						className={`${
-							isSelected && 'ring-2 ring-offset-2'
-						} h-10 w-10 flex items-center justify-center rounded-full cursor-pointer hover:scale-110 transition-scale duration-75 ease-out`}
-						style={{ backgroundColor: color }}
-						onClick={() => setSelectedColor(color)}
-					>
-						{isSelected && <Check color="white" strokeWidth={4} />}
-					</div>
-				);
-			})}
+		<Menu width="w-60" verticalPosition="top-12" open={isOpen} onClose={() => setIsOpen(false)}>
+			<MenuItem clickable={false} className="gap-4 grid grid-cols-5">
+				{PREDEFINED_CATEGORY_COLORS.map(color => {
+					const isSelected = selectedColor === color;
+					return (
+						<div
+							key={color}
+							className={`${
+								isSelected && 'ring-2 ring-offset-2'
+							} col-span-1 h-7 w-7 flex items-center justify-center rounded-full cursor-pointer hover:scale-110 transition-scale duration-75 ease-out`}
+							style={{ backgroundColor: color }}
+							onClick={() => setSelectedColor(color)}
+						>
+							{isSelected && <Check color="white" size={20} strokeWidth={2} />}
+						</div>
+					);
+				})}
+			</MenuItem>
 		</Menu>
 	);
 }
