400986b4158904045b5cbe93ead5bf81dd1e85ea
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-18 16:57:51 +0800

    Add updateCategoryColorAction to handle category color updates in todo lists

--- src/actions/category-action.ts
+++ src/actions/category-action.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { removeCategoryFromTodo, storeCategories } from '@/lib/category';
+import { removeCategoryFromTodo, storeCategories, updateCategoryColor } from '@/lib/category';
 
 export async function addTodoCategoriesAction(categoryColorsIds: number[], todoId: number) {
 	await storeCategories(todoId, categoryColorsIds);
@@ -10,4 +10,10 @@ export async function removeCategoryFromTodoAction(categoryColorId: number, todo
 	const result = await removeCategoryFromTodo(categoryColorId, todoId);
 
 	if (!result) console.error('Failed to delete category from todo');
+}
+
+export async function updateCategoryColorAction(categoryColorId: number, todolistId: number, newColor: string) {
+	const result = await updateCategoryColor(categoryColorId, todolistId, newColor);
+
+	if (!result) console.error('Failed to update hex_color from category_color');
 }
