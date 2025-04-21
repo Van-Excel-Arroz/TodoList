24357c9e04c31660fde2773f482b4f31b3d4b64f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-04-21 08:23:52 +0800

    Update TodolistContainer to add flex-1 class for improved layout responsiveness.

--- src/components/sidebar/components/TodolistContainer.tsx
+++ src/components/sidebar/components/TodolistContainer.tsx
@@ -21,7 +21,7 @@ function TodolistContainer({ initialTodoLists }: { initialTodoLists: TodoList[] 
 	const isTodolistsEmpty = todolists.length === 0;
 
 	return (
-		<div className="overflow-y-auto px-5 py-3">
+		<div className="overflow-y-auto px-5 py-3 flex-1">
 			{isTodolistsEmpty ? (
 				<TodoListEmptyText />
 			) : (
