a4aa6b31e177a8282e320292aca3c0a05ba84e96
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-05-26 19:27:11 +0800

    Remove commented-out section in TodoListPage component to clean up the code.

--- src/components/todo-list/TodoListPage.tsx
+++ src/components/todo-list/TodoListPage.tsx
@@ -23,10 +23,6 @@ export default async function TodoListPage({ todolistId }: { todolistId: number 
 	);
 }
 
-// ------------------------------------------------------------------------------------------------ //
-// COMPONENTS
-// ------------------------------------------------------------------------------------------------ //
-
 const LoadingAnimation = () => (
 	<div className="flex justify-center items-center h-screnn">
 		<h1 className="text-2xl font-bold">Loading.....</h1>
