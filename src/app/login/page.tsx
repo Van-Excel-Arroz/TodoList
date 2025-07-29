ac11f6e3aaf39936476c2b8869f0f35704e68283
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-07-29 18:15:00 +0800

    Enhance LoginPage component by adding input fields for username and password, improving user interaction and functionality.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,9 @@
 export default function LoginPage() {
-	return <p>Login Page</p>;
+	return (
+		<div>
+			<p>Login Page</p>
+			<input type="text" placeholder="Username" />
+			<input type="password" placeholder="Password" />
+		</div>
+	);
 }
