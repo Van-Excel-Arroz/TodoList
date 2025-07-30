4dc51947d4f2a3aea3a808c535114bd03cbf05a1
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-07-30 19:04:03 +0800

    Update LoginPage component layout and text for improved user experience and visual appeal.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 export default function LoginPage() {
 	return (
-		<div>
-			<p>Login Page</p>
+		<div className="h-[100vh] flex flex-col items-center justify-center">
+			<p>Login</p>
 			<input type="text" placeholder="Username" />
 			<input type="password" placeholder="Password" />
 		</div>
