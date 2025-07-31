1abcc9c245d763f733228784b61e8478176571c0
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-07-31 17:36:53 +0800

    Add labels for username and password fields in LoginPage component to enhance user clarity and interaction.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,9 @@ export default function LoginPage() {
 	return (
 		<div className="h-[100vh] flex flex-col items-center justify-center">
 			<p>Login</p>
+			<p>Username</p>
 			<input type="text" placeholder="Username" />
+			<p>Password</p>
 			<input type="password" placeholder="Password" />
 		</div>
 	);
