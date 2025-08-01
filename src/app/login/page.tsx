3083358f642d3eb99e436fa806124f4a27e71dc6
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-01 16:53:35 +0800

    Refactor LoginPage component layout to improve visual hierarchy and user experience by grouping input fields and enhancing text styling.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,15 @@
 export default function LoginPage() {
 	return (
 		<div className="h-[100vh] flex flex-col items-center justify-center">
-			<p>Login</p>
-			<p>Username</p>
-			<input type="text" placeholder="Username" />
-			<p>Password</p>
-			<input type="password" placeholder="Password" />
+			<div className="flex flex-col gap-6">
+				<p className="text-2xl font-bold text-center">Login</p>
+				<div className="flex flex-col gap-2">
+					<p>Username</p>
+					<input type="text" placeholder="Username" />
+					<p>Password</p>
+					<input type="password" placeholder="Password" />
+				</div>
+			</div>
 		</div>
 	);
 }
