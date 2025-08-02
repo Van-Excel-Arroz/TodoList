ea4544ba249fba189ed1852aab465f057847e249
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-02 15:51:19 +0800

    Update text styling for username and password labels in LoginPage component to enhance readability and user experience.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,9 +4,9 @@ export default function LoginPage() {
 			<div className="flex flex-col gap-6">
 				<p className="text-2xl font-bold text-center">Login</p>
 				<div className="flex flex-col gap-2">
-					<p>Username</p>
+					<p className="text-sm font-medium">Username</p>
 					<input type="text" placeholder="Username" />
-					<p>Password</p>
+					<p className="text-sm font-medium">Password</p>
 					<input type="password" placeholder="Password" />
 				</div>
 			</div>
