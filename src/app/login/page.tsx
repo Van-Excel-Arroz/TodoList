e1308e944be56829686af16899c5d1a1b0e69d62
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-03 13:46:01 +0800

    Enhance LoginPage component by adding input field styling, a login button, and a registration link, improving user interaction and overall layout.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,22 @@
+import Link from 'next/link';
+
 export default function LoginPage() {
 	return (
 		<div className="h-[100vh] flex flex-col items-center justify-center">
-			<div className="flex flex-col gap-6">
+			<div className="flex flex-col gap-8 w-[300px]">
 				<p className="text-2xl font-bold text-center">Login</p>
-				<div className="flex flex-col gap-2">
+				<div className="flex flex-col gap-4">
 					<p className="text-sm font-medium">Username</p>
-					<input type="text" placeholder="Username" />
+					<input type="text" placeholder="Username" className="border border-gray-300 rounded-md p-2" />
 					<p className="text-sm font-medium">Password</p>
-					<input type="password" placeholder="Password" />
+					<input type="password" placeholder="Password" className="border border-gray-300 rounded-md p-2" />
+				</div>
+				<button className="bg-black text-white rounded-md p-2">Login</button>
+				<div className="text-sm text-center flex flex-row items-center justify-center gap-2">
+					<p>Don&apos;t have an account?</p>
+					<Link className="text-blue-500" href="/register">
+						Register
+					</Link>
 				</div>
 			</div>
 		</div>
---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+	return (
+		<div className="h-[100vh] flex flex-col items-center justify-center">
+			<div className="flex flex-col gap-8 w-[300px]">
+				<p className="text-2xl font-bold text-center">Register</p>
+			</div>
+		</div>
+	);
+}
