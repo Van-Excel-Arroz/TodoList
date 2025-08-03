2171fe17ce262bddfa684e6f83e1dec4f603631b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-04 07:19:06 +0800

    Update LoginPage component to replace username input with email input for improved clarity and user experience.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,14 +6,14 @@ export default function LoginPage() {
 			<div className="flex flex-col gap-8 w-[300px]">
 				<p className="text-2xl font-bold text-center">Login</p>
 				<div className="flex flex-col gap-4">
-					<p className="text-sm font-medium">Username</p>
-					<input type="text" placeholder="Username" className="border border-gray-300 rounded-md p-2" />
+					<p className="text-sm font-medium">Email</p>
+					<input type="text" placeholder="example@gmail.com" className="border border-gray-300 rounded-md p-2" />
 					<p className="text-sm font-medium">Password</p>
 					<input type="password" placeholder="Password" className="border border-gray-300 rounded-md p-2" />
 				</div>
 				<button className="bg-black text-white rounded-md p-2">Login</button>
 				<div className="text-sm text-center flex flex-row items-center justify-center gap-2">
-					<p>Don&apos;t have an account?</p>
+					<p>Don't have an account?</p>
 					<Link className="text-blue-500" href="/register">
 						Register
 					</Link>
