a3195ad6e5982fa115d7ffcaf2c0ed622f4cc47f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-05 07:42:41 +0800

    Add bcryptjs for password hashing in user registration process

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,12 +1,15 @@
 import { query } from './db';
+import bcrypt from 'bcryptjs';
+const saltRounds = 10;
 
 export async function storeUser(email: string, username: string, password: string) {
 	try {
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
 		const result = await query(
 			`
 			INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING id
 			`,
-			[email, username, password]
+			[email, username, hashedPassword]
 		);
 		return result.rows[0].id;
 	} catch (error) {
