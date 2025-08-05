9c7ffd47f03ea7a5e0d351b72011c7f00de317ed
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-05 08:04:44 +0800

    Add user authentication functionality by implementing authenticateUser and authenticateUserAction functions for user login process.

--- src/actions/user-action.ts
+++ src/actions/user-action.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { storeUser } from '@/lib/user';
+import { authenticateUser, storeUser } from '@/lib/user';
 import { ActionState } from '@/utils/types';
 
 export async function createUserAction(
@@ -19,6 +19,24 @@ export async function createUserAction(
 		console.error('Failed to create the user');
 		return {
 			message: 'Failed to create the user',
+			success: false,
+			data: null,
+		};
+	}
+}
+
+export async function authenticateUserAction(email: string, password: string): Promise<ActionState<number | null>> {
+	const result = await authenticateUser(email, password);
+	if (result) {
+		return {
+			message: 'User authenticated successfully',
+			success: true,
+			data: result,
+		};
+	} else {
+		console.error('Failed to authenticate the user');
+		return {
+			message: 'Failed to authenticate the user',
 			success: false,
 			data: null,
 		};
--- src/lib/user.ts
+++ src/lib/user.ts
@@ -16,3 +16,19 @@ export async function storeUser(email: string, username: string, password: strin
 		console.log('Error inserting user in the database', error);
 	}
 }
+
+export async function authenticateUser(email: string, password: string): Promise<number | null> {
+	try {
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
+		const result = await query(
+			`
+			SELECT id FROM users WHERE email = $1 AND password = $2
+			`,
+			[email, hashedPassword]
+		);
+		return result.rows[0].id;
+	} catch (error) {
+		console.log('Error authenticating user in the database', error);
+		return null;
+	}
+}
