1054bb3e6626fa1c4e91e3d8d1a07b632587de21
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-25 07:09:41 +0800

    refractor generic server action test

--- src/tests/actions.test.ts
+++ src/tests/actions.test.ts
@@ -1,49 +1,8 @@
 import { updateTodoCompletionAction } from '@/actions/todo-action';
 import { updateTodoCompletion } from '../lib/todo';
+import { testServerAction } from './test-server-action';
 
 jest.mock(`../lib/todo`);
-
-function testServerAction(
-	serverAction: (...args: any) => any,
-	databaseFunction: (...args: any) => any,
-	testArgs: any,
-	successMessage: string,
-	failureMessage: string
-) {
-	const mockDatabaseFunction = databaseFunction as jest.MockedFunction<typeof databaseFunction>;
-
-	describe('serverAction', () => {
-		beforeEach(() => {
-			jest.clearAllMocks();
-		});
-
-		it('should return sucess when database update succeeds', async () => {
-			mockDatabaseFunction.mockResolvedValue(true);
-
-			const result = await serverAction(...testArgs);
-
-			expect(result).toEqual({
-				success: true,
-				message: successMessage,
-			});
-
-			expect(mockDatabaseFunction).toHaveBeenCalledWith(...testArgs);
-		});
-
-		it('should return failure when database update fails', async () => {
-			mockDatabaseFunction.mockResolvedValue(false);
-
-			const result = await serverAction(...testArgs);
-
-			expect(result).toEqual({
-				success: false,
-				message: failureMessage,
-			});
-
-			expect(mockDatabaseFunction).toHaveBeenCalledWith(...testArgs);
-		});
-	});
-}
 
 testServerAction(
 	updateTodoCompletionAction,
---
+++ src/tests/test-server-action.ts
@@ -0,0 +1,43 @@
+function testServerAction(
+	serverAction: (...args: any) => any,
+	databaseFunction: (...args: any) => any,
+	testArgs: any,
+	successMessage: string,
+	failureMessage: string
+) {
+	const mockDatabaseFunction = databaseFunction as jest.MockedFunction<typeof databaseFunction>;
+
+	describe('serverAction', () => {
+		beforeEach(() => {
+			jest.clearAllMocks();
+		});
+
+		it('should return sucess when database update succeeds', async () => {
+			mockDatabaseFunction.mockResolvedValue(true);
+
+			const result = await serverAction(...testArgs);
+
+			expect(result).toEqual({
+				success: true,
+				message: successMessage,
+			});
+
+			expect(mockDatabaseFunction).toHaveBeenCalledWith(...testArgs);
+		});
+
+		it('should return failure when database update fails', async () => {
+			mockDatabaseFunction.mockResolvedValue(false);
+
+			const result = await serverAction(...testArgs);
+
+			expect(result).toEqual({
+				success: false,
+				message: failureMessage,
+			});
+
+			expect(mockDatabaseFunction).toHaveBeenCalledWith(...testArgs);
+		});
+	});
+}
+
+export { testServerAction };
