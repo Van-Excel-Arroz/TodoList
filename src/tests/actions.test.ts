7094b85e2dd5eb3f4b1b049fe837e6af34d45a9b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-08-29 18:34:42 +0800

    Add tests for additional todo actions in actions.test.ts, including updating title, due date, description, and completed status.

--- src/tests/actions.test.ts
+++ src/tests/actions.test.ts
@@ -1,5 +1,23 @@
-import { deleteTodoAction, updateTodoCompletionAction, updateTodoImportanceAction } from '@/actions/todo-action';
-import { deleteTodo, updateTodoCompletion, updateTodoImportance } from '../lib/todo';
+import {
+	deleteTodoAction,
+	deleteTodoDueDateAction,
+	updateTodoCompletedAtAction,
+	updateTodoCompletionAction,
+	updateTodoDescriptionAction,
+	updateTodoDueDateAction,
+	updateTodoImportanceAction,
+	updateTodoTitleAction,
+} from '@/actions/todo-action';
+import {
+	deleteTodo,
+	deleteTodoDueDate,
+	updateTodoCompletedAt,
+	updateTodoCompletion,
+	updateTodoDescription,
+	updateTodoDueDate,
+	updateTodoImportance,
+	updateTodoTitle,
+} from '../lib/todo';
 import { testServerAction } from './test-server-action';
 
 jest.mock(`../lib/todo`);
@@ -21,3 +39,51 @@ testServerAction(
 );
 
 testServerAction(deleteTodoAction, deleteTodo, [1], 'Todo deleted successfully', 'Failed to delete todo');
+
+testServerAction(
+	updateTodoTitleAction,
+	updateTodoTitle,
+	[1, 'New Title'],
+	'Updated todo title successfully',
+	'Failed to update todo title'
+);
+
+testServerAction(
+	updateTodoDueDateAction,
+	updateTodoDueDate,
+	[1, new Date().toDateString()],
+	'Updated todo due date successfully',
+	'Failed to update todo due date'
+);
+
+testServerAction(
+	deleteTodoDueDateAction,
+	deleteTodoDueDate,
+	[1],
+	'Deleted todo due date successfully',
+	'Failed to delete todo due date'
+);
+
+testServerAction(
+	updateTodoDescriptionAction,
+	updateTodoDescription,
+	[1, 'New Description'],
+	'Updated todo description successfully',
+	'Failed to update todo description'
+);
+
+testServerAction(
+	updateTodoDescriptionAction,
+	updateTodoDescription,
+	[1, null],
+	'Updated todo description successfully',
+	'Failed to update todo description'
+);
+
+testServerAction(
+	updateTodoCompletedAtAction,
+	updateTodoCompletedAt,
+	[1, new Date().toDateString()],
+	'Updated todo completed at successfully',
+	'Failed to update todo completed at'
+);
