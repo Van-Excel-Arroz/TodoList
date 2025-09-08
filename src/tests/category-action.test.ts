ff4c62c1aaf9991d48deb148162014491a63fa89
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-09-08 14:50:05 +0800

    Add test for updating category color action in category-action.test.ts

--- src/tests/category-action.test.ts
+++ src/tests/category-action.test.ts
@@ -1,6 +1,7 @@
 import { removeCategoryFromTodoAction } from '@/actions/category-action';
 import { testServerAction } from './test-server-action';
-import { removeCategoryFromTodo } from '../lib/category';
+import { removeCategoryFromTodo, updateCategoryColor } from '../lib/category';
+import { updateCategoryColorAction } from '../actions/category-action';
 
 jest.mock(`../lib/category`);
 
@@ -10,4 +11,12 @@ testServerAction(
 	[1, 2],
 	'Category removed from todo successfully',
 	'Failed to remove category from todo'
+);
+
+testServerAction(
+	updateCategoryColorAction,
+	updateCategoryColor,
+	[1, 2, '#000000'],
+	'Category color updated successfully',
+	'Failed to update category color'
 );
